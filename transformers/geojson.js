5e9e73e494086345de928f4938c499e8f5cbed4a
William Newman <newmanw10@gmail.com>
2013-04-24 19:54:27 +0000

    forgot to add some files>
    .

---
+++ transformers/geojson.js
@@ -0,0 +1,43 @@
+module.exports = function() {
+
+  var include = function(property, fields) {
+    return fields.indexOf(property) != -1;
+  }
+
+  var transformFeature = function(feature, ret, options) {
+    delete ret._id;
+    delete ret.__v;
+
+    // handle attributes
+    var properties = options.properties;
+    var attributes = null;
+    if (properties) {
+      for (var property in feature.properties) {
+        if (!include(property, properties)) {
+          delete ret.properties[property];
+        }
+      }
+    }
+  }
+
+  var transform = function(features, properties) {
+    var response = { type: "FeatureCollection",
+      bbox: [-180, -90, 180, 90.0],
+      features: []
+    };
+
+    // Generate features portion of response
+    features.forEach(function(feature) {
+      response.features.push(feature.toJSON({
+        transform: transformFeature,
+        properties: properties
+      }));
+    });
+
+    return response;
+  }
+
+  return {
+    transform: transform
+  }
+}
