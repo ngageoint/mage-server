094d39bf0210e84318e77482c93d0b7edf274a24
Tyler Burgett <tyler.burgett@gmail.com>
2013-05-01 21:28:57 +0000

    very rough layer builder mockup

---
+++ public/js/app/FormBuilderController.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var sage = angular.module("sage", ["sage.***REMOVED***s"]);
+
+/*
+  Handle communication between the server and the map.
+  Load observations, allow users to view them, and allow them to add new ones themselves.
+*/
+function FormBuilderController($scope, $log, $http, $injector, appConstants, teams, levels, observationTypes) {
+	$scope.selectedTab = 1;
+	$scope.formFields = {};
+	$scope.currentLayer = {};
+	$scope.layers = appConstants.layers;
+
+	$scope.addTextInput = function () {
+
+	}
+
+	$scope.addTextArea = function () {
+
+	}
+
+	$scope.addSelect = function () {
+
+	}
+
+	$scope.addCheckbox = function () {
+
+	}
+}
