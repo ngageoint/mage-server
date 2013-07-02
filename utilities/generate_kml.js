2e928094f52ae1393e6a88bf852c68b1f995490e
Travis Baumgart <travis.baumgart@gmail.com>
2013-07-02 21:46:25 +0000

    Added kml export to existing mage instance as a new endpoint: /api/locations/export.

--- utilities/generate_kml.js
+++ utilities/generate_kml.js
@@ -1,33 +1,19 @@
-var http = require("http");
-//var exec = require('child_process').exec;
+module.exports = function(options) {
 
-http.createServer(function(req, res) {
-  
-    res.writeHead(200,{"Content-Type": "application/vnd.google-earth.kml+xml"});
-    res.write(generateKMLHeader());
-    res.write(generateKMLDocument());
-    res.write(generatePlacemark('point1', 'localhost', 39.83636818,-105.646844,3332.199951));
-    res.write(generateKMLDocumentClose());
-    res.end(generateKMLClose());
-
-}).listen(8000);
-console.log('Node server running');
-
-
-function generateKMLHeader() {
+var generateKMLHeader = function() {
   var header = "<?xml version='1.0' encoding='UTF-8'?>" + 
                "<kml xmlns='http://www.opengis.net/kml/2.2' xmlns:gx='http://www.google.com/kml/ext/2.2' xmlns:kml='http://www.opengis.net/kml/2.2' xmlns:atom='http://www.w3.org/2005/Atom'>";
   return header;
-}
+};
 
-function generateKMLDocument() {
+var generateKMLDocument = function() {
 	var doc = "<Document>" + 
 	          "<name>MAGE Export</name>" +
 	          "<open>1</open>";
 	           return doc;
-}
+};
 
-function generatePlacemark(name, styleUrl, lat, lon, alt) {
+var generatePlacemark = function(name, styleUrl, lat, lon, alt) {
   var placemark = "<Placemark>" + 
                   "  <name>" + name + "</name>" + 
                   "  <styleUrl>" + styleUrl + "</styleUrl>" +
@@ -36,14 +22,24 @@ function generatePlacemark(name, styleUrl, lat, lon, alt) {
                   "  </Point>" +
                   "</Placemark>";
   return placemark;
-}
+};
 
-function generateKMLDocumentClose() {
+var generateKMLDocumentClose = function() {
   var doc = "</Document>";	
   return doc;
-}
+};
 
-function generateKMLClose() {
+var generateKMLClose = function() {
 	var kml = "</kml>";
 	return kml;
-}
+};
+
+return {
+  generateKMLHeader: generateKMLHeader,
+  generateKMLDocument: generateKMLDocument,
+  generatePlacemark: generatePlacemark,
+  generateKMLDocumentClose: generateKMLDocumentClose,
+  generateKMLClose: generateKMLClose
+ }
+
+}()
