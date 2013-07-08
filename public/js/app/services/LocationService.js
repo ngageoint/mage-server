654ea8063bf2da97a6cbecafb5b9b4ba56e3af2b
Travis Baumgart <travis.baumgart@gmail.com>
2013-07-08 20:40:42 +0000

    Added preliminary wiring for kml export.  More to come.

--- public/js/app/services/LocationService.js
+++ public/js/app/services/LocationService.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('mage.locationService', ['mage.***REMOVED***s', 'mage.lib'])
-  .factory('locationService', ['$http', 'appConstants', 'mageLib', 
+  .factory('LocationService', ['$http', 'appConstants', 'mageLib', 
     function ($http, appConstants, mageLib) {
       var locationServiceFunctions = {};
 
       locationServiceFunctions.export = function () {
-        
+        return $http.get(appConstants.rootUrl + "/api/locations/export?&access_token=" + mageLib.getLocalItem('token'));
       };
 
       return locationServiceFunctions;
