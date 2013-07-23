6b8c9f3e051dd6d22585f3126dbd737f70bef26d
Travis Baumgart <travis.baumgart@gmail.com>
2013-07-23 17:18:05 +0000

    added edits to export capability.
    1) Added support for basic error handling.
    2) Added logic to set export filename.
    3) General cleanup of variable names and looking constructs.

--- utilities/generate_kml.js
+++ utilities/generate_kml.js
@@ -11,8 +11,28 @@ var generateKMLHeader = function() {
 
 var generateKMLDocument = function() {
 	var doc = "<Document>" + 
-	          "  <name>MAGE Export</name>" +
-	          "  <open>1</open>";
+	          "  <name>MAGE-Export.kml</name>" +
+	          "  <open>1</open>" +
+            "  <Style id='Animal Issue'><IconStyle> <Icon> <href>animal_issue.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Chemical Hazard'><IconStyle> <Icon> <href>hazardous_material_incident.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Command Post'><IconStyle> <Icon> <href>.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Confirmed Victim'><IconStyle> <Icon> <href>confirmed_survivor.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Confirmed Victim Removed'><IconStyle> <Icon> <href>confirmed_victim_removed.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Emergency Collection Point'><IconStyle> <Icon> <href>emergency_shelter.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Fire'><IconStyle> <Icon> <href>.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Helicopter Landing Site'><IconStyle> <Icon> <href>helicopter_landing_site.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Human Remains'><IconStyle> <Icon> <href>human_remains.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Possible Criminal Activity'><IconStyle> <Icon> <href>.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Shelter in Place'><IconStyle> <Icon> <href>shelter_in_place.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Special Needs'><IconStyle> <Icon> <href>***REMOVED***isted.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Staging Area'><IconStyle> <Icon> <href>.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Start Search'><IconStyle> <Icon> <href>.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Stop Search'><IconStyle> <Icon> <href>.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Structure Damaged but Safe'><IconStyle> <Icon> <href>structure_minor_damage.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Structure Major Damage No Entry'><IconStyle> <Icon> <href>structure_major_damage.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Structure No Damage'><IconStyle> <Icon> <href>structure_no_damage.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Victim Detected'><IconStyle> <Icon> <href>human_remains.png</href> </Icon></IconStyle></Style>" +
+            "  <Style id='Water Level'><IconStyle> <Icon> <href>flood_water_level.png</href> </Icon></IconStyle></Style>";
 	 return doc;
 };
 
@@ -25,7 +45,7 @@ var generateKMLFolderStart = function(name) {
 var generatePlacemark = function(name, styleUrl, lon, lat, alt, desc) {
   var placemark = "<Placemark>" + 
                   "  <name>" + name + "</name>" + 
-                  "  <styleUrl>" + styleUrl + "</styleUrl>" +
+                  "  <styleUrl>#" + styleUrl + "</styleUrl>" +
                   "  <Point>" +
                   "    <coordinates>" + lon + "," + lat + "," + alt + "</coordinates>" +
                   "  </Point>" +
