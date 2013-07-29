56bef3fa8591ee4efc8cc63567a7ecc93ca81d12
William Newman <newmanw10@gmail.com>
2013-07-29 17:25:02 +0000

    - nav bar is now in index.html, removed sprinkled navbar from all other partials - changed up some code in leaflet directive with respect to showing users locations

--- public/js/app/controllers/NavController.js
+++ public/js/app/controllers/NavController.js
@@ -2,13 +2,24 @@
 
 function NavController($rootScope, $scope, $log, $http, $injector, $location, appConstants, UserService, mageLib) {
   $scope.user = {};
-  $scope.showMapLink = false;
-  $scope.showLayerLink = false;
-  $scope.showAdminLink = false;
+  $scope.showMapNav = false;
+  $scope.showLayerNav = false;
+  $scope.showAdminNav = false;
+
+  $scope.navCl***REMOVED*** = function (page) {
+    var currentRoute = $location.path().substring(1) || 'map';
+    return page === currentRoute ? 'active' : '';
+  };
 
   $scope.user = UserService.getMyself().
-    success(function (data) {
-      $scope.user = data;
+    success(function (user) {
+      switch (user.role.name) {
+        case "ADMIN_ROLE":
+          $scope.showLayerNav = true;
+          $scope.showAdminNav = true;
+        case "USER_ROLE":
+          $scope.showMapNav = true;
+      }
     }).
     error(function (data, status, headers, config) {
 
