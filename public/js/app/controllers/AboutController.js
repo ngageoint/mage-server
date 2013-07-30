fd7598fd35ef9df65b0b40bdf2cc01dc48b84340
Tyler Burgett <tyler.burgett@gmail.com>
2013-07-30 00:32:51 +0000

    Updated the user manangement section of the admin page to be more like the device section. Added an about page with some info about MAGE and a link to the APK for easy user install. Updated some icons.

---
+++ public/js/app/controllers/AboutController.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function AboutController ($scope, $log, appConstants) {
+  $scope.deviceProvisioningEnabled = true;
+  $scope.locationServicesEnabled = true;
+  $scope.serverVersion = 0.2;
+
+}
