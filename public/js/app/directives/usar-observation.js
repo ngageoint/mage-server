800c242ebb9053212995b1ea0e8ddece1d19c505
William Newman <newmanw10@gmail.com>
2013-08-07 17:44:47 +0000

    - added feature controller - symbology using leafalet awesome markers complete for PDC

---
+++ public/js/app/directives/usar-observation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+mage.directive('***REMOVED***Observation', function () {
+  return {
+    restrict: "A",
+    templateUrl: "/js/app/partials/***REMOVED***/observation.html",
+    controller: "FeatureController"
+  };
+});
---
+++ public/js/app/services/IconService.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('mage.iconService', ['mage.***REMOVED***s', 'mage.lib'])
+  .factory('IconService', ['$http', 'appConstants',
+    function ($http, appConstants) {
+      var ***REMOVED*** = {};
+
+      ***REMOVED***.defaultIcon = function() {
+        return L.AwesomeMarkers.icon({
+          icon: 'plus',
+          color: 'cadetblue'
+        });
+      }
+
+      ***REMOVED***.icon = function (feature, o) {
+        var type = _.find(o.types, function(type) { 
+          return type.name == feature.properties.TYPE; 
+        });
+        var icon = type ? type.icon : 'circle';
+
+        var level = _.find(o.levels, function(level) {
+          return level.name === feature.properties.LEVEL;
+        });
+        var color = level ? level.color : 'blue';
+
+        var icon = L.AwesomeMarkers.icon({
+          icon: icon,
+          color: color
+        });
+
+        return icon;
+      };
+
+      return ***REMOVED***;
+    }
+  ]
+);
