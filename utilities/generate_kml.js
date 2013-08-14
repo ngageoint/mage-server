179d1573fe12d76494381f31e5e64f486ebbcfd2
Travis Baumgart <travis.baumgart@gmail.com>
2013-08-14 22:18:35 +0000

    Adding new icons for KML export.  Refactor generate_kml.js to handle EVENTLEVEL.

--- utilities/generate_kml.js
+++ utilities/generate_kml.js
@@ -1,5 +1,7 @@
 module.exports = function(options) {
 
+feature_types = require('../utilities/feature_types')
+
 var generateKMLHeader = function() {
   var header = "<?xml version='1.0' encoding='UTF-8'?>" + 
                "<kml xmlns='http://www.opengis.net/kml/2.2' " + 
@@ -10,30 +12,19 @@ var generateKMLHeader = function() {
 };
 
 var generateKMLDocument = function() {
-	var doc = "<Document>" + 
+	
+  var types = feature_types.getTypes();
+
+  var doc = "<Document>" + 
 	          "  <name>MAGE-Export.kml</name>" +
-	          "  <open>1</open>" +
-            "  <Style id='Animal Issue'><IconStyle> <Icon> <href>icons/bear.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Chemical Hazard'><IconStyle> <Icon> <href>icons/radiation.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Command Post'><IconStyle> <Icon> <href>icons/emb***REMOVED***y.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Confirmed Victim'><IconStyle> <Icon> <href>icons/pirates.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Confirmed Victim Removed'><IconStyle> <Icon> <href>icons/rescue-2.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Emergency Collection Point'><IconStyle> <Icon> <href>icons/communitycentre.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Fire'><IconStyle> <Icon> <href>icons/fire.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Helicopter Landing Site'><IconStyle> <Icon> <href>icons/helicopter.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Human Remains'><IconStyle> <Icon> <href>icons/headstone-2.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Possible Criminal Activity'><IconStyle> <Icon> <href>icons/wrestling-2.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Shelter in Place'><IconStyle> <Icon> <href>icons/bunker-2-2.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Special Needs'><IconStyle> <Icon> <href>icons/nursing_home_icon.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Staging Area'><IconStyle> <Icon> <href>icons/regroup.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Start Search'><IconStyle> <Icon> <href>icons/start-race-2.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Stop Search'><IconStyle> <Icon> <href>icons/stop.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Structure Damaged but Safe'><IconStyle> <Icon> <href>icons/hut.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Structure Major Damage No Entry'><IconStyle> <Icon> <href>icons/earthquake-3.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Structure No Damage'><IconStyle> <Icon> <href>icons/home-2.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Victim Detected'><IconStyle> <Icon> <href>icons/crimescene.png</href> </Icon></IconStyle></Style>" +
-            "  <Style id='Water Level'><IconStyle> <Icon> <href>icons/flood.png</href> </Icon></IconStyle></Style>" + 
-            "  <Style id='FFT'><IconStyle> <Icon> <href>icons/male-2.png</href> </Icon></IconStyle></Style>";
+	          "  <open>1</open>";
+  for(i in types) {
+    var type = types[i];
+    doc += "<Style id='" + type.name + "-blue'><IconStyle><Icon><href>icons/" + type.icon + "-blue.png</href></Icon></IconStyle></Style>";
+    doc += "<Style id='" + type.name + "-green'><IconStyle><Icon><href>icons/" + type.icon + "-green.png</href></Icon></IconStyle></Style>";
+    doc += "<Style id='" + type.name + "-yellow'><IconStyle><Icon><href>icons/" + type.icon + "-yellow.png</href></Icon></IconStyle></Style>";
+    doc += "<Style id='" + type.name + "-red'><IconStyle><Icon><href>icons/" + type.icon + "-red.png</href></Icon></IconStyle></Style>";
+  }
 	 return doc;
 };
 
@@ -66,14 +57,33 @@ var generatePlacemark = function(name, styleUrl, lon, lat, alt, feature, attachm
     }
   }
 
+  switch (feature.EVENTLEVEL) 
+  {
+    case 'Normal':
+      styleColor = "blue";
+      break;
+    case 'Yellow':
+      styleColor = "yellow";
+      break;
+    case 'Green':
+      styleColor = "green";
+      break;
+    case 'Red':
+      styleColor = "red";
+      break;
+    default:
+      styleColor = "blue";
+  }
+
   var placemark = "<Placemark>" + 
                   "  <name>" + name + "</name>" + 
-                  "  <styleUrl>#" + styleUrl + "</styleUrl>" +
+                  "  <styleUrl>#" + styleUrl + "-" + styleColor + "</styleUrl>" +
                   "  <Point>" +
                   "    <coordinates>" + lon + "," + lat + "," + alt + "</coordinates>" +
                   "  </Point>" +
                   "  <description>" + desc + '<br/>' + media +"</description>" + 
                   "</Placemark>";
+
   return placemark;
 };
 
