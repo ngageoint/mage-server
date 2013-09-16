0e86fd0ded177c5491a82587d04384b31205702a
Dan Barela <daniel.barela@gmail.com>
2013-09-16 14:32:09 +0000

    added in some thoughts

--- public/js/app/services/MapService.js
+++ public/js/app/services/MapService.js
@@ -16,4 +16,19 @@ angular.module('mage.mapService', ['mage.***REMOVED***s', 'mage.lib'])
       };
 
       return functions;
-    }]) // end of MapService
+    }]); // end of MapService
+
+  /* Barela idea */
+  /*
+angular.module('mage.mapService', ['mage.***REMOVED***s', 'mage.lib'])
+  .factory('MapService', ['appConstants', 'mageLib',
+    function (appConstants, mageLib) {
+      var ***REMOVED*** = {
+        currentMapPanel: 'layers' // options are: layers, observation, export, or none
+      };
+      // now in your controller you can just access MapService.currentMapPanel.  The controller
+      // will be able to watch that variable fine if you attach the ***REMOVED*** to a scope variable ie
+      // $scope.mapService = MapService; (after injecting that of course)
+      return ***REMOVED***;
+    }]);
+    */
