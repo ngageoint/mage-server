b85f2080bd0508661be918b30a76d840204b7bd9
Dan Barela <daniel.barela@gmail.com>
2013-09-27 17:37:38 +0000

    set the access token in the header

--- public/js/app/services/LocationService.js
+++ public/js/app/services/LocationService.js
@@ -6,17 +6,17 @@ angular.module('mage.locationService', ['mage.***REMOVED***s', 'mage.lib'])
       var ***REMOVED*** = {};
 
       ***REMOVED***.export = function () {
-        return $http.get(appConstants.rootUrl + "/api/locations/export?&access_token=" + mageLib.getLocalItem('token'));
+        return $http.get(appConstants.rootUrl + "/api/locations/export");
       };
 
       ***REMOVED***.createLocation = function (location) {
-        return $http.post(appConstants.rootUrl + '/api/locations?&access_token=' + mageLib.getLocalItem('token'), 
+        return $http.post(appConstants.rootUrl + '/api/locations', 
           JSON.stringify(location),
           {headers: {"Content-Type": "application/json"}});
       };
 
       ***REMOVED***.getLocations = function () {
-        return $http.get(appConstants.rootUrl + '/api/locations?&access_token=' + mageLib.getLocalItem('token'));
+        return $http.get(appConstants.rootUrl + '/api/locations');
       }
 
       ***REMOVED***.getPosition = function() {
