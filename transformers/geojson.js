cf455a32f1e268edcddadcb942c3ea3660545514
William Newman <newmanw10@gmail.com>
2013-10-09 16:29:20 +0000

    - needed to transform all geojson request to add id field and remove _id

--- transformers/geojson.js
+++ transformers/geojson.js
@@ -1,9 +1,16 @@
-// TODO switch to use Terraformer to create a geojson FeatureCollection
-exports.transform = function(features) {
+var util = require('util');
 
+// TODO switch to use Terraformer to create a geojson FeatureCollection
+var transformFeature = function(feature) {
+  feature.id = feature._id;
+  delete feature._id;
+
+  return feature;
+}
+
+var transformFeatures = function(features) {
   features.forEach(function(feature) {
-    feature.id = feature._id;
-    delete feature._id;
+    transformFeature(feature);
   });
 
   return { 
@@ -11,4 +18,8 @@ exports.transform = function(features) {
     bbox: [-180, -90, 180, 90.0],
     features: features
   };
+}
+
+exports.transform = function(features) {
+  return util.isArray(features) ? transformFeatures(features) : transformFeature(features);
 }
