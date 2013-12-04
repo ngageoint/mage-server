4e83fe78171794fcd44fa46f10721637e7c2f9d6
Dan Barela <daniel.barela@gmail.com>
2013-12-04 17:15:38 +0000

    news feed paging

--- public/js/app/directives/news-feed.js
+++ public/js/app/directives/news-feed.js
@@ -2,8 +2,9 @@ mage.directive('newsFeed', function(UserService, appConstants) {
   return {
     restrict: "A",
     templateUrl:  "js/app/partials/news-feed.html",
-    controller: function ($scope, MapService) {
+    controller: function ($scope, MapService, TimeBucketService) {
       $scope.ms = MapService;
+      $scope.timebuckets = TimeBucketService.buckets;
     }
   };
 });
