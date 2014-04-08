596441496c6c6bf4cb035aeec5958c5caa9bc0e4
William Newman <newmanw10@gmail.com>
2014-04-08 10:30:27 -0600

    User Account state
    User accounts now carry active state.  If an admin user creates an account the state is set to active and the user can login.  I a person creates a new account from the signup page the active state is set to false.  An admin can approve the account.
    
    We used to not ign a role to a new user, therefore denying them access to any data.  Users could login without roles they just could not see anything.
    
    We will now require all new users have a 'default' role, for now that is USER_ROLE.
    
    fixes #27

--- migrations/004-create-admin-user.js
+++ migrations/004-create-admin-user.js
@@ -15,10 +15,12 @@ exports.up = function(next) {
     if (!role) return next(new Error('No ADMIN_ROLE found to attach to ADMIN_USER'));
 
     var adminUser = {
+      active: 'true',
       username: 'admin',
       p***REMOVED***word: 'admin',
       firstname: 'admin',
-      lastname: 'admin'
+      lastname: 'admin',
+      role: role._id
     };
 
     User.createUser(adminUser, function(err, user) {
@@ -27,14 +29,7 @@ exports.up = function(next) {
         return next(err);
       }
 
-      User.setRoleForUser(user, role._id, function(err, user) {
-        if (err) {
-          mongoose.disconnect();
-          return next(err);
-        }
-
-        mongoose.disconnect(next);
-      });
+      mongoose.disconnect(next);
     });
   });
 };
