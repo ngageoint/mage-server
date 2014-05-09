70d837ed6ab7846184db3421ffca229e8a73a711
William Newman <newmanw10@gmail.com>
2014-05-09 09:57:17 -0600

    Initial move of rage to a plugin

---
+++ plugins/mage-rage/index.js
@@ -0,0 +1,58 @@
+var child = require('child_process')
+  , config = require('./config.json')
+
+console.log('activating rage plugin');
+
+function startObservations() {
+  var observations = config.esri.observations;
+  if (observations.enable) {
+    // start observations worker
+    var observationsWorker = child.fork(__dirname + '/observations');
+
+    observationsWorker.on('error', function(err) {
+      console.log('***************** epic observation error ******************************', err);
+      observationsWorker.kill();
+      startObservations();
+    });
+
+    observationsWorker.on('exit', function(exitCode) {
+      console.log('***************** epic observation  exit, code ************************', exitCode);
+      if (exitCode != 0) {
+        observationsWorker.kill();
+        startObservations();
+      }
+    });
+
+    process.on('exit', function() {
+      console.log('***************** epic parent process exit, killing ********************', err);
+      observationsWorker.kill();
+    });
+  }
+}
+startObservations();
+
+function startAttachments() {
+  var attachments = config.esri.attachments;
+  if (attachments.enable) {
+    // start attachments worker
+    var attachmentsWorker = child.fork(__dirname + '/attachments');
+
+    attachmentsWorker.on('error', function(err) {
+      console.log('epic attachment error', err);
+      attachmentsWorker.kill();
+    });
+
+    attachmentsWorker.on('exit', function(exitCode) {
+      console.log('epic attachment  exit, code', exitCode);
+      if (exitCode != 0) {
+        attachmentsWorker.kill();
+        startAttachments();
+      }
+    });
+
+    process.on('exit', function() {
+      attachmentsWorker.kill();
+    });
+  }
+}
+startAttachments();
