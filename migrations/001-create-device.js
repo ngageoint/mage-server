8b4c1d9015c25f1a66536f2004c268b7d6437002
William Newman <newmanw10@gmail.com>
2014-05-12 13:38:54 -0600

    Changed tmp migrations back

--- migrations/001-create-device.js
+++ migrations/001-create-device.js
@@ -8,7 +8,7 @@ exports.up = function(next) {
 
   var device = {
     name: "Initial Device",
-    uid: "123456",
+    uid: "12345",
     registered: true,
     description: "This is the initial device for the web console.  Please create a new device with a more secure unique id and delete this device."
   };
