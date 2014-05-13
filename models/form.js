a709cf1cace78d89f7b7a934fb1c8f5747774e5b
William Newman <newmanw10@gmail.com>
2014-05-13 10:42:29 -0600

    Redesign on admin page. Fixed up some of the dynamic form layout stuff as it was messed up

--- models/form.js
+++ models/form.js
@@ -3,12 +3,21 @@ var mongoose = require('mongoose');
 // Creates a new Mongoose Schema object
 var Schema = mongoose.Schema;
 
+var OptionSchema = new Schema({
+  id: { type: Number, required: true },
+  title: { type: String, required: true },  
+  value: { type: Number, required: true }
+},{
+  _id: false
+});
+
 var FieldSchema = new Schema({
   id: { type: Number, required: true },
   title: { type: String, required: true },  
   type: { type: String, required: true },
   value: { type: Schema.Types.Mixed, required: true },
-  required: { type: Boolean, required: true }
+  required: { type: Boolean, required: true },
+  choices: [OptionSchema]
 },{
   _id: false
 });
