f58c405be13768d2e94986bde1f76fc70fd5c31a
William Newman <newmanw10@gmail.com>
2014-05-21 14:14:45 -0600

    Dynamic form observation view is working

--- public/js/app/services/observation/ObservationService.js
+++ public/js/app/services/observation/ObservationService.js
@@ -1,49 +1,54 @@
 'use strict';
 
-angular.module('mage')
-  .factory('ObservationService', ['$q', 'appConstants', 'Feature', 'UserService', 'Layer', 'Form', 'FormService',
-    function ($q, appConstants, Feature, UserService, Layer, Form, FormService) {
-      var ***REMOVED*** = {};
+angular.module('mage').factory('ObservationService', ['$q', 'appConstants', 'Feature', 'UserService', 'Layer', 'Form', 'FormService',
+  function ($q, appConstants, Feature, UserService, Layer, Form, FormService) {
+    var ***REMOVED*** = {};
 
-      var form = $q.defer();
-      var formPromise = form.promise;
-      var layers = Layer.query(function(){
-          angular.forEach(layers, function (layer) {
-            if (layer.type == 'Feature') {
-              Form.query(function(forms) {
-                angular.forEach(forms, function(returnedForm) {
-                  if (returnedForm.id == layer.formId) {
-                    form.resolve(returnedForm);
-                    appConstants.formId = layer.formId;
-                  }
-                });
-              });
-            }
-          });
-      });
-
-      ***REMOVED***.newForm = null;
-      ***REMOVED***.createNewForm = function(observation) {
-        var promise = formPromise.then(function(form) {
-            var newForm = angular.copy(form);
-            newForm.observationId = observation.id;
-            newForm.getField("timestamp").value  = observation.properties.timestamp;
-            newForm.getField("geometry").value = observation.geometry.coordinates;
-
-            _.each(observation.properties, function(value, key) {
-              newForm.getField(key).value = value;
+    var form = $q.defer();
+    var formPromise = form.promise;
+    var layers = Layer.query(function(){
+      angular.forEach(layers, function (layer) {
+        if (layer.type == 'Feature') {
+          Form.query(function(forms) {
+            angular.forEach(forms, function(returnedForm) {
+              if (returnedForm.id == layer.formId) {
+                form.resolve(returnedForm);
+                appConstants.formId = layer.formId;
+              }
             });
+          });
+        }
+      });
+    });
 
-            return newForm;
+    ***REMOVED***.newForm = null;
+    ***REMOVED***.createNewForm = function(observation) {
+      var promise = formPromise.then(function(form) {
+        var newForm = angular.copy(form);
+        newForm.observationId = observation.id;
+        newForm.getField("timestamp").value  = observation.properties.timestamp;
+        newForm.getField("geometry").value = {
+          x: observation.geometry.coordinates[0],
+          y: observation.geometry.coordinates[1]
+        }
+
+        _.each(observation.properties, function(value, key) {
+          var field = newForm.getField(key);
+          if (field) {
+            field.value = value;
+          }
         });
 
-        return promise;
-      }
+        return newForm;
+      });
 
-      ***REMOVED***.cancelNewForm = function() {
-        ***REMOVED***.newForm = null;
-      }
-
-      return ***REMOVED***;
+      return promise;
     }
-  ]);
+
+    ***REMOVED***.cancelNewForm = function() {
+      ***REMOVED***.newForm = null;
+    }
+
+    return ***REMOVED***;
+  }
+]);
