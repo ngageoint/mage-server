1cb5f5815436f822cc2d7cbd77dd2974834fe4e3
William Newman <newmanw10@gmail.com>
2014-06-13 08:23:01 -0600

    Fixed bug in news feed where correct icon variant was not shown

--- public/js/app/controllers/FormController.js
+++ public/js/app/controllers/FormController.js
@@ -108,6 +108,7 @@ angular.module('mage').controller('FormCtrl', function ($scope, $injector, appCo
       if (!$scope.variantField) {
         // they do not want a variant
         $scope.variants = [];
+        $scope.form.variantField = null;
         if (!doNotAutoSave) {
           $scope.autoSave();
         }
