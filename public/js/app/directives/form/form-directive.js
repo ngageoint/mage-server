ddcb20a2de933326750e1c8bd826bafad50f47eb
William Newman <newmanw10@gmail.com>
2014-06-19 09:00:43 -0600

    you can now delete attachment you added in edit mode.

--- public/js/app/directives/form/form-directive.js
+++ public/js/app/directives/form/form-directive.js
@@ -11,57 +11,66 @@ angular.module('mage').directive('formDirective', function (FormService, Observa
         formEdit: '='
       },
       controller: function($scope) {
-          var attachmentsUploaded = 0;
+        var uploadId = 0;
 
-          $scope.getToken = mageLib.getToken;
-          $scope.amAdmin = UserService.amAdmin;
-          $scope.attachmentUploads = [];
+        $scope.getToken = mageLib.getToken;
+        $scope.amAdmin = UserService.amAdmin;
+        $scope.attachmentUploads = {};
 
-          $scope.save = function() {
-            $scope.form.getObservation().$save({}, function(observation) {
+        $scope.save = function() {
+          $scope.form.getObservation().$save({}, function(observation) {
 
-              if ($scope.attachmentUploads.length > 0) {
-                $scope.observationSaved = true;
-              } else {
-                $scope.form = null;
-              }
-
-              // delete any attachments that are marked for delete
-              var markedForDelete = _.filter($scope.formObservation.attachments, function(a){ return a.markedForDelete; });
-              _.each(markedForDelete, function(attachment) {
-                var data = {id: attachment.id, layerId: appConstants.featureLayer.id, featureId: $scope.formObservation.id};
-                FeatureAttachment.delete(data, function(success) {
-                  $scope.$emit('attachmentDeleted', attachment);
-                });
-              });
-
-              $scope.$emit('newObservationSaved', observation);
-            });
-          }
-
-          $scope.cancelEdit = function() {
-            $scope.form = null;
-            $scope.attachmentUploads = [];
-            _.each($scope.formObservation.attachments, function(attachment) {
-              delete attachment.markedForDelete;
-            });
-          }
-
-          $scope.addAttachment = function() {
-            $scope.attachmentUploads.push({});
-          }
-
-          $scope.$on('uploadComplete', function(e, url, response) {
-            $scope.$emit('newAttachmentSaved', response);
-
-            attachmentsUploaded++;
-            if (attachmentsUploaded == $scope.attachmentUploads.length) {
+            if (_.some(_.values($scope.attachmentUploads), function(v) {return v;})) {
+              $scope.observationSaved = true;
+            } else {
               $scope.form = null;
-              $scope.observationSaved = false;
-              $scope.attachmentUploads = [];
-              attachmentsUploaded = 0;
+              $scope.attachmentUploads = {};
             }
+
+            // delete any attachments that are marked for delete
+            var markedForDelete = _.filter($scope.formObservation.attachments, function(a){ return a.markedForDelete; });
+            _.each(markedForDelete, function(attachment) {
+              var data = {id: attachment.id, layerId: appConstants.featureLayer.id, featureId: $scope.formObservation.id};
+              FeatureAttachment.delete(data, function(success) {
+                $scope.$emit('attachmentDeleted', attachment);
+              });
+            });
+
+            $scope.$emit('newObservationSaved', observation);
           });
+        }
+
+        $scope.cancelEdit = function() {
+          $scope.form = null;
+          $scope.attachmentUploads = [];
+          _.each($scope.formObservation.attachments, function(attachment) {
+            delete attachment.markedForDelete;
+          });
+        }
+
+        $scope.addAttachment = function() {
+          uploadId++;
+          $scope.attachmentUploads[uploadId] = false;
+        }
+
+        $scope.removeFileUpload = function(id) {
+          delete $scope.attachmentUploads[id];
+        }
+
+        $scope.$on('uploadComplete', function(e, url, response, id) {
+          $scope.$emit('newAttachmentSaved', response);
+
+          delete $scope.attachmentUploads[id];
+          if (_.keys($scope.attachmentUploads).length == 0) {
+            $scope.form = null;
+            $scope.observationSaved = false;
+            $scope.attachmentUploads = {};
+          }
+        });
+
+        $scope.$on('uploadFile', function(e, id) {
+          $scope.attachmentUploads[id] = true;
+        });
       }
     };
   });
