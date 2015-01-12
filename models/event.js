d087223d7fb5b4e42fb16aa6c49afe926ed2184d
William Newman <newmanw10@gmail.com>
2015-01-12 14:30:23 -0700

    Initial event routes and model
    Added new event permissions, routes, and the event model.  Each new event has its own observation
    collection.
    
    Updated swagger docs for these new routes

---
+++ models/event.js
@@ -0,0 +1,124 @@
+var mongoose = require('mongoose')
+, Counter = require('./counter');
+
+// Creates a new Mongoose Schema object
+var Schema = mongoose.Schema;
+
+// Creates the Schema for the Attachments object
+var EventSchema = new Schema({
+  id: { type: Number, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
+  description: { type: String, required: false },
+  collectionName: { type: String, required: true }
+},{
+  versionKey: false
+});
+
+var transform = function(event, ret, options) {
+  delete ret._id;
+  delete ret.collectionName;
+}
+
+EventSchema.set("toObject", {
+  transform: transform
+});
+
+EventSchema.set("toJSON", {
+  transform: transform
+});
+
+// Creates the Model for the Layer Schema
+var Event = mongoose.model('Event', EventSchema);
+exports.Model = Event;
+
+exports.getEvents = function(filter, callback) {
+  if (typeof filter == 'function') {
+    callback = filter;
+    filter = {};
+  }
+
+  var query = {};
+  // var type = filter.type;
+  // if (type) query.type = type;
+
+  Event.find(query, function (err, events) {
+    if (err) {
+      console.log("Error finding events in mongo: " + err);
+    }
+
+    callback(err, events);
+  });
+}
+
+exports.getById = function(id, callback) {
+  Event.findOne({id: id}, function (err, event) {
+    if (err) {
+      console.log("Error finding event in mongo: " + err);
+    }
+
+    callback(err, event);
+  });
+}
+
+var createObservationCollection = function(event) {
+  console.log("Creating observation collection: " + event.collectionName + ' for event ' + event.name);
+  mongoose.connection.db.createCollection(event.collectionName, function(err, collection) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log("Successfully created observation collection for event " + event.name);
+  });
+}
+
+var dropObservationCollection = function(event) {
+  console.log("Dropping observation collection: " + event.collectionName);
+  mongoose.connection.db.dropCollection(event.collectionName, function(err, results) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log('Dropped observation collection ' + event.collectionName);
+  });
+}
+
+exports.create = function(event, callback) {
+  Counter.getNext('event', function(id) {
+    event.id = id;
+    event.collectionName = 'observations' + id;
+
+    Event.create(event, function(err, newEvent) {
+      if (err) {
+        console.log("Problem creating event. " + err);
+        return callback(err);
+      }
+
+      createObservationCollection(newEvent);
+      callback(err, newEvent);
+    });
+  });
+}
+
+exports.update = function(id, event, callback) {
+  Event.findOneAndUpdate({id: id}, event, function(err, updatedEvent) {
+    if (err) {
+      console.log("Could not update event: " + err);
+    }
+
+    callback(err, updatedEvent);
+  });
+}
+
+exports.remove = function(event, callback) {
+  event.remove(function(err) {
+    if (err) {
+      console.error(err);
+    } else {
+      dropObservationCollection(event);
+    }
+
+    callback(err, event);
+  });
+}
