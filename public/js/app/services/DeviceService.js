db10b86293dc503a8a3481ae3075129fa1225230
William Newman <newmanw10@gmail.com>
2015-02-05 08:46:31 -0700

    fixed bug with device registration not updating UI updated swagger docs to remove some routes that no longer exist removed all old mage apk from source control

--- public/js/app/services/DeviceService.js
+++ public/js/app/services/DeviceService.js
@@ -1,52 +1,51 @@
 'use strict';
 
-angular.module('mage.deviceService', ['mage.***REMOVED***s', 'mage.lib'])
-  .factory('DeviceService', ['$http', 'appConstants', 'mageLib',
-    function ($http, appConstants, mageLib) {
-      var ***REMOVED*** = {};
+angular.module('mage.deviceService', ['mage.***REMOVED***s', 'mage.lib']).factory('DeviceService', ['$http', 'appConstants', 'mageLib',
+  function ($http, appConstants, mageLib) {
+    var ***REMOVED*** = {};
 
-      ***REMOVED***.getAllDevices = function () {
-        return $http.get('/api/devices/');
-      };
+    ***REMOVED***.getAllDevices = function () {
+      return $http.get('/api/devices/');
+    };
 
-      var resolvedDevices = {};
+    var resolvedDevices = {};
 
-      ***REMOVED***.getDevice = function(id) {
-        resolvedDevices[id] = resolvedDevices[id] || $http.get(
-          '/api/devices/' + id
-        );
-        return resolvedDevices[id];
-      }
+    ***REMOVED***.getDevice = function(id) {
+      resolvedDevices[id] = resolvedDevices[id] || $http.get(
+        '/api/devices/' + id
+      );
+      return resolvedDevices[id];
+    }
 
-      ***REMOVED***.createDevice = function(device) {
-        return $http.post(
-          '/api/devices',
-          $.param(device),
-          {headers: {"Content-Type": "application/x-www-form-urlencoded"}}
-        );
-      };
+    ***REMOVED***.createDevice = function(device) {
+      return $http.post(
+        '/api/devices',
+        $.param(device),
+        {headers: {"Content-Type": "application/x-www-form-urlencoded"}}
+      );
+    };
 
-      ***REMOVED***.updateDevice = function(device) {
-        return $http.put(
-          '/api/devices/' + device.id,
-          $.param(device),
-          {headers: {"Content-Type": "application/x-www-form-urlencoded"}}
-        );
-      };
+    ***REMOVED***.updateDevice = function(device) {
+      return $http.put(
+        '/api/devices/' + device.id,
+        $.param(device),
+        {headers: {"Content-Type": "application/x-www-form-urlencoded"}}
+      );
+    };
 
-      ***REMOVED***.registerDevice = function(device) {
-        return $http.put(
-          '/api/devices/' + device.id,
-          $.param({registered: true}),
-          {headers: {"Content-Type": "application/x-www-form-urlencoded"}}
-        );
-      };
+    ***REMOVED***.registerDevice = function(device) {
+      return $http.put(
+        '/api/devices/' + device.id,
+        $.param({registered: true}),
+        {headers: {"Content-Type": "application/x-www-form-urlencoded"}}
+      );
+    };
 
-      ***REMOVED***.deleteDevice = function(device) {
-        return $http.delete(
-        '/api/devices/' + device.id
-        );
-      }
+    ***REMOVED***.deleteDevice = function(device) {
+      return $http.delete(
+      '/api/devices/' + device.id
+      );
+    }
 
-      return ***REMOVED***;
-    }]);
+    return ***REMOVED***;
+}]);
