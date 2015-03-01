d61f6986dfa6f8b1a6c2eb59c2a4093b46782bff
William Newman <newmanw10@gmail.com>
2015-02-28 19:30:01 -0700

    Clean up a few more files to follow new style `

--- public/app/file-upload/file-upload.directive.js
+++ public/app/file-upload/file-upload.directive.js
@@ -1,6 +1,9 @@
-// TODO need to rename this directive
-mage.directive('simpleUpload', function() {
-  return {
+angular
+  .module('mage')
+  .directive('fileUpload', fileUpload);
+
+function fileUpload() {
+  var directive = {
     restrict: "A",
     templateUrl: '/app/file-upload/file-upload.directive.html',
     scope: {
@@ -12,113 +15,120 @@ mage.directive('simpleUpload', function() {
       uploadFileFormName: '=',
       defaultImageUrl: '='
     },
-    controller: function ($scope, $element) {
-      $scope.uploadImageMissing = true;
-      $element.find("img").error(function() {
-        $scope.uploadImageMissing = true;
-        $scope.$apply();
-        if (!$scope.defaultImageUrl) return;
-         $(this).attr('src', $scope.defaultImageUrl);
-      }).load(function() {
-        $scope.uploadImageMissing = false;
-      });
+    controller: FileUploadController,
+    bindToController: true
+  }
 
-      $element.find('.file-custom').attr('data-filename', 'Choose a file...');
+  return directive;
+}
 
-      $element.find(':file').bind('change', function() {
-        $scope.file = this.files[0];
-        var name = $scope.file.name;
+FileUploadController.$inject = ['$scope', '$element'];
 
-        var element = $($element.find('.upload-file')[0]);
-        if ($scope.preview) {
-          previewFile($scope.file, element);
-        }
-        $scope.$apply(function() {
-          $scope.$emit('uploadFile', $scope.uploadId);
-        });
-        upload();
-      });
+function FileUploadController($scope, $element) {
+  $scope.uploadImageMissing = true;
+  $element.find("img").error(function() {
+    $scope.uploadImageMissing = true;
+    $scope.$apply();
+    if (!$scope.defaultImageUrl) return;
+     $(this).attr('src', $scope.defaultImageUrl);
+  }).load(function() {
+    $scope.uploadImageMissing = false;
+  });
 
-      $scope.$watch('url', function(url) {
-        if (!url) return;
+  $element.find('.file-custom').attr('data-filename', 'Choose a file...');
 
-        $scope.uploadImageMissing = false;
-        $element.find('.preview').html(['<img cl***REMOVED***="preview-image" src="',$scope.url,'"/>'].join(''));
-      });
+  $element.find(':file').bind('change', function() {
+    $scope.file = this.files[0];
+    var name = $scope.file.name;
 
-      var previewFile = function(file, element) {
-        if (window.FileReader) {
-          var reader = new FileReader();
+    var element = $($element.find('.upload-file')[0]);
+    if ($scope.preview) {
+      previewFile($scope.file, element);
+    }
+    $scope.$apply(function() {
+      $scope.$emit('uploadFile', $scope.uploadId);
+    });
+    upload();
+  });
 
-          reader.onload = (function(theFile) {
-            return function(e) {
-              $scope.uploadImageMissing = false;
-              $scope.$apply();
-              element.find('.preview').html(['<img cl***REMOVED***="preview-image" src="', e.target.result,'" title="', theFile.name, '"/>'].join(''));
-            };
-          })(file);
+  $scope.$watch('url', function(url) {
+    if (!url) return;
 
-          reader.readAsDataURL(file);
-        }
-      }
+    $scope.uploadImageMissing = false;
+    $element.find('.preview').html(['<img cl***REMOVED***="preview-image" src="',$scope.url,'"/>'].join(''));
+  });
 
-      var uploadProgress = function(e) {
-        if(e.lengthComputable){
-          $scope.$apply(function() {
-            $scope.uploading = true;
-            $scope.uploadProgress = (e.loaded/e.total) * 100;
-          });
-        }
-      }
+  var previewFile = function(file, element) {
+    if (window.FileReader) {
+      var reader = new FileReader();
 
-      var uploadComplete = function(response) {
-        $scope.$apply(function() {
-          $scope.file = null;
-          $scope.uploadStatus = "Upload Complete";
-          $scope.uploading = false;
-          $scope.$emit('uploadComplete', $scope.url, response, $scope.uploadId);
-        });
-      }
+      reader.onload = (function(theFile) {
+        return function(e) {
+          $scope.uploadImageMissing = false;
+          $scope.$apply();
+          element.find('.preview').html(['<img cl***REMOVED***="preview-image" src="', e.target.result,'" title="', theFile.name, '"/>'].join(''));
+        };
+      })(file);
 
-      var uploadFailed = function(response, status, error) {
-        $scope.$apply(function() {
-          $scope.file = null;
-          $scope.uploadStatus = "Upload Failed";
-          $scope.uploading = false;
-          $scope.$emit('uploadFailed', $scope.url, response, $scope.uploadId);
-        });
-      }
+      reader.readAsDataURL(file);
+    }
+  }
 
-      var upload = function() {
-        if (!$scope.url || !$scope.allowUpload || !$scope.file) return;
-
-        var formData = new FormData();
-        formData.append($scope.uploadFileFormName, $scope.file);
-
-        $.ajax({
-            url: $scope.url,
-            type: 'POST',
-            xhr: function() {
-                var myXhr = $.ajaxSettings.xhr();
-                if(myXhr.upload){
-                    myXhr.upload.addEventListener('progress',uploadProgress, false);
-                }
-                return myXhr;
-            },
-            success: uploadComplete,
-            error: uploadFailed,
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false
-        });
-      }
-
-      $scope.$watch('allowUpload', function(newValue, old) {
-        if (newValue && old != newValue) {
-          upload();
-        }
+  var uploadProgress = function(e) {
+    if(e.lengthComputable){
+      $scope.$apply(function() {
+        $scope.uploading = true;
+        $scope.uploadProgress = (e.loaded/e.total) * 100;
       });
     }
-  };
-});
+  }
+
+  var uploadComplete = function(response) {
+    $scope.$apply(function() {
+      $scope.file = null;
+      $scope.uploadStatus = "Upload Complete";
+      $scope.uploading = false;
+      $scope.$emit('uploadComplete', $scope.url, response, $scope.uploadId);
+    });
+  }
+
+  var uploadFailed = function(response, status, error) {
+    $scope.$apply(function() {
+      $scope.file = null;
+      $scope.uploadStatus = "Upload Failed";
+      $scope.uploading = false;
+      $scope.$emit('uploadFailed', $scope.url, response, $scope.uploadId);
+    });
+  }
+
+  var upload = function() {
+    if (!$scope.url || !$scope.allowUpload || !$scope.file) return;
+
+    var formData = new FormData();
+    formData.append($scope.uploadFileFormName, $scope.file);
+
+    $.ajax({
+        url: $scope.url,
+        type: 'POST',
+        xhr: function() {
+            var myXhr = $.ajaxSettings.xhr();
+            if(myXhr.upload){
+                myXhr.upload.addEventListener('progress',uploadProgress, false);
+            }
+            return myXhr;
+        },
+        success: uploadComplete,
+        error: uploadFailed,
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+  }
+
+  $scope.$watch('allowUpload', function(newValue, old) {
+    if (newValue && old != newValue) {
+      upload();
+    }
+  });
+}
