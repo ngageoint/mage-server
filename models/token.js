c7e0496cb0ba6e18b48d112e8912aeb2a922ce21
William Newman <newmanw10@gmail.com>
2015-03-05 07:31:35 -0700

    Lets let mongo remove expried tokens with TTL

--- models/token.js
+++ models/token.js
@@ -13,40 +13,26 @@ var TokenSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User' },
     deviceId: { type: Schema.Types.ObjectId, ref: 'Device' },
     timestamp: { type: Date, required: true },
+    expireAt: { type: Date, required: true },
     token: { type: String, required: true }
   },{
     versionKey: false
   }
 );
 
+TokenSchema.index({'expireAt': 1}, {expireAfterSeconds: 0});
+
 // Creates the Model for the User Schema
 var Token = mongoose.model('Token', TokenSchema);
 
-var deleteExpiredTokens = function(callback) {
-  var expired = new Date(Date.now() -  tokenExpiration);
-  var query = {timestamp: {$lt: expired}};
-  Token.remove(query, function(err, number) {
-    if (err) {
-      console.log('could not remove expired tokens: ' + err);
+exports.getToken = function(token, callback) {
+  Token.findOne({token: token}).populate({path: 'userId', options: {lean: true}}).exec(function(err, token) {
+    if (!token || !token.userId) {
+      return callback(null, null);
     }
 
-    callback(err, number);
-  });
-}
-
-exports.getToken = function(token, callback) {
-  deleteExpiredTokens(function(err) {
-    var conditions = {token: token};
-    Token.findOne(conditions).populate({path: 'userId', options: {lean: true}}).exec(function(err, token) {
-
-      if (!token || !token.userId) {
-        return callback(null, null);
-      }
-
-      token.userId.populate('roleId', function(err, user) {
-        return callback(err, {user: user, deviceId: token.deviceId});
-      });
-
+    token.userId.populate('roleId', function(err, user) {
+      return callback(err, {user: user, deviceId: token.deviceId});
     });
   });
 }
@@ -59,7 +45,13 @@ exports.createToken = function(options, callback) {
   if (options.device) {
     query.deviceId = options.device._id;
   }
-  var update = {token: token, timestamp: new Date()};
+
+  var now = Date.now();
+  var update = {
+    token: token,
+    timestamp: new Date(now),
+    expireAt: new Date(now + 60000)
+  };
   var options = {upsert: true};
   Token.findOneAndUpdate(query, update, options, function(err, newToken) {
     newToken.expirationDate = new Date(newToken.timestamp.getTime() +  tokenExpiration);
