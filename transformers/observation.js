a15d310894cdc8c19f5eef51301e205a2cda6dad
William Newman <newmanw10@gmail.com>
2015-03-05 09:57:46 -0700

    Add event id to observation xform

---
+++ transformers/observation.js
@@ -0,0 +1,15 @@
+var util = require('util');
+
+var transformObservations = function(observations, options) {
+  return observations.map(function(observation) {
+    return observation.toJSON({transform: true, eventId: options.eventId, path: options.path});
+  });
+}
+
+exports.transform = function(observations, options) {
+  options = options || {};
+
+  return util.isArray(observations) ?
+    transformObservations(observations, options) :
+    observations.toJSON({transform: true, eventId: options.eventId, path: options.path});
+}
