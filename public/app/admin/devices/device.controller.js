b0f00c3b21100d035248f0e7f6e6557753ed8c84
William Newman <newmanw10@gmail.com>
2015-08-11 13:29:34 -0600

    Added user logins to device page
    You can filter on user, and date
    
    fixes #104

--- public/app/admin/devices/device.controller.js
+++ public/app/admin/devices/device.controller.js
@@ -2,10 +2,22 @@ angular
   .module('mage')
   .controller('AdminDeviceController', AdminDeviceController);
 
-AdminDeviceController.$inject = ['$scope', '$injector', '$filter', '$routeParams', '$location', 'LocalStorageService', 'DeviceService', 'UserService'];
+AdminDeviceController.$inject = ['$scope', '$injector', '$filter', '$routeParams', '$location', 'LocalStorageService', 'DeviceService', 'UserService', 'LoginService'];
 
-function AdminDeviceController($scope, $injector, $filter, $routeParams, $location, LocalStorageService, DeviceService, UserService) {
+function AdminDeviceController($scope, $injector, $filter, $routeParams, $location, LocalStorageService, DeviceService, UserService, LoginService) {
   $scope.token = LocalStorageService.getToken();
+
+  var filter = {
+    device: {id: $routeParams.deviceId}
+  }
+  $scope.user = {};
+  $scope.login = {
+    startDateOpened: false,
+    endDateOpened: false
+  };
+  var firstLogin = null;
+  $scope.showPrevious = false;
+  $scope.showNext = true;
 
   DeviceService.getDevice($routeParams.deviceId).then(function(device) {
     $scope.device = device.data;
@@ -15,6 +27,13 @@ function AdminDeviceController($scope, $injector, $filter, $routeParams, $locati
     $scope.users = users;
   });
 
+  LoginService.query({filter: filter, limit: $scope.loginResultsLimit}).success(function(loginPage) {
+    $scope.loginPage = loginPage;
+    if (loginPage.logins.length) {
+      firstLogin = loginPage.logins[0];
+    }
+  });
+
   $scope.userIdMap = {};
   $scope.$watch('users', function(users) {
     $scope.userIdMap = _.indexBy(users, 'id');
@@ -22,6 +41,10 @@ function AdminDeviceController($scope, $injector, $filter, $routeParams, $locati
 
   $scope.editDevice = function(device) {
     $location.path('/admin/devices/' + device.id + '/edit');
+  }
+
+  $scope.gotoUser = function(user) {
+    $location.path('/admin/users/' + user.id);
   }
 
   $scope.deleteDevice = function(device) {
@@ -64,4 +87,59 @@ function AdminDeviceController($scope, $injector, $filter, $routeParams, $locati
     }, function(response) {
     });
   }
+
+  $scope.pageLogin = function(url) {
+    LoginService.query({url: url, filter: filter, limit: $scope.loginResultsLimit}).success(function(loginPage) {
+
+      if (loginPage.logins.length) {
+        $scope.loginPage = loginPage;
+        $scope.showNext = loginPage.logins.length != 0;
+        $scope.showPrevious = loginPage.logins[0].id != firstLogin.id;
+      }
+    });
+  }
+
+  $scope.filterLogins = function() {
+    filter.user = $scope.user.selected;
+    filter.startDate = $scope.login.startDate;
+    if ($scope.login.endDate) {
+      filter.endDate = moment($scope.login.endDate).endOf('day').toDate();
+    }
+
+    LoginService.query({filter: filter, limit: $scope.loginResultsLimit}).success(function(loginPage) {
+      $scope.showNext = loginPage.logins.length != 0;
+      $scope.showPrevious = false;
+      $scope.loginPage = loginPage;
+    });
+  }
+
+  $scope.openLoginStart = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.login.startDateOpened = true;
+  }
+
+  $scope.openLoginEnd = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.login.endDateOpened = true;
+  }
+
+  $scope.loginResultsLimitChanged = function() {
+    $scope.filterLogins();
+  }
+
+  $scope.$watch('login.startDate', function(newDate, oldDate) {
+    if (!newDate && !oldDate) return;
+
+    $scope.filterLogins();
+  });
+
+  $scope.$watch('login.endDate', function(newDate, oldDate) {
+    if (!newDate && !oldDate) return;
+
+    $scope.filterLogins();
+  });
 }
