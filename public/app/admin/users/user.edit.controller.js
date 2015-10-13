8e6938ff94f5f067868d5ddbdf2452b18182d427
William Newman <newmanw10@gmail.com>
2015-10-13 17:33:17 -0600

    Consistient feel profile edit and admin user edit

--- public/app/admin/users/user.edit.controller.js
+++ public/app/admin/users/user.edit.controller.js
@@ -40,8 +40,6 @@ function AdminUserEditController($scope, $injector, $filter, $routeParams, $loca
       displayName: $scope.user.displayName,
       email: $scope.user.email,
       phone: $scope.user.phone,
-      p***REMOVED***word: $scope.user.p***REMOVED***word,
-      p***REMOVED***wordconfirm: $scope.user.p***REMOVED***wordconfirm,
       roleId: $scope.user.role.id,
       avatar: $scope.user.avatar,
       icon: $scope.user.icon
@@ -76,5 +74,29 @@ function AdminUserEditController($scope, $injector, $filter, $routeParams, $loca
         });
       }, failure, progress);
     }
+  }
+
+  $scope.updateP***REMOVED***word = function() {
+    if (!$scope.user.p***REMOVED***word) {
+      $scope.p***REMOVED***wordStatus = {status: "error", msg: "p***REMOVED***word cannot be blank"};
+      return;
+    }
+
+    if ($scope.user.p***REMOVED***word != $scope.user.p***REMOVED***wordconfirm) {
+      $scope.p***REMOVED***wordStatus = {status: "error", msg: "p***REMOVED***words do not match"};
+      return;
+    }
+
+    var user = {
+      p***REMOVED***word: $scope.user.p***REMOVED***word,
+      p***REMOVED***wordconfirm: $scope.user.p***REMOVED***wordconfirm
+    }
+
+    UserService.updateUser($scope.user.id, user, function(response) {
+      $location.path('/admin/users/' + $scope.user.id);
+    })
+    .error(function(data, status) {
+      $scope.p***REMOVED***wordStatus = {status: "error", msg: data};
+    });
   }
 }
