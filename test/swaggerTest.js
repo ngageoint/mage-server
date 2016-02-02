9b6fb96afc6eb992e1e68587a6d228f3c07a758c
William Newman <newmanw10@gmail.com>
2016-02-02 12:26:02 -0700

    Added swagger route test

---
+++ test/swaggerTest.js
@@ -0,0 +1,15 @@
+var request = require('supertest')
+  , app = require('../express');
+
+describe("user tests", function() {
+
+  it("should get swagger document", function(done) {
+    request(app)
+      .get('/api/api-docs')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(done);
+  });
+
+});
