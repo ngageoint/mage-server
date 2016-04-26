8bf319da23ae838da56ebd2382f80ec70abb31e8
Syrus Mesdaghi <syrusm@prominentedge.com>
2016-04-26 11:39:07 -0400

    removed animate call where document.find is not resolved

--- public/app/mage/mage.controller.js
+++ public/app/mage/mage.controller.js
@@ -40,8 +40,8 @@ function MageController($scope, $compile, $timeout, $http, $animate, $document, 
   $scope.filteredEvent = FilterService.getEvent();
   $scope.filteredInterval = FilterService.getIntervalChoice().label;
 
-  $animate.on('addClass', $document.find('.feed'), resolveMapAfterFeaturesPaneTransition);
-  $animate.on('removeClass', $document.find('.feed'), resolveMapAfterFeaturesPaneTransition);
+  //$animate.on('addClass', $document.find('.feed'), resolveMapAfterFeaturesPaneTransition);
+  //$animate.on('removeClass', $document.find('.feed'), resolveMapAfterFeaturesPaneTransition);
 
   function resolveMapAfterFeaturesPaneTransition($mapPane, animationPhase) {
     if (animationPhase === 'close') {
