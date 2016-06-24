c74e67b9c9a0be5715fcd5a6c6e94870fae709b7
William Newman <newmanw10@gmail.com>
2016-06-24 17:07:35 -0600

    Rotate avatars
    * Pull in EXIF library to parse exif metadata and rotate images with css

--- public/app/user/user-avatar.directive.js
+++ public/app/user/user-avatar.directive.js
@@ -5,7 +5,7 @@ angular
 function avatarUser() {
   var directive = {
     restrict: "A",
-    templateUrl: '/app/user/user-avatar.directive.html',
+    replace: true,
     scope: {
       user: '=avatarUser',
       avatarWidth: '=?',
@@ -17,29 +17,73 @@ function avatarUser() {
   return directive;
 }
 
-AvatarUserController.$inject = ['$scope', '$element', 'LocalStorageService'];
+AvatarUserController.$inject = ['$scope', '$element', '$http', 'LocalStorageService'];
 
-function AvatarUserController($scope, $element, LocalStorageService) {
+function AvatarUserController($scope, $element, $http, LocalStorageService) {
   if (!$scope.avatarWidth) $scope.avatarWidth = 60;
   if (!$scope.avatarHeight) $scope.avatarHeight = 60;
 
-  $scope.avatar = avatarUrl($scope.user);
+  var image = new Image();
+  var imageElement = $(image);
+  imageElement.addClass('center-crop');
+  imageElement.attr('width', $scope.avatarWidth);
+  imageElement.attr('height', $scope.avatarHeight);
+  $element.replaceWith(image);
 
-  $scope.$watch('user.avatarUrl', function(url) {
-    if (!url || $scope.user.avatarData) return;
+  if ($scope.user && $scope.user.avatarUrl) {
+    var url = $scope.user.avatarUrl + '?_dc=' + $scope.user.lastUpdated;
+    getAvatar(url);
+  } else {
+    image.src = "img/missing_photo.png";
+  }
 
-    $scope.avatar = avatarUrl($scope.user);
+  $scope.$watch('user.avatarUrl', function(newUrl, oldUrl) {
+    if (!newUrl || $scope.user.avatarData || newUrl === oldUrl) return;
+
+    getAvatar(avatarUrl($scope.user, LocalStorageService.getToken()));
   });
 
   $scope.$watch('user.avatarData', function(avatarData) {
-    $scope.avatar = avatarData ? avatarData : avatarUrl($scope.user);
+    if (!avatarData) return;
+    var mimeType = avatarData.split(',')[0].split(':')[1].split(';')[0];
+    var byteString = atob(avatarData.split(',')[1]);
+    var u8arr = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+      u8arr[i] = byteString.charCodeAt(i);
+    }
+    var blob = new Blob([u8arr], {type: mimeType});
+
+    EXIF.getData(blob, function() {
+      var orientation = EXIF.getTag(this, 'Orientation');
+      imageElement.css('transform', orientationMap[orientation] || "");
+      image.src = avatarData;
+    });
   });
 
-  function avatarUrl(user) {
-    if (user && user.avatarUrl) {
-      return user.avatarUrl + "?access_token=" + LocalStorageService.getToken() + '&_dc=' + user.lastUpdated;
-    } else {
-      return "img/missing_photo.png";
-    }
+  function getAvatar(url) {
+    $http.get(url, {responseType: 'arraybuffer'}).then(function(response) {
+      var blob = new Blob([response.data], {type: 'image/jpeg'});
+      EXIF.getData(blob, function() {
+        var orientation = EXIF.getTag(this, 'Orientation');
+        imageElement.css('transform', orientationMap[orientation] || "");
+
+        var urlCreator = window.URL || window.webkitURL;
+        image.src = urlCreator.createObjectURL(blob);
+      });
+    });
+  }
+}
+
+var orientationMap = {
+  3: 'rotate(180deg)',
+  6: 'rotate(90deg)',
+  8: 'rotate(270deg)'
+};
+
+function avatarUrl(user) {
+  if (user && user.avatarUrl) {
+    return user.avatarUrl + '?_dc=' + user.lastUpdated;
+  } else {
+    return "img/missing_photo.png";
   }
 }
