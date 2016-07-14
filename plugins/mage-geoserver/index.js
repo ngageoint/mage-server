78bc7099c09214fb03f426acd1d90c70ffee353a
William Newman <newmanw10@gmail.com>
2016-07-14 12:19:02 -0600

    better

--- plugins/mage-geoserver/index.js
+++ plugins/mage-geoserver/index.js
@@ -16,19 +16,13 @@ var geoserverProxy = proxy(config.geoserver.url, {
     var url = URL.parse(req.url, Boolean('parse query string'));
     delete url.search;
 
-    // console.log('req is', req);
-    console.log('URL is', url);
-    console.log('req query is', req.query);
-    console.log('req query layers', req.query.layers);
-    console.log('req query LAYERS', req.query['LAYERS']);
-
-    var layers = '';
-    var layersMatch = /[Ll][Aa][Yy][Ee][Rr][Ss]=([^&]*)/.exec(url.path);
-    if (layersMatch && layersMatch.length > 1) {
-      layers = layersMatch[1];
-    }
-
     if (!url.query['SLD_BODY']) {
+      var layers = '';
+      var layersMatch = /layers=([^&]*)/i.exec(url.path);
+      if (layersMatch && layersMatch.length > 1) {
+        layers = layersMatch[1];
+      }
+
       url.query.sld = util.format('%s/ogc/sld?layers=%s&access_token=%s', req.getRoot(), layers, config.token);
     }
     return '/geoserver/' + config.geoserver.namespace + '/ows' + URL.parse(URL.format(url)).path;
