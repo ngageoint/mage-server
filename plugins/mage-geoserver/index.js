5f7b564c371eeef41a0657c6c781611ec82b26f0
William Newman <newmanw10@gmail.com>
2016-08-08 14:59:16 -0600

    Use http-proxy-middleware

--- plugins/mage-geoserver/index.js
+++ plugins/mage-geoserver/index.js
@@ -1,39 +1,51 @@
 var express = require('express')
   , passport = require('passport')
   , util = require('util')
-  , proxy = require('express-http-proxy')
+  , proxy = require('http-proxy-middleware')
   , URL = require('url')
   , config = require('./config');
 
 require('./authentication')(passport);
 
-// var urlFilter = new RegExp('^/' + config.token + '/' + config.geoserver.namespace);
-var urlFilter = new RegExp('^/' + config.token + '/(.*)');
-var geoserverProxy = proxy(config.geoserver.url, {
-  filter: function(req) {
-    return urlFilter.test(req.url);
-  },
-  forwardPath: function(req) {
-    var url = URL.parse(urlFilter.exec(req.url)[1], Boolean('parse query string'));
-    console.log('request coming in is', req.url);
-    delete url.search;
+var urlFilter = new RegExp('^/' + config.context + '/' + config.token + '/(.*)');
+function filter(pathname, req) {
+  console.log('req.url: ', req.url);
+  console.log('pathname: ', pathname);
+  console.log('config context', config.context);
+  // console.log('req', req);
+  var filter = urlFilter.exec(pathname);
+  console.log('filter is', filter);
+  return filter;
+}
 
-    var wmsMatch = /service=wms/i.exec(url.path);
-    if (wmsMatch && !url.query['SLD_BODY']) {
-      var layers = '';
-      var layersMatch = /layers=([^&]*)/i.exec(url.path);
-      if (layersMatch && layersMatch.length > 1) {
-        layers = layersMatch[1];
-      }
+function pathRewrite(path, req) {
+  // console.log('request coming in is', req);
+  console.log('path coming in is', path);
 
-      url.query.sld = util.format('%s/ogc/sld?layers=%s&access_token=%s', req.getRoot(), layers, config.token);
+  var url = URL.parse(urlFilter.exec(path)[1], Boolean('parse query string'));
+  delete url.search;
+
+  var wmsMatch = /service=wms/i.exec(url.path);
+  if (wmsMatch && !url.query['SLD_BODY']) {
+    var layers = '';
+    var layersMatch = /layers=([^&]*)/i.exec(url.path);
+    if (layersMatch && layersMatch.length > 1) {
+      layers = layersMatch[1];
     }
 
-    var forward = '/geoserver/' + URL.parse(URL.format(url)).path;
-    console.log('making request', forward);
-
-    return forward;
+    url.query.sld = util.format('%s/ogc/sld?layers=%s&access_token=%s', req.getRoot(), layers, config.token);
   }
+
+  var forward = '/geoserver/' + URL.parse(URL.format(url)).path;
+  console.log('making request', forward);
+
+  return forward;
+}
+
+var geoserverProxy = proxy(filter, {
+  target: config.geoserver.url,
+  pathRewrite: pathRewrite,
+  logLevel: 'debug'
 });
 
 var geoserver = new express.Router();
