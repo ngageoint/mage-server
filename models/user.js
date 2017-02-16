760988460bca63a514ed57d5720d3b3a86757727
William Newman <newmanw10@gmail.com>
2017-02-16 13:59:47 -0700

    Add bulk user import
    * Bulk user create.  From 'Users' admin page click the 'Bulk Import' button to import a csv of users.
    * When deleting a team, you can now delete all users that are part of that team.

--- models/user.js
+++ models/user.js
@@ -97,10 +97,7 @@ UserSchema.pre('save', function(next) {
 
 UserSchema.pre('save', function(next) {
   var user = this;
-  console.log('************rmove token for inactive user');
   if (user.active === false) {
-    console.log('************rmove token for inactive user');
-
     Token.removeTokensForUser(user, function(err) {
       next(err);
     });
