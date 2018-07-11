8e71dc9063504c9ac9ebe0bd3c02faaed13d8de1
William Newman <newmanw10@gmail.com>
2018-07-11 13:20:23 -1000

    Validate lat/lng for geometry fields

--- api/field/geometryField.js
+++ api/field/geometryField.js
@@ -3,6 +3,19 @@ var util = require('util')
   , geoJsonValidator = require('geojson-validation')
   , Field = require('./field');
 
+geoJsonValidator.define('Position', function(position) {
+  let errors = [];
+  if (position[0] < -180 || position[0] > 180) {
+    errors.push('Longitude must be between -180 and 180');
+  }
+
+  if (position[1] < -90 || position[1] > 90) {
+    errors.push('Latitude y must be between -90 and 90');
+  }
+
+  return errors;
+});
+
 function GeometryField(fieldDefinition, form) {
   GeometryField.super_.call(this, fieldDefinition, form[fieldDefinition.name]);
 }
