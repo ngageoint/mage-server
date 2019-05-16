ebd5c146c2d78727eed4d134ec711825059b5855
William Newman <newmanw10@gmail.com>
2019-05-16 08:00:24 -0600

    LDAP authentication

--- public/app/authentication/index.js
+++ public/app/authentication/index.js
@@ -3,6 +3,7 @@ var angular = require('angular');
 angular.module('mage')
   .component('localSignin', require('./local.signin.js'))
   .component('localSignup', require('./local.signup.js'))
+  .component('ldapSignin', require('./ldap.signin.js'))
   .component('oauthSignin', require('./oauth.signin.js'))
   .controller('SigninController', require('./signin.controller'))
   .controller('SignupController', require('./signup.controller'))
