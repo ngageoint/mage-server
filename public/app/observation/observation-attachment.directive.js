a69261cc7cb55698c4a50fdde2af86c8c28df5e1
Dan Barela <daniel.barela@gmail.com>
2019-06-05 10:33:32 -0600

    moving the video error message into the video tag

--- public/app/observation/observation-attachment.directive.js
+++ public/app/observation/observation-attachment.directive.js
@@ -19,20 +19,6 @@ AttachmentController.$inject = ['$scope', '$filter', 'UserService', 'LocalStorag
 function AttachmentController($scope, $filter, UserService, LocalStorageService) {
   $scope.amAdmin = UserService.amAdmin;
   $scope.token = LocalStorageService.getToken();
-  $scope.fullscreen = false;
-
-  $scope.videoAPI = null;
-  $scope.onPlayerReady = function(videoAPI) {
-    $scope.videoAPI = videoAPI;
-  };
-
-  $scope.onVideoError = function() {
-    $scope.videoError = true;
-  };
-
-  $scope.$watch('videoAPI.isFullScreen', function(isFullScreen) {
-    $scope.fullscreen = isFullScreen;
-  });
 
   $scope.deleteAttachment = function () {
     $scope.attachment.markedForDelete = true;
