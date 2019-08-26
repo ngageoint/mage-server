347a4846b232373a8d26a69e8589650eb1d6e336
Dan Barela <daniel.barela@gmail.com>
2019-08-26 09:11:01 -0600

    migrating typeahead component to ES2015

--- public/app/material-components/typeahead.select.component.js
+++ public/app/material-components/typeahead.select.component.js
@@ -1,88 +1,91 @@
-var MDCMenuSurface = require('material-components-web').menuSurface.MDCMenuSurface
-  , MDCTextField = require('material-components-web').textField.MDCTextField;
+import {menuSurface, textField} from 'material-components-web'
 
-module.exports = {
-  template: require('./typeahead.select.component.html'),
-  bindings: {
-    options: '<',
-    initialValue: '<',
-    fieldLabel: '@',
-    idProperty: '@',
-    displayProperty: '@',
-    width: '@',
-    onSelect: '&'
-  },
-  controller: TypeaheadSelectController
-};
+class TypeaheadSelectController {
 
-TypeaheadSelectController.$inject = ['$element', '$timeout'];
+  constructor($element, $timeout) {
+    this._$element = $element;
+    this._$timeout = $timeout;
+    this.teamsUpdated = true;
+    this._menu;
+    this._textField;
+  }
 
-function TypeaheadSelectController($element, $timeout) {
-  var menu;
-  var textField;
-  this.teamsUpdated = true;
-
-  this.$onChanges = function() {
-    $timeout(function() {
+  $onChanges() {
+    this._$timeout(() => {
       this.initializeOptions();
-    }.bind(this))
+    })
   }
 
-  this.$postLink = function() {
-    $timeout(function() {
+  $postLink() {
+    this._$timeout(() => {
       this.initializeTypeahead()
-    }.bind(this))
+    })
   }
-  this.initializeTypeahead = function() {
+
+  initializeTypeahead() {
     this.idPropery = this.idProperty|| 'id';
     this.displayProperty = this.displayProperty || 'name';
-    textField = new MDCTextField($element.find('.mdc-text-field')[0])
-    menu = new MDCMenuSurface($element.find('.mdc-menu-surface')[0])
-    menu.hoistMenuToBody()
+    this._textField = new textField.MDCTextField(this._$element.find('.mdc-text-field')[0])
+    this._menu = new menuSurface.MDCMenuSurface(this._$element.find('.mdc-menu-surface')[0])
+    this._menu.hoistMenuToBody()
     this.initializeOptions()
   }
 
-  this.typeaheadChange = function() {
+  typeaheadChange() {
     this.openDropdown()
-    $element.find('.mdc-text-field')[0].focus()
+    this._$element.find('.mdc-text-field')[0].focus()
     var lowerCaseOption = this.selectedOptionDisplay.toLowerCase()
-    this.sortedOptions = this.options.filter(function(option) {
-      var matches = option[this.displayProperty].indexOf(this.selectedOptionDisplay) !== -1;
-      console.log(`${option[this.displayProperty]} matches ${this.selectedOptionDisplay}: ${matches}`)
-      return option[this.displayProperty].toLowerCase().indexOf(lowerCaseOption) !== -1;
-    }.bind(this))
+    this.sortedOptions = this.options.filter(option => option[this.displayProperty].toLowerCase().indexOf(lowerCaseOption) !== -1)
   }
 
-  this.openDropdown = function() {
-    if (!menu.isOpen()) {
-      menu.open();
+  openDropdown() {
+    if (!this._menu.isOpen()) {
+      this._menu.open();
     }
   }
 
-  this.setSelectedOption = function(option) {
-    menu.close();
+  setSelectedOption(option) {
+    this._menu.close();
     this.selectedOption = option;
     this.selectedOptionDisplay = option.name;
-    textField.value = this.selectedOptionDisplay;
+    this._textField.value = this.selectedOptionDisplay;
     this.onSelect({event: option})
   }
 
-  this.initializeOptions = function() {
-    if (menu && this.options) {
-      this.sortedOptions = this.options.sort(function(a, b) {
+  initializeOptions() {
+    if (this._menu && this.options) {
+      this.sortedOptions = this.options.sort((a, b) => {
         if (a[this.displayProperty] < b[this.displayProperty]) return -1;
         if (a[this.displayProperty] > b[this.displayProperty]) return 1;
         return 0;
-      }.bind(this))
+      })
       if (this.initialValue) {
-        this.setSelectedOption(this.filteredOptions().find(function(option) {
-          return this.initialValue[this.displayProperty] === option[this.displayProperty]
-        }.bind(this)))
+        this.setSelectedOption(this.filteredOptions().find(option => this.initialValue[this.displayProperty] === option[this.displayProperty]))
       }
     }
   }
 
-  this.filteredOptions = function() {
+  filteredOptions() {
     return this.sortedOptions;
   }
+}
+
+TypeaheadSelectController.$inject = ['$element', '$timeout'];
+
+var template = require('./typeahead.select.component.html'),
+bindings = {
+  options: '<',
+  initialValue: '<',
+  fieldLabel: '@',
+  idProperty: '@',
+  displayProperty: '@',
+  width: '@',
+  onSelect: '&'
+},
+controller = TypeaheadSelectController
+
+export {
+  template,
+  bindings,
+  controller
 }
