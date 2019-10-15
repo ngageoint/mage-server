a9bf89757dbde4002e95263d4055990d9ec04566
William Newman <newmanw10@gmail.com>
2019-10-15 12:28:48 -0600

    Rework bulk user import
    * Material styles
    * Update error messages to show as child table rows

--- public/app/admin/users/user.bulk.controller.js
+++ public/app/admin/users/user.bulk.controller.js
@@ -3,22 +3,44 @@ var _ = require('underscore')
 
 module.exports = AdminUserBulkController;
 
-AdminUserBulkController.$inject = ['$scope', '$filter', '$routeParams', '$location', '$q', 'LocalStorageService', 'UserService', 'Team', 'UserIconService'];
+AdminUserBulkController.$inject = ['$scope', '$filter', '$routeParams', '$location', '$q', '$timeout', 'LocalStorageService', 'UserService', 'Team', 'UserIconService'];
 
-function AdminUserBulkController($scope, $filter, $routeParams, $location, $q, LocalStorageService, UserService, Team, UserIconService) {
+function AdminUserBulkController($scope, $filter, $routeParams, $location, $q, $timeout, LocalStorageService, UserService, Team, UserIconService) {
   var requiredFields = ['username', 'displayname', 'password'];
 
   $scope.token = LocalStorageService.getToken();
-  $scope.roles = [];
-  $scope.team = {};
-  $scope.role = {};
+
+  $scope.columnOptions = [{
+    value: 'username',
+    title: 'Username'
+  },{
+    value: 'displayname',
+    title: 'Display Name'
+  },{
+    value: 'email',
+    title: 'Email'
+  },{
+    value: 'phone',
+    title: 'Phone Number'
+  },{
+    value: 'password',
+    title: 'Password'
+  },{
+    value: 'iconInitials',
+    title: 'Icon Initials'
+  },{
+    value: 'iconolor',
+    title: 'Icon Color'
+  }];
+
+  $scope.team = { selected: null };
+  $scope.role = { selected: null };
+
   $scope.edit = {};
-  $scope.import = {};
-  $scope.results = {};
-  $scope.errors = [];
   $scope.columnMap = {};
   $scope.unmappedFields = [];
-  $scope.newUsers = [];
+
+  $scope.someError = {};
 
   UserService.getRoles().success(function (roles) {
     $scope.roles = roles;
@@ -27,6 +49,22 @@ function AdminUserBulkController($scope, $filter, $routeParams, $location, $q, L
   Team.query(function(teams) {
     $scope.teams = _.reject(teams, function(team) { return team.teamEventId; });
   });
+
+  $scope.teamSelected = function(team) {
+    $scope.team.selected = team;
+  };
+
+  $scope.roleSelected = function(role) {
+    $scope.role.selected = role;
+  };
+
+  $scope.teamSelectedForUser = function(team, user) {
+    user.team.selected = team;
+  };
+
+  $scope.roleSelectedForUser = function(role, user) {
+    user.role.selected = role;
+  };
 
   $scope.importFile = function() {
     Papa.parse(event.target.files[0], {
@@ -38,7 +76,9 @@ function AdminUserBulkController($scope, $filter, $routeParams, $location, $q, L
             user.team = {
               selected: $scope.team.selected
             };
-            user.role = $scope.role.selected;
+            user.role = {
+              selected: $scope.role.selected
+            };
           });
 
           mapColumns($scope.columns);
@@ -47,172 +87,158 @@ function AdminUserBulkController($scope, $filter, $routeParams, $location, $q, L
     });
   };
 
-  $scope.numberOfUsersToImport = function() {
-    return $scope.users.length - Object.keys($scope.results).length + $scope.errors.length;
+  $scope.onStatusClosed = function() {
+    $scope.importStatus = null;
   };
 
-  $scope.numberOfUsersImported = function() {
-    return Object.keys($scope.results).length - $scope.errors.length;
-  };
-
-  $scope.mapColumn = function(name, columnIndex) {
-    var mappedKey = _.findKey($scope.columnMap, function(index) {
-      return columnIndex === index;
-    });
-    if (mappedKey) {
-      delete $scope.columnMap[mappedKey];
-    }
-
-    $scope.columnMap[name] = columnIndex;
+  $scope.onColumnOption = function($event, $index) {
+    mapColumn($event.option.value, $index);
     validateMapping();
+  };
+
+  $scope.selected = function($index) {
+    for (let [key, value] of Object.entries($scope.columnMap)) {
+      if (value === $index) return key;
+    }
   };
 
   $scope.import = function () {
     $scope.imported = true;
-
-    var imports = _.map($scope.users, function(user, index) {
-      return {
-        index: index,
-        team: user.team.selected,
-        user: {
-          username: user[$scope.columnMap.username],
-          displayName: user[$scope.columnMap.displayname],
-          email: user[$scope.columnMap.email],
-          phone: user[$scope.columnMap.phone],
-          password: user[$scope.columnMap.password],
-          passwordconfirm: user[$scope.columnMap.password],
-          iconInitials: user[$scope.columnMap.iconInitials],
-          iconColor: user[$scope.columnMap.iconColor],
-          roleId: user.role.id
-        }
-      };
-    });
-
-    createUsers(imports);
+    importUsers($scope.users);
   };
 
-  $scope.retry = function() {
-    var imports = _.map($scope.errors, function(error) {
-      return {
-        index: error.index,
-        team: error.user.team.selected,
-        user: {
-          username: error.user[$scope.columnMap.username],
-          displayName: error.user[$scope.columnMap.displayname],
-          email:error.user[$scope.columnMap.email],
-          phone: error.user[$scope.columnMap.phone],
-          password: error.user[$scope.columnMap.password],
-          passwordconfirm: error.user[$scope.columnMap.password],
-          roleId: error.user.role.id
-        }
-      };
-    });
-
-    createUsers(imports);
-  };
-
-  function createUsers(imports) {
+  function importUsers(users) {
     $scope.importing = true;
-    asyncSeries(imports).then(function() {
+    $scope.edit.row = null;
+    asyncSeries(users).then(function() {
       $scope.importing = false;
 
-      var errors = [];
-      _.each($scope.results, function(result, index) {
-        if (result.status !== 200) {
-          errors.push({
-            index: index,
-            user: $scope.users[index]
-          });
-        }
+      $scope.users = $scope.users.filter(user => {
+        return user.result.status !== 200;
       });
 
-      $scope.errors = errors;
+      if ($scope.users.length) {
+        $scope.importStatus = {
+          message: `${$scope.users.length} users failed to import, please fix errors and try again.`,
+          dismiss: true
+        };
+      } else {
+        $scope.importStatus = {
+          message: `All users successfully imported.`,
+          dismiss: true
+        };
+
+        $scope.columns = null;
+        $scope.team = { selected: null };
+        $scope.role = { selected: null };
+      }
     });
   }
 
-  function importUser(results, user) {
+  function importUser(row) {
     var deferred = $q.defer();
+
+    var user = {
+      username: row[$scope.columnMap.username],
+      displayName: row[$scope.columnMap.displayname],
+      email: row[$scope.columnMap.email],
+      phone: row[$scope.columnMap.phone],
+      password: row[$scope.columnMap.password],
+      passwordconfirm: row[$scope.columnMap.password],
+      iconInitials: row[$scope.columnMap.iconInitials],
+      iconColor: row[$scope.columnMap.iconColor],
+      roleId: row.role.selected.id
+    };
 
     var canvas = document.createElement("canvas");
     canvas.height = 44;
     canvas.width = 44;
 
-    if (user.user.iconInitials && user.user.iconColor && canvas.getContext) {
-      var iconColor = user.user.iconColor;
-      var iconInitials = user.user.iconInitials.substring(0, 2);
+    if (user.iconInitials && user.iconColor && canvas.getContext) {
+      var iconColor = user.iconColor;
+      var iconInitials = user.iconInitials.substring(0, 2);
       UserIconService.drawMarker(canvas, iconColor, iconInitials);
-      user.user.icon = UserIconService.canvasToPng(canvas);
-      user.user.iconMetadata = JSON.stringify({
+      user.icon = UserIconService.canvasToPng(canvas);
+      user.iconMetadata = JSON.stringify({
         type: 'create',
         text: iconInitials,
         color: iconColor
       });
     }
 
-    UserService.createUser(user.user, function(newUser) {
-      Team.addUser({id: user.team.id}, newUser, function() {
-        $scope.results[user.index] = {
-          data: newUser,
+    UserService.createUser(user, function(newUser) {
+      Team.addUser({id: row.team.selected.id}, newUser, function() {
+        row.result = {
           status: 200
         };
 
-        deferred.resolve(results);
+        deferred.resolve();
       }, function(response) {
-        $scope.results[user.index] = {
+        row.result = {
           status: response.status,
-          message: response.responseText
+          message: `Could not create user, ${response.responseText}`
         };
 
-        deferred.resolve(results);
+        deferred.resolve();
       });
     }, function(result) {
-      $scope.results[user.index] = {
+      row.result = {
         status: result.status,
-        message: result.responseText
+        message: `Could not create user, ${result.responseText}`
       };
-      deferred.resolve(results);
+      deferred.resolve();
     });
 
     return deferred.promise;
   }
 
-  function asyncSeries(arr) {
-    return arr.reduce(function (promise, item) {
-      return promise.then(function(result) {
-        return importUser(result, item);
+  function asyncSeries(users) {
+    return users.reduce(function (promise, user) {
+      return promise.then(function() {
+        return importUser(user);
       });
     }, $q.when());
   }
 
-  function mapColumns(columnNames) {
-    var map = {};
+  function mapColumn(column, index) {
+    var mappedKey = _.findKey($scope.columnMap, function(i) {
+      return index === i;
+    });
 
+    if (mappedKey) {
+      delete $scope.columnMap[mappedKey];
+    }
+
+    $scope.columnMap[column] = index;
+  }
+
+  function mapColumns(columnNames) {
     var usernameIndex = _.indexOf(columnNames, 'username');
-    if (usernameIndex !== -1) map.username = usernameIndex;
+    if (usernameIndex !== -1) mapColumn('username', usernameIndex);
 
     var displayNameIndex = _.indexOf(columnNames, 'displayname');
-    if (displayNameIndex !== -1) map.displayname = displayNameIndex;
+    if (displayNameIndex !== -1) mapColumn('displayname', displayNameIndex);
 
     var emailIndex = _.indexOf(columnNames, 'email');
-    if (emailIndex !== -1) map.email = emailIndex;
+    if (emailIndex !== -1) mapColumn('email', emailIndex);
 
     var phoneIndex = _.indexOf(columnNames, 'phone');
-    if (phoneIndex !== -1) map.phone = phoneIndex;
+    if (phoneIndex !== -1) mapColumn('phone', phoneIndex);
 
     var passwordIndex = _.indexOf(columnNames, 'password');
-    if (passwordIndex !== -1) map.password = passwordIndex;
+    if (passwordIndex !== -1) mapColumn('password', passwordIndex);
 
     var iconInitialsIndex = _.indexOf(columnNames, 'iconInitials');
-    if (iconInitialsIndex !== -1) map.iconInitials = iconInitialsIndex;
+    if (iconInitialsIndex !== -1) mapColumn('iconInitials', iconInitialsIndex);
 
     var iconColorIndex = _.indexOf(columnNames, 'iconColor');
-    if (iconColorIndex !== -1) map.iconColor = iconColorIndex;
+    if (iconColorIndex !== -1) mapColumn('iconColor', iconColorIndex);
 
-    $scope.columnMap = map;
     validateMapping();
   }
 
   function validateMapping () {
-    $scope.unmappedFields = _.difference(requiredFields, _.intersection(_.allKeys($scope.columnMap), requiredFields));
+    var fields = _.difference(requiredFields, _.intersection(_.allKeys($scope.columnMap), requiredFields));
+    $scope.unmappedFields = _.filter($scope.columnOptions, field => { return fields.includes(field.value); });
   }
 }
