51254bcb1502113221231030c05bc858395e0184
William Newman <newmanw10@gmail.com>
2020-01-06 08:24:49 -0700

    Upgrade to angularjs/angular hybrid app

---
+++ public/mage-app/src/ng1/authentication/signin.component.js
@@ -0,0 +1,44 @@
+import _ from 'underscore';
+
+class SigninController {
+
+  constructor(UserService, $element) {
+    this._UserService = UserService;
+    this._$element = $element;
+  }
+
+  $onInit() {
+    this.localAuthenticationStrategy = this.api.authenticationStrategies.local;
+    this.thirdPartyStrategies = _.map(_.omit(this.api.authenticationStrategies, this.localStrategyFilter), function(strategy, name) {
+      strategy.name = name;
+      return strategy;
+    });
+  }
+
+  localStrategyFilter(strategy, name) {
+    return name === 'local';
+  }
+
+  localSignin(user) {
+    this.user = user;
+    this.onSignin({user: user.user, strategy: this.localAuthenticationStrategy});
+  }
+}
+
+var template = require('./signin.component.html');
+var bindings = {
+  api: '<',
+  onSignin: '&',
+  onSignup: '&',
+  hideSignup: '<'
+};
+
+SigninController.$inject = ['UserService', '$element'];
+
+var controller = SigninController;
+
+export {
+  template,
+  bindings,
+  controller
+};
