b1a25ddbbd43334f7defd2cf33c33b6811fb33d4
restjohn <restjohn@users.noreply.github.com>
2020-01-29 12:22:27 -0700

    manifold: removed unused module file

--- plugins/mage-manifold/manifold.ts
+++
@@ -1,9 +0,0 @@
-
-import { SourceDescriptor, AdapterDescriptor } from './models';
-
-export interface ManifoldService {
-
-  getAdapters(): AdapterDescriptor[];
-  getSources(): Map<string, SourceDescriptor>;
-  getSource(sourceId: string): SourceDescriptor;
-}
