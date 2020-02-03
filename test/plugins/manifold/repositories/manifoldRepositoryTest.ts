8c0cf2e46b258bd3006b7cb00eaf5ac655115325
restjohn <restjohn@users.noreply.github.com>
2020-02-03 14:04:55 -0700

    manifold: fix repository test hooks

--- test/plugins/manifold/repositories/manifoldRepositoryTest.ts
+++ test/plugins/manifold/repositories/manifoldRepositoryTest.ts
@@ -1,29 +1,169 @@
 
 import mongoose from 'mongoose'
-import mongodb from 'mongodb'
 import { describe, it, before, beforeEach, after, afterEach } from 'mocha'
 import { expect } from 'chai'
 import { MongoMemoryServer } from 'mongodb-memory-server'
-import { AdapterRepository } from '../../../../plugins/mage-manifold/repositories'
+import { AdapterRepository, BaseRepository, EntityReference } from '../../../../plugins/mage-manifold/repositories'
 import { AdapterDescriptor, AdapterDescriptorModel, ManifoldModels, AdapterDescriptorSchema } from '../../../../plugins/mage-manifold/models';
 
 describe.only('manifold repositories', function() {
 
-  describe('adapter repository', async function() {
+  let mongo: MongoMemoryServer
+  let uri: string
+  let conn: mongoose.Connection
+
+  before(async function() {
+    mongo = new MongoMemoryServer()
+    uri = await mongo.getUri()
+  })
+
+  beforeEach(async function() {
+    conn = await mongoose.createConnection(uri, {
+      useMongoClient: true,
+      promiseLibrary: Promise
+    })
+  })
+
+  afterEach(async function() {
+    await conn.close()
+  })
+
+  after(async function() {
+    await mongoose.disconnect()
+    await mongo.stop()
+  })
+
+  describe('base repository', async function() {
+
+    interface BaseValueObject {
+      id?: string
+      derp: string
+      lerp?: string
+      squee?: boolean
+      noo?: Number
+    }
+    type BaseEntity = BaseValueObject & mongoose.Document
+    type BaseModel = mongoose.Model<BaseEntity>
+
+    const collection = 'base'
+    const schema = new mongoose.Schema({
+      derp: { type: String, required: true },
+      lerp: { type: String, required: false },
+      squee: { type: Boolean, required: false, default: false },
+      noo: { type: Number, required: false, default: -1 },
+    })
+    let model: mongoose.Model<BaseEntity>
+    let repo: BaseRepository<BaseEntity, BaseModel, BaseValueObject>
+
+    beforeEach(async function() {
+      model = conn.model('Base', schema, collection)
+      repo = new BaseRepository(model)
+    })
+
+    afterEach(async function() {
+      await model.remove({})
+    })
+
+    it('creates a record', async function() {
+
+      const seed: BaseValueObject = {
+        derp: 'sloo',
+        lerp: 'noich',
+        squee: true,
+        noo: 37
+      }
+      const created = await repo.create({
+        id: 'ignore',
+        ...seed
+      })
+      const read = await conn.db.collection(model.collection.name).find().toArray()
+
+      expect(created.id).to.not.be.empty
+      expect(created.id).to.not.equal('ignore')
+      expect(created).to.deep.include(seed)
+      expect(read.length).to.equal(1)
+      expect(read[0]).to.deep.include(seed)
+    })
+
+    it('reads all records', async function() {
+
+      const seed1: BaseValueObject = {
+        derp: 'bam',
+        lerp: 'plop',
+        squee: true,
+        noo: 11
+      }
+      const seed2: BaseValueObject = {
+        derp: 'sloo',
+        lerp: 'tum',
+        squee: false,
+        noo: 22
+      }
+      await Promise.all([
+        repo.create(seed1),
+        repo.create(seed2)
+      ])
+      const all = await repo.readAll()
+
+      expect(all.length).to.equal(2)
+      expect(all[0]).to.deep.include(seed1)
+      expect(all[1]).to.deep.include(seed2)
+    })
+
+    it('updates a record', async function() {
+
+      const seed: BaseValueObject = {
+        derp: 'spor',
+        lerp: 'jeb',
+        squee: true,
+        noo: 39
+      }
+      const existing = await repo.create(seed)
+      const update: Partial<BaseValueObject> & EntityReference = {
+        id: existing.id,
+        derp: 'sped',
+        lerp: 'jebler',
+        noo: 42
+      }
+      const beforeUpdate = await repo.readAll()
+      const updated = await repo.update(update)
+      const afterUpdate = await repo.readAll()
+
+      expect(updated).to.deep.include(update)
+      expect(beforeUpdate.length).to.equal(1)
+      expect(afterUpdate.length).to.equal(1)
+      expect(beforeUpdate[0]).to.deep.include(seed)
+      expect(afterUpdate[0]).to.deep.include(update)
+      expect(beforeUpdate[0]).to.not.deep.include(update)
+      expect(afterUpdate[0]).to.not.deep.include(seed)
+    })
+
+    it('deletes a record', async function() {
+
+      const seed: BaseValueObject = {
+        derp: 'spor',
+        lerp: 'jeb',
+        squee: true,
+        noo: 39
+      }
+      const created = await repo.create(seed)
+      const beforeDelete = await repo.readAll()
+      await repo.deleteById(created.id)
+      const afterDelete = await repo.readAll()
+
+      expect(beforeDelete.length).to.equal(1)
+      expect(beforeDelete[0]).to.deep.include(seed)
+      expect(afterDelete.length).to.equal(0)
+    })
+  })
+
+  describe('adapter repository', function() {
 
     const collection = 'adapters'
-
-    let db: MongoMemoryServer
-    let conn: mongoose.Connection
     let model: AdapterDescriptorModel
     let repo: AdapterRepository
 
-    before(async function() {
-      db = new MongoMemoryServer()
-      const uri = await db.getUri('adapter_repository')
-      conn = await mongoose.createConnection(uri, {
-        useMongoClient: true
-      })
+    beforeEach(async function() {
       model = conn.model(ManifoldModels.AdapterDescriptor, AdapterDescriptorSchema, collection)
       repo = new AdapterRepository(model)
     })
@@ -32,10 +172,8 @@ describe.only('manifold repositories', function() {
       await model.remove({})
     })
 
-    after(async function() {
-      await conn.close()
-      const stopped = await db.stop()
-      expect(stopped).to.be.true
+    it('does what base repository can do', function() {
+      expect(repo).to.be.instanceOf(BaseRepository)
     })
 
     it('creates an adatper descriptor record', async function() {
