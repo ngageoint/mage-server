901c27d64446e9e26f91199750a6d4846b9b15a3
William Newman <newmanw10@gmail.com>
2020-02-14 17:37:05 -0700

    Observation save/edit sticky header

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -6,17 +6,34 @@ import { UIRouterUpgradeModule } from '@uirouter/angular-hybrid';
 
 import app from '../ng1/app.js';
 import { SwaggerComponent } from './swagger/swagger.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { 
+  MatIcon,
+  MatButton,
+  MatToolbar,
+  MatSpinner,
+  MatIconModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [SwaggerComponent],
   imports: [
     BrowserModule,
     UpgradeModule,
-    UIRouterUpgradeModule.forRoot({ states: [] })
+    UIRouterUpgradeModule.forRoot({ states: [] }),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [],
-  entryComponents:[SwaggerComponent]
+  entryComponents:[MatIcon, MatButton, MatToolbar, MatSpinner, SwaggerComponent]
 })
 export class AppModule { 
   constructor(private upgrade: UpgradeModule) { }
