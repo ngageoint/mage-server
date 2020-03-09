047d965d5649747846442986f4d09c4393a6ba9a
restjohn <restjohn@users.noreply.github.com>
2020-03-09 07:10:11 -0600

    change app.js to typescript

--- service/app.ts
+++ service/app.ts
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose'),
-  waitForMongooseConnection = require('./utilities/waitForMongooseConnection'),
-  fs = require('fs-extra'),
-  environment = require('./environment/env'),
-  log = require('./logger');
+const environment = require('./environment/env')
+const log = require('./logger')
+
+import fs from 'fs-extra'
+import * as mongoose from 'mongoose'
 
 const mongooseLogger = log.loggers.get('mongoose');
 
-mongoose.set('debug', function(collection, method, query, doc, options) {
+mongoose.set('debug', function(this: mongoose.Collection, collection: any, method: any, query: any, doc: any, options: any) {
   mongooseLogger.log('mongoose', "%s.%s(%s, %s, %s)", collection, method, this.$format(query), this.$format(doc), this.$format(options));
 });
 
@@ -16,17 +16,17 @@ log.info('Starting MAGE Server ...');
 
 // Create directory for storing media attachments
 const attachmentBase = environment.attachmentBaseDirectory;
-fs.mkdirp(attachmentBase, function(err) {
+fs.mkdirp(attachmentBase, function(err: any) {
   if (err) {
     log.error("Could not create directory to store MAGE media attachments. "  + err);
     throw err;
   } else {
-    log.info("Using '" + attachmentBase + "' as base directory for feature attachments.");
+    log.info("Using '" + attachmentBase + "' as base directory for observation attachments.");
   }
 });
 
 const iconBase = environment.iconBaseDirectory;
-fs.mkdirp(iconBase, function(err) {
+fs.mkdirp(iconBase, function(err: any) {
   if (err) {
     log.error("Could not create directory to store MAGE icons. "  + err);
   } else {
@@ -37,19 +37,17 @@ fs.mkdirp(iconBase, function(err) {
 require('./migrate').runDatabaseMigrations()
   .then(() => {
     log.info('database initialized; loading plugins ...');
-
     var plugins = require('./plugins');
-    plugins.initialize(app, function(err) {
+    plugins.initialize(app, function(err: any) {
       if (err) {
         throw err;
       }
-
       log.info('opening app for connections ...');
       app.emit('comingOfMage');
     });
 
   })
-  .catch(err => {
+  .catch((err: any) => {
     log.error('error initializing database: ' + err);
     process.exitCode = 1;
   });
