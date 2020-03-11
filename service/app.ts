dcb1b1ce5114a4f16b87e33aba4fbbc558977c95
restjohn <restjohn@users.noreply.github.com>
2020-03-11 09:59:30 -0600

    [service] change app startup to an imperative function call instead of side-effects of require() or import

--- service/app.ts
+++ service/app.ts
@@ -1,49 +1,74 @@
 const environment = require('./environment/env');
 const log = require('./logger');
 
+import http from 'http';
 import fs from 'fs-extra';
 import mongoose from 'mongoose';
 import express from 'express';
 
-const mongooseLogger = log.loggers.get('mongoose');
 
-mongoose.set('debug', function(this: mongoose.Collection, collection: any, method: any, query: any, doc: any, options: any) {
-  mongooseLogger.log('mongoose', "%s.%s(%s, %s, %s)", collection, method, this.$format(query), this.$format(doc), this.$format(options));
-});
+let service: MageService | null = null;
 
-mongoose.Error.messages.general.required = "{PATH} is required.";
-
-log.info('Starting MAGE Server ...');
-
-// Create directory for storing media attachments
-const attachmentBase = environment.attachmentBaseDirectory;
-fs.mkdirp(attachmentBase, function(err: any) {
-  if (err) {
-    log.error("Could not create directory to store MAGE media attachments. "  + err);
-    throw err;
-  } else {
-    log.info("Using '" + attachmentBase + "' as base directory for observation attachments.");
+export type MageService = {
+  app: express.Application,
+  server: http.Server | null,
+  open(): MageService,
+};
+export const MageReadyEvent = 'comingOfMage';
+export const boot = async function(): Promise<MageService> {
+  if (service) {
+    return service;
   }
-});
 
-const iconBase = environment.iconBaseDirectory;
-fs.mkdirp(iconBase, function(err: any) {
-  if (err) {
-    log.error("Could not create directory to store MAGE icons. "  + err);
-  } else {
-    log.info("Using '" + iconBase + "' as base directory for MAGE icons.");
-  }
-});
-
-require('./models').initializeModels();
-require('./migrate').runDatabaseMigrations()
-  .then(() => {
-    app.emit(MageReadyEvent);
-  })
-  .catch((err: any) => {
-    log.error('error initializing database: ' + err);
-    process.exitCode = 1;
+  const mongooseLogger = log.loggers.get('mongoose');
+  mongoose.set('debug', function(this: mongoose.Collection, collection: any, method: any, query: any, doc: any, options: any) {
+    mongooseLogger.log('mongoose', "%s.%s(%s, %s, %s)", collection, method, this.$format(query), this.$format(doc), this.$format(options));
   });
 
-export const MageReadyEvent: string = 'comingOfMage';
-export const app = require('./express.js') as express.Application;
+  mongoose.Error.messages.general.required = "{PATH} is required.";
+
+  log.info('Starting MAGE Server ...');
+
+  // Create directory for storing media attachments
+  const attachmentBase = environment.attachmentBaseDirectory;
+  log.info(`creating attachments directory at ${attachmentBase}`);
+  try {
+    await fs.mkdirp(attachmentBase)
+  }
+  catch (err) {
+    log.error(`error creating attachments directory ${attachmentBase}: `, err);
+    throw err;
+  }
+
+  const iconBase = environment.iconBaseDirectory;
+  log.info(`creating icon directory at ${iconBase}`);
+  try {
+    await fs.mkdirp(iconBase)
+  }
+  catch (err) {
+    log.error(`error creating icon directory ${iconBase}: `, err);
+    throw err;
+  }
+
+  require('./models').initializeModels();
+  try {
+    await require('./migrate').runDatabaseMigrations()
+  }
+  catch (err) {
+    log.error('error initializing database: ' + err);
+    process.exitCode = 1;
+  }
+
+  const app = require('./express.js') as express.Application;
+
+  function open(): MageService {
+    service!.server = app.listen(environment.port, environment.address,
+      () => log.info(`MAGE Server listening at address ${environment.address} on port ${environment.port}`));
+    return service!
+  };
+  return service = {
+    app,
+    open,
+    server: null
+  };
+};
