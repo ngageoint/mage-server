2e4a2f5862de74866175e434f7eb899c09e66b6f
restjohn <restjohn@users.noreply.github.com>
2020-03-11 16:23:53 -0600

    [instance] add web-app to mage express app

--- instance/bin/mage.js
+++ instance/bin/mage.js
@@ -1,4 +1,12 @@
 
+const path = require('path');
+const express = require('express');
 const { boot } = require('@ngageoint/mage.service/lib/app');
 
-boot().then(service => service.open());
+const webappPackage = require.resolve('@ngageoint/mage.web-app/package');
+const webappDir = path.resolve(path.dirname(webappPackage), 'dist');
+
+boot().then(service => {
+  service.app.use(express.static(webappDir));
+  service.open();
+});
