aff8c413b65c639c8654c7bc13f436690875c960
William Newman <newmanw10@gmail.com>
2020-04-13 08:37:10 -0600

    Fix scroll for multiselect components in observation edit

--- web-app/src/app/wrapper/scroll/feed-scroll.component.ts
+++ web-app/src/app/wrapper/scroll/feed-scroll.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'feed-scroll-wrapper',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class ScrollWrapperComponent {
-
+  @Input() mask: boolean;
 }
