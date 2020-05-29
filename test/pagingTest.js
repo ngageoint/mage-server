59d4fdb0044ee0a478cf0cc41bb911e44b422653
Kevin Gilland <kgilland@caci.com>
2020-05-29 05:49:02 -0600

    Adding first test for paging

--- test/pagingTest.js
+++ test/pagingTest.js
@@ -24,15 +24,26 @@ describe("Paging Tests", function () {
         sinon.stub(countQuery, 'count');
         countQuery.count.returns(Promise.resolve(10));
 
+        let user0 = {
+            _id: '0'
+        };
+    
         var query = new mongoose.Query();
-        sinon.stub(query, 'sort');
-        sinon.stub(query, 'limit');
-        sinon.stub(query, 'skip');
-        sinon.stub(query, 'exec');
+        sinon.stub(query, 'sort').returns({
+            limit: sinon.stub().returnsThis(),
+            skip: sinon.stub().returnsThis(),
+            exec: sinon.stub().resolves([user0])
+        });
 
-        let options = {limit: '1'};
-        Paging.pageUsers(countQuery, query, options, callback);
-
-        done();
+        var callback = function (error, users, pageInfo) {
+            expect(error).to.be.null;
+            expect(users).to.not.be.null;
+            expect(users.length).to.equal(1);
+            expect(pageInfo).to.not.be.null;
+            done();
+        };
+        let spy = sinon.spy(callback);
+        let options = { limit: '1' };
+        Paging.pageUsers(countQuery, query, options, spy);
     });
 })
