eec973fc76e2d1dd0bfe6553b6bd4f5533edbfff
William Newman <newmanw10@gmail.com>
2020-07-01 09:30:22 -0600

    Indicate why auth failed on web client

--- web-app/src/ng1/authentication/idp.signin.js
+++ web-app/src/ng1/authentication/idp.signin.js
@@ -13,8 +13,17 @@ class OAuthSigninController {
   signin() {
     this.UserService.idpSignin(this.strategy.name).then(({ user, token }) => {
       if (!token || !user) {
+        let message = 'There was a problem signing in, Please contact a MAGE administrator for assistance.';
+        if (user) {
+          if (!user.active) {
+            message = 'Your account has been created but it is not active. An administrator needs to activate your account before you can log in.';
+          } else if (!user.enabled) {
+            message = 'Your account has been disabled, please contact a MAGE administrator for assistance.'
+          }
+        }
+
         this.statusTitle = 'Signin Failed';
-        this.statusMessage = 'There was a problem signing in, Please contact a MAGE administrator for assistance.';
+        this.statusMessage = message
         this.snackbar.open();
         return;
       }
