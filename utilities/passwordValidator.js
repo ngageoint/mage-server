b4df743c40e4adf49c6c382e790c19cde1155174
Kevin Gilland <kgilland@caci.com>
2020-07-14 08:17:35 -0600

    Adding password policy validator

---
+++ utilities/passwordValidator.js
@@ -0,0 +1,13 @@
+var Setting = require('../models/setting');
+
+function validate(strategy, password) {
+    return Setting.getSetting("security").then(securitySettings => {
+        const passwordPolicy = securitySettings[strategy].passwordPolicy;
+
+        return Promise.resolve(true);
+    });
+}
+
+module.exports = {
+    validate
+}
