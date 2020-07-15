187d21dad9ae7771145894c205bfcea5db33a5a4
Kevin Gilland <kgilland@caci.com>
2020-07-15 05:14:18 -0600

    Adding NPE guard

--- utilities/passwordValidator.js
+++ utilities/passwordValidator.js
@@ -1,6 +1,10 @@
 var Setting = require('../models/setting');
 
 function validate(strategy, password) {
+    if (!strategy || !password) {
+        return Promise.resolve(false);
+    }
+
     return Setting.getSetting("security").then(securitySettings => {
         const passwordPolicy = securitySettings.settings[strategy].passwordPolicy;
 
