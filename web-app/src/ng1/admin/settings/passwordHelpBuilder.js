715b628bc9b40789ab13c61016aeec65bd463e7c
Kevin Gilland <kgilland@caci.com>
2020-07-23 06:28:54 -0600

    Making code a bit more concise

--- web-app/src/ng1/admin/settings/passwordHelpBuilder.js
+++ web-app/src/ng1/admin/settings/passwordHelpBuilder.js
@@ -3,54 +3,57 @@
 function build(passwordPolicy) {
     if (passwordPolicy.customizeHelpText) {
         return;
-      }
-  
-      passwordPolicy.helpText = 'Your password is invalid and must contain '
-  
-      const originalKeys = Object.keys(passwordPolicy);
-      const filtered = originalKeys.filter(function (value, index, arr) {
+    }
+
+    passwordPolicy.helpText = 'Your password is invalid and must contain '
+
+    const originalKeys = Object.keys(passwordPolicy);
+    const filtered = originalKeys.filter(function (value, index, arr) {
         if (value == 'helpText' || value == 'helpTextTemplate' || value == 'customizeHelpText'
-          || value == 'lastNumPass' || value == 'restrictSpecialChars' || value.endsWith('Enabled')) {
-          return false;
+            || value == 'lastNumPass' || value == 'restrictSpecialChars' || value.endsWith('Enabled')) {
+            return false;
         }
         return true;
-      });
-  
-      let commaCount = 0;
-      let passwordText = "";
-      for (let i = 0; i < filtered.length; i++) {
+    });
+
+    let commaCount = 0;
+    let passwordText = "";
+    for (let i = 0; i < filtered.length; i++) {
         const key = filtered[i];
-  
+
         const enabled = passwordPolicy[key + 'Enabled'];
-  
+
         if (enabled) {
-          const value = passwordPolicy[key];
-          const msg = passwordPolicy.helpTextTemplate[key];
-          if (msg) {
-            const subbedMsg = msg.replace('#', value);
-  
-            passwordText += subbedMsg;
-            passwordText += ', ';
-            commaCount++;
-          }
+            const value = passwordPolicy[key];
+            const msg = passwordPolicy.helpTextTemplate[key];
+            if (msg) {
+                const subbedMsg = msg.replace('#', value);
+
+                passwordText += subbedMsg;
+                passwordText += ', ';
+                commaCount++;
+            }
         }
-      }
-  
-      //Remove the last comma.  Tricky to do in the loop since we don't know ahead 
-      //of time if things are enabled.
-      passwordText = passwordText.substring(0, passwordText.length - 2);
-      passwordText += '.';
-  
-      if(commaCount > 1) {
-        const idx = passwordText.lastIndexOf(',');
-        let firstPart = passwordText.slice(0, idx + 1);
-        let lastPart = passwordText.slice(idx + 1, passwordText.length);
-    
-        const andInclude = firstPart + ' and' + lastPart;
-        passwordText = andInclude;
-      }
-  
-      passwordPolicy.helpText += passwordText;
+    }
+
+    passwordPolicy.helpText += fixSentenceStructure(passwordText, commaCount);
+}
+
+function fixSentenceStructure(passwordText, commaCount) {
+    //Remove the last comma.  Tricky to do in the loop since we don't know ahead 
+    //of time if things are enabled.
+    let correctedPasswordText = passwordText.substring(0, passwordText.length - 2);
+    correctedPasswordText += '.';
+
+    if (commaCount > 1) {
+        const idx = correctedPasswordText.lastIndexOf(',');
+        const firstPart = correctedPasswordText.slice(0, idx + 1);
+        const lastPart = correctedPasswordText.slice(idx + 1, correctedPasswordText.length);
+
+        correctedPasswordText = firstPart + ' and' + lastPart;
+    }
+
+    return correctedPasswordText;
 }
 
 module.exports = {
