a4c130cf1e313161bcfa2e1fafd6330803d4d5eb
Kevin Gilland <kgilland@caci.com>
2020-07-28 07:12:07 -0600

    Adding promises for password checking to setup route

--- routes/setup.js
+++ routes/setup.js
@@ -1,41 +1,13 @@
-module.exports = function(app, security) {
+module.exports = function (app, security) {
   var async = require('async')
     , Role = require('../models/role')
     , User = require('../models/user')
     , Device = require('../models/device')
-    , userTransformer = require('../transformers/user');
-
-  var passwordLength = null;
-  var passwordHelp = null;
-
-  function loadPasswordSettings(req, res, next) {
-
-    if (!passwordLength) {
-      Setting.getSetting('security').then((securitySettings = {}) => {
-        const settings = securitySettings.settings || {};
-        passwordLength = Object.keys(security.authentication.strategies).reduce((prev, authName) => {
-          let result = null;
-          if (settings[authName]) {
-            const passwordPolicy = settings[authName].passwordPolicy;
-            if (passwordPolicy) {
-              if(passwordPolicy.passwordMinLengthEnabled){
-                result = passwordPolicy.passwordMinLength;
-              }
-              passwordHelp = passwordPolicy.helpText;
-            }
-          }
-          return result || prev;
-        }, null);
-      }).finally(() => {
-        next();
-      });
-    } else {
-      next();
-    }
-  }
+    , userTransformer = require('../transformers/user')
+    , passwordValidator = require('../utilities/passwordValidator');
 
   function authorizeSetup(req, res, next) {
-    User.count(function(err, count) {
+    User.count(function (err, count) {
       if (err) next(err);
 
       if (count > 0) {
@@ -66,62 +38,63 @@ module.exports = function(app, security) {
       return res.status(400).send('passwords do not match');
     }
 
-    if (password.length < passwordLength) {
-      return res.status(400).send(passwordHelp);
-    }
-
-    var uid = req.param('uid');
-    if (!uid) {
-      return res.send(400).send('passwordconfirm is required');
-    }
-
-    req.user = {
-      username: username,
-      displayName: username,
-      active: true,
-      authentication: {
-        type: 'local',
-        password: password
+    passwordValidator.validate('local', password).then(validationStatus => {
+      if (!validationStatus.isValid) {
+        return res.status(400).send(validationStatus.msg);
       }
-    };
 
-    req.device = {
-      uid: uid,
-      registered: true
-    };
+      var uid = req.param('uid');
+      if (!uid) {
+        return res.send(400).send('passwordconfirm is required');
+      }
 
-    next();
+      req.user = {
+        username: username,
+        displayName: username,
+        active: true,
+        authentication: {
+          type: 'local',
+          password: password
+        }
+      };
+
+      req.device = {
+        uid: uid,
+        registered: true
+      };
+    }).finally(() => {
+      next();
+    });
   }
 
   app.post(
     '/api/setup',
-    loadPasswordSettings,
     authorizeSetup,
     validateSetup,
     function (req, res, next) {
       async.waterfall([
-        function(done) {
-          Role.getRole('ADMIN_ROLE', function(err, role) {
+        function (done) {
+          Role.getRole('ADMIN_ROLE', function (err, role) {
             done(err, role);
           });
         },
-        function(role, done) {
+        function (role, done) {
           req.user.roleId = role._id;
-          User.createUser(req.user, function(err, user) {
+          User.createUser(req.user, function (err, user) {
             done(err, user);
           });
         },
-        function(user, done) {
+        function (user, done) {
           req.device.registered = true;
           req.device.userId = user._id;
           Device.createDevice(req.device)
             .then(device => done(null, user, device))
             .catch(err => done(err));
         }
-      ], function(err, user, device) {
+      ], function (err, user, device) {
         if (err) return next(err);
 
-        res.json({user: userTransformer.transform(user, {path: req.getRoot()}), device: device});
+        res.json({ user: userTransformer.transform(user, { path: req.getRoot() }), device: device });
       });
     }
   );
