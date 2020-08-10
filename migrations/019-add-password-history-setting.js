d5c63e5cff104b23cc9e7f3d0fe655995d97cc4e
Kevin Gilland <kgilland@caci.com>
2020-08-10 07:04:41 -0600

    Doing correct property exists check

--- migrations/019-add-password-history-setting.js
+++ migrations/019-add-password-history-setting.js
@@ -42,7 +42,7 @@ async function addPasswordHistoryToSettings(db) {
             log.debug("Processing " + doc._id);
             strategies.forEach(strategy => {
 
-                if (doc.settings[strategy] && doc.settings[strategy].passwordPolicy) {
+                if (doc.settings.hasOwnProperty(strategy) && doc.settings[strategy].passwordPolicy) {
                     const passwordPolicy = doc.settings[strategy].passwordPolicy;
                     passwordPolicy.passwordHistoryCount = 0;
                     passwordPolicy.passwordHistoryCountEnabled = false;
