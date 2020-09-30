c116b4da6df1fd7564296e6150e52e08a43b1cbe
Kevin Gilland <kgilland@caci.com>
2020-09-30 12:11:40 -0600

    Changing case of STATUS

--- models/exportmetadata.js
+++ models/exportmetadata.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 // Creates a new Mongoose Schema object
 const Schema = mongoose.Schema;
 
-const STATUS = {
+const exportStatusEnum = {
     Starting: 'Starting',
     Running: 'Running',
     Completed: 'Completed',
@@ -25,7 +25,9 @@ const ExportMetadataSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     physicalPath: { type: String },
     exportType: { type: String, required: true },
-    status: { type: String, enum: [STATUS.Starting, STATUS.Running, STATUS.Completed, STATUS.Failed], default: STATUS.Starting },
+    status: { 
+        type: String, 
+        enum: [exportStatusEnum.Starting, exportStatusEnum.Running, exportStatusEnum.Completed, exportStatusEnum.Failed] },
     options: {
         eventId: { type: Number, ref: 'Event', required: true },
         filter: { type: Schema.Types.Mixed }
@@ -40,14 +42,14 @@ const ExportMetadataSchema = new Schema({
 
 const ExportMetadata = mongoose.model('ExportMetadata', ExportMetadataSchema);
 exports.ExportModel = ExportMetadata;
-exports.ExportStatus = STATUS;
+exports.ExportStatus = exportStatusEnum;
 
 exports.createMetadata = function (meta) {
     const newMeta = new ExportMetadata({
         userId: meta.userId,
         physicalPath: meta.physicalPath,
         exportType: meta.exportType,
-        status: STATUS.Starting,
+        status: exportStatusEnum.Starting,
         options: {
             eventId: meta.options.eventId,
             filter: meta.options.filter
