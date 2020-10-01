eaf652781b7122c73a5f81ab32265c18b22c5841
Kevin Gilland <kgilland@caci.com>
2020-10-01 12:10:01 -0600

    Adding more functionality to the file sweeper

--- schedule/export-file-sweeper/index.js
+++ schedule/export-file-sweeper/index.js
@@ -2,10 +2,19 @@
 'use strict';
 
 const log = require('winston')
-  , fs = require('fs')
-  , exportDirectory = require('../../environment/env').exportDirectory;
+    , fs = require('fs')
+    , exportDirectory = require('../../environment/env').exportDirectory;
 
-exports.initialize = function(app, done) {
-    log.info('export-file-sweeper: Initializing job to check ' + exportDirectory + ' for expired export files.')
+function sweep() {
+    log.info('export-file-sweeper: Sweeping ' + exportDirectory);
+
+    //TODO implement
+}
+
+exports.initialize = function (app, done) {
+    log.info('export-file-sweeper: Initializing job to check ' + exportDirectory + ' for expired export files.');
+
+    setInterval(sweep, 5000);
+
     done();
 };
