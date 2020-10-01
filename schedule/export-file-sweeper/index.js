214ff625da804f5806af2ed988f1a591c6d2f111
Kevin Gilland <kgilland@caci.com>
2020-10-01 12:44:03 -0600

    Adding export directory traversal to the sweeper

--- schedule/export-file-sweeper/index.js
+++ schedule/export-file-sweeper/index.js
@@ -1,19 +1,31 @@
 
 'use strict';
 
-const log = require('winston')
+const async = require('async')
+    , log = require('winston')
     , fs = require('fs')
     , exportDirectory = require('../../environment/env').exportDirectory;
 
 function sweep() {
     log.info('export-file-sweeper: Sweeping ' + exportDirectory);
 
-    //TODO implement
+    const files = fs.readdirSync(exportDirectory);
+
+    async.eachSeries(files, function (file, done) {
+        log.info('export-file-sweeper: Checking export file ' + file);
+        //TODO implement
+        done();
+    }, function (err) {
+        if (err) {
+            log.error('export-file-sweeper: Error sweeping ' + exportDirectory, err);
+        }
+    });
 }
 
 exports.initialize = function (app, done) {
     log.info('export-file-sweeper: Initializing job to check ' + exportDirectory + ' for expired export files.');
 
+    //TODO use config value
     setInterval(sweep, 5000);
 
     done();
