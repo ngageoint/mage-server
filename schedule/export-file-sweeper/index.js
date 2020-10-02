05a4901c6523b69b57649b2e1edafe28a5fe147e
Kevin Gilland <kgilland@caci.com>
2020-10-02 07:40:14 -0600

    Adding ttl checks

--- schedule/export-file-sweeper/index.js
+++ schedule/export-file-sweeper/index.js
@@ -10,15 +10,20 @@ const async = require('async')
 function sweep() {
     log.info('export-file-sweeper: Sweeping directory ' + exportDirectory);
 
-    const files = fs.readdirSync(exportDirectory).map(function(file) {
+    const files = fs.readdirSync(exportDirectory).map(function (file) {
         return path.join(exportDirectory, file);
-      });
+    });
 
     async.eachSeries(files, function (file, done) {
-        log.info('export-file-sweeper: Checking export file ' + file);
+        log.debug('export-file-sweeper: Checking export file ' + file);
         const stats = fs.lstatSync(file);
         //TODO delete file if older than some time
-        log.info(stats.ctimeMs);
+        const now = new Date();
+        if (stats.birthtimeMs + (72 * 60 * 60) < now.getMilliseconds()) {
+            log.info('export-file-sweeper: ' + file + ' is too old, and will be deleted');
+        } else {
+            log.debug('export-file-sweeper: ' + file + ' has not expired, and does not need to be deleted');
+        }
         done();
     }, function (err) {
         if (err) {
@@ -30,7 +35,7 @@ function sweep() {
 exports.initialize = function (app, done) {
     log.info('export-file-sweeper: Initializing job to check ' + exportDirectory + ' for expired export files.');
 
-    //TODO use config value
+    //TODO use config value for interval
     setInterval(sweep, 5000);
 
     done();
