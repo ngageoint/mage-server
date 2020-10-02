428666597cac2267a978dc1c33cce3d390d453db
William Newman <newmanw10@gmail.com>
2020-10-02 13:02:20 -0600

    Remove console log

--- routes/layers.js
+++ routes/layers.js
@@ -311,7 +311,6 @@ module.exports = function(app, security) {
     validateEventAccess,
     parseQueryParams,
     function(req, res, next) {
-      console.log('req.parameters.includeUnavailable', req.parameters);
       new api.Layer()
         .getLayers({
           layerIds: req.event.layerIds,
