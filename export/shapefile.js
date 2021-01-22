3c810eed7f0a0a78d65df7bdc6727bb263faec82
Kevin Gilland <kgilland@caci.com>
2021-01-22 10:26:39 -0700

    :Using cursor for locations

--- export/shapefile.js
+++ export/shapefile.js
@@ -16,7 +16,7 @@ function Shapefile(options) {
 util.inherits(Shapefile, Exporter);
 module.exports = Shapefile;
 
-Shapefile.prototype.export = function(stream) {
+Shapefile.prototype.export = function (stream) {
   var self = this;
 
   stream.type('application/zip');
@@ -26,13 +26,13 @@ Shapefile.prototype.export = function(stream) {
   archive.pipe(stream);
 
   async.parallel({
-    events: function(done) {
+    events: function (done) {
       self.observationsToShapefile(archive, done);
     },
-    locations: function(done) {
+    locations: function (done) {
       self.locationsToShapefiles(archive, done);
     }
-  }, function(err) {
+  }, function (err) {
     if (err) {
       log.error('Error streaming shapefile', err);
     }
@@ -41,104 +41,88 @@ Shapefile.prototype.export = function(stream) {
   });
 };
 
-Shapefile.prototype.observationsToShapefile = function(archive, done) {
+Shapefile.prototype.observationsToShapefile = function (archive, done) {
   var self = this;
 
   if (!self._filter.exportObservations) return done(null, []);
 
-  self.requestObservations(self._filter, function(err, observations) {
+  self.requestObservations(self._filter, function (err, observations) {
     if (err) return done(err);
 
     mapObservations.call(self, observations);
 
-    write(observations, function(err, files) {
+    write(observations, function (err, files) {
       if (err) return done(err);
 
-      archive.append(files.shp, {name: 'observations/observations'+files.shapeType+'.shp'});
-      archive.append(files.shx, {name: 'observations/observations'+files.shapeType+'.shx'});
-      archive.append(files.dbf, {name: 'observations/observations'+files.shapeType+'.dbf'});
-      archive.append(files.prj, {name: 'observations/observations'+files.shapeType+'.prj'});
+      archive.append(files.shp, { name: 'observations/observations' + files.shapeType + '.shp' });
+      archive.append(files.shx, { name: 'observations/observations' + files.shapeType + '.shx' });
+      archive.append(files.dbf, { name: 'observations/observations' + files.shapeType + '.dbf' });
+      archive.append(files.prj, { name: 'observations/observations' + files.shapeType + '.prj' });
     }, done);
   });
 };
 
-Shapefile.prototype.locationsToShapefiles = function(archive, done) {
-  var self = this;
+Shapefile.prototype.locationsToShapefiles = function (archive, done) {
+  const self = this;
 
   if (!self._filter.exportLocations) return done(null, []);
 
-  var startDate = self._filter.startDate ? moment(self._filter.startDate) : null;
-  var endDate = self._filter.endDate ? moment(self._filter.endDate) : null;
-  var lastLocationId = null;
+  let startDate = self._filter.startDate ? moment(self._filter.startDate) : null;
+  const endDate = self._filter.endDate ? moment(self._filter.endDate) : null;
 
-  var locations = [];
-  async.doUntil(function(done) {
-    self.requestLocations({startDate: startDate, endDate: endDate, lastLocationId: lastLocationId}, function(err, requestedLocations) {
-      if (err) return done(err);
+  const cursor = self.requestLocations({ startDate: startDate, endDate: endDate, stream: true });
 
-      locations = requestedLocations;
+  let locations = [];
+  cursor.eachAsync(async function (doc, i) {
+    if (self._users[doc.properties.user]) doc.properties.user = self._users[doc.properties.user].username;
+    if (self._users[doc.properties.deviceId]) doc.properties.device = self._users[doc.properties.deviceId].uid;
 
-      log.info('got some locations ' + locations.length);
+    const centroid = turfCentroid(doc);
+    doc.properties.mgrs = mgrs.forward(centroid.geometry.coordinates);
 
-      locations.forEach(function(l) {
-        if (self._users[l.properties.user]) l.properties.user = self._users[l.properties.user].username;
-        if (self._users[l.properties.deviceId]) l.properties.device = self._users[l.properties.deviceId].uid;
+    delete doc.properties.deviceId;
+    locations.push(doc);
+  }).then(() => {
+    cursor.close;
+    const first = locations.slice(0).pop();
+    const last = locations.slice(-1).pop();
+    if (last) {
+      const interval = moment(first.properties.timestamp).toISOString() + '_' + moment(last.properties.timestamp).toISOString();
+      write(locations, function (err, files) {
+        if (err) return done(err);
 
-        const centroid = turfCentroid(l);
-        l.properties.mgrs = mgrs.forward(centroid.geometry.coordinates);
+        archive.append(files.shp, { name: 'locations/' + interval + '.shp' });
+        archive.append(files.shx, { name: 'locations/' + interval + '.shx' });
+        archive.append(files.dbf, { name: 'locations/' + interval + '.dbf' });
+        archive.append(files.prj, { name: 'locations/' + interval + '.prj' });
 
-        delete l.properties.deviceId;
-      });
+        log.info('Successfully wrote ' + locations.length + ' locations to SHAPEFILE');
 
-      var first = locations.slice(0).pop();
-      var last = locations.slice(-1).pop();
-      if (last) {
-        var interval = moment(first.properties.timestamp).toISOString() + '_' + moment(last.properties.timestamp).toISOString();
-        write(locations, function(err, files) {
-          if (err) return done(err);
+        const locationTime = moment(last.properties.timestamp);
+        if (!startDate || startDate.isBefore(locationTime)) {
+          startDate = locationTime;
+        }
 
-          archive.append(files.shp, {name: 'locations/' + interval + '.shp'});
-          archive.append(files.shx, {name: 'locations/' + interval + '.shx'});
-          archive.append(files.dbf, {name: 'locations/' + interval + '.dbf'});
-          archive.append(files.prj, {name: 'locations/' + interval + '.prj'});
-
-          log.info('Successfully wrote ' + locations.length + ' locations to SHAPEFILE');
-
-          var locationTime = moment(last.properties.timestamp);
-          lastLocationId = last._id;
-          if (!startDate || startDate.isBefore(locationTime)) {
-            startDate = locationTime;
-          }
-
-        }, done);
-      } else {
-        done();
-      }
-    });
-  },
-  function() {
-    return locations.length === 0;
-  },
-  function(err) {
-    log.info('done writing all locations for to SHAPEFILE', err);
-
-    done(err);
+      }, done);
+    } else {
+      done();
+    }
   });
 };
 
 function write(geojson, callback, doneCallback) {
-  var gj = {
+  const gj = {
     features: geojson
   };
   [shpgeojson.polygon(gj), shpgeojson.point(gj), shpgeojson.line(gj)]
-    .forEach(function(l) {
+    .forEach(function (l) {
       console.log('type', l.type);
       console.log('geometries', l.geometries);
       console.log('properties', l.properties);
       if (l.geometries.length && l.geometries[0].length) {
         console.log('write type', l.type);
         shpwrite.write(l.properties, l.type, l.geometries,
-          function(err, files) {
+          function (err, files) {
             callback(err, {
               shapeType: l.type,
               shp: new Buffer(new Uint8Array(files.shp.buffer)),
@@ -162,7 +146,7 @@ function mapObservationProperties(observation) {
   const centroid = turfCentroid(observation);
   observation.properties.mgrs = mgrs.forward(centroid.geometry.coordinates);
 
-  observation.properties.forms.forEach(function(observationForm) {
+  observation.properties.forms.forEach(function (observationForm) {
     if (Object.keys(observationForm).length === 0) return;
 
     var form = event.formMap[observationForm.formId];
@@ -187,7 +171,7 @@ function mapObservations(observations) {
 
   if (!Array.isArray(observations)) observations = [observations];
 
-  observations.forEach(function(o) {
+  observations.forEach(function (o) {
     mapObservationProperties.call(self, o);
 
     if (self._users[o.userId]) o.properties.user = self._users[o.userId].username;
