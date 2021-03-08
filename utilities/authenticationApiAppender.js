6584a0b8cd874d119ff6216c8bcaba700e933223
Kevin Gilland <kgilland@caci.com>
2021-03-08 04:38:52 -0700

    Changing to an async function to help with readability

--- utilities/authenticationApiAppender.js
+++ utilities/authenticationApiAppender.js
@@ -14,20 +14,17 @@ module.exports = {
  * @param {*} api 
  * @param {*} options 
  */
-function append(api, options) {
+async function append(api, options) {
     options = options || {};
 
     const apiCopy = extend({}, api);
     delete apiCopy.authenticationStrategies;
     apiCopy.authenticationStrategies = {};
 
-    return Authentication.Model.find().then(authenticationStrategies => {
-        const transformedStrategies = AuthenticationTransformer.transform(authenticationStrategies, options);
-
-        transformedStrategies.forEach(function (strategy) {
-            apiCopy.authenticationStrategies[strategy.type] = extend({}, strategy);
-        });
-
-        return Promise.resolve(apiCopy);
+    const authenticationStrategies = await Authentication.Model.find();
+    const transformedStrategies = AuthenticationTransformer.transform(authenticationStrategies, options);
+    transformedStrategies.forEach(function (strategy) {
+        apiCopy.authenticationStrategies[strategy.type] = extend({}, strategy);
     });
+    return Promise.resolve(apiCopy);
 }
