b97bafcd54bc4455716ff81d458dd43029d96f78
Kevin Gilland <kgilland@caci.com>
2021-03-31 07:51:50 -0600

    Adding more appender tests

--- test/utilities/authenticationApiAppenderTest.js
+++ test/utilities/authenticationApiAppenderTest.js
@@ -14,6 +14,33 @@ describe("Authentication API Appender Tests", function () {
         sinon.restore();
     });
 
+    it('Test append is a copy', function (done) {
+        const api = {
+            name: 'testApi',
+            description: 'Test description',
+            authenticationStrategies: {
+                fakeAuth: {
+                    username: 'test',
+                    password: 'test'
+                }
+            }
+        };
+
+        const models = [];
+
+        sinon.mock(AuthenticationConfiguration.Model)
+            .expects('find')
+            .resolves(models);
+
+        AuthenticationApiAppender.append(api).then(appendedApi => {
+            expect(appendedApi).to.not.equal(api);
+
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
+
     it('Test append', function (done) {
         const api = {
             name: 'testApi',
@@ -52,7 +79,32 @@ describe("Authentication API Appender Tests", function () {
             done();
         }).catch(err => {
             done(err);
-        })
+        });
+    });
+
+    it('Test append with no strategies', function (done) {
+        const api = {
+            name: 'testApi',
+            description: 'Test description',
+            authenticationStrategies: {
+            }
+        };
+
+        const models = [];
+
+        sinon.mock(AuthenticationConfiguration.Model)
+            .expects('find')
+            .resolves(models);
+
+        AuthenticationApiAppender.append(api).then(appendedApi => {
+            expect(appendedApi.name).to.equal(api.name);
+            expect(appendedApi.description).to.equal(api.description);
+            expect(Object.keys(appendedApi.authenticationStrategies).length).to.equal(0);
+
+            done();
+        }).catch(err => {
+            done(err);
+        });
     });
 
 });
