cf0c83cd023bd84d23bc5d146a618acf30186461
Kevin Gilland <kgilland@caci.com>
2021-04-15 08:54:52 -0600

    Removing unused files and moving model out of component

--- web-app/src/app/admin/admin-settings/authentication-settings/generic-settings/generic-settings.component.ts
+++ web-app/src/app/admin/admin-settings/authentication-settings/generic-settings/generic-settings.component.ts
@@ -1,9 +1,5 @@
 import { OnInit, Component, Input } from '@angular/core';
-
-export interface GenericSetting {
-    key: string,
-    value: string
-}
+import { GenericSetting } from './generic-settings.model';
 
 @Component({
     selector: 'generic-settings',
@@ -14,17 +10,26 @@ export class GenericSettingsComponent implements OnInit {
     @Input() strategy: any;
     dataSource: GenericSetting[] = [];
     displayedColumns: string[] = ['key', 'value'];
+    settingsKeysToIgnore: string[] = ['accountLock', 'devicesReqAdmin', 'usersReqAdmin', 'passwordPolicy', 'newUserTeams', 'newUserEvents'];
 
     ngOnInit(): void {
         for (const [key, value] of Object.entries(this.strategy.settings)) {
 
-            if (key === 'accountLock' || key === 'devicesReqAdmin' || key === 'usersReqAdmin'
-                || key === 'passwordPolicy' || key === 'newUserTeams' || key === 'newUserEvents') {
+            if (this.settingsKeysToIgnore.includes(key)) {
                 continue;
             }
+
+            let castedValue: string;
+
+            if(value instanceof String){
+                castedValue = value as string;
+            } else {
+                castedValue = JSON.stringify(value);
+            }
+
             const gs: GenericSetting = {
                 key: key,
-                value: value as string
+                value: castedValue
             };
             this.dataSource.push(gs);
         }
---
+++ web-app/src/app/admin/admin-settings/authentication-settings/generic-settings/generic-settings.model.ts
@@ -0,0 +1,4 @@
+export interface GenericSetting {
+    key: string,
+    value: string
+}
