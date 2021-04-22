34f61eeb6626bf4ad6d4edec030931b88af28407
Kevin Gilland <kgilland@caci.com>
2021-04-22 08:26:33 -0600

    Forgot to add these files to git in the previous push.

---
+++ web-app/src/app/admin/admin-settings/authentication-create/authentication-create.component.ts
@@ -0,0 +1,48 @@
+import { Component, Inject } from '@angular/core'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+@Component({
+    selector: 'authentication-create',
+    templateUrl: './authentication-create.component.html',
+    styleUrls: ['./authentication-create.component.scss']
+})
+export class AuthenticationCreateComponent {
+
+    readonly types: any[] = [{
+        title: 'Local',
+        description: 'Local account.',
+        value: 'local'
+    }, {
+        title: 'OAuth',
+        description: 'OAuth account.',
+        value: 'oauth'
+    }, {
+        title: 'LDAP',
+        description: 'LDAP account.',
+        value: 'ldap'
+    }, {
+        title: 'Login-gov',
+        description: 'Login-gov account.',
+        value: 'login-gov'
+    }, {
+        title: 'SAML',
+        description: 'SAML account.',
+        value: 'saml'
+    }];
+
+    constructor(
+        public dialogRef: MatDialogRef<AuthenticationCreateComponent>,
+        @Inject(MAT_DIALOG_DATA) public strategy: any) {
+
+    }
+
+    close(): void {
+        this.dialogRef.close('cancel');
+    }
+
+    delete(): void {
+        this.dialogRef.close('create');
+    }
+
+}
