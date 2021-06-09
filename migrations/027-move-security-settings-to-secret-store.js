ed8bcb061f50cf2d368b34d14e07bc69defcffaf
Kevin Gilland <kgilland@caci.com>
2021-06-09 11:53:53 -0600

    Migrating auth config security settings to file store

---
+++ migrations/027-move-security-settings-to-secret-store.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const log = require('winston')
+    , SecretStoreService = require('../security/secret-store-service');
+
+exports.id = 'move-security-settings-to-secret-store';
+
+function moveSecuritySettings(settings) {
+    const data = {};
+    Object.keys(settings).forEach(key => {
+        switch (key.toLowerCase()) {
+            case 'clientid':
+            case 'clientsecret':
+            case 'client_id':
+                data[key] = settings[key];
+                delete settings[key];
+                break;
+            default:
+                break;
+        }
+    });
+    return data;
+}
+
+exports.up = async function (done) {
+
+    const authenticationConfigurationsCollection = await this.db.collection('authenticationconfigurations');
+    const cursor = authenticationConfigurationsCollection.find();
+
+    let hasNext = true;
+    while (hasNext === true) {
+        hasNext = await cursor.hasNext()
+        if (hasNext !== true) break;
+
+        const authConfig = await cursor.next();
+
+        if (authConfig.settings) {
+            const data = moveSecuritySettings(authConfig.settings);
+
+            if (Object.keys(data).length > 0) {
+                log.info('Moving security settings for auth config ' + authConfig.name);
+
+                const store = new SecretStoreService();
+                try {
+                    await store.write(authConfig._id.toString(), JSON.stringify(data));
+                    await authenticationConfigurationsCollection.updateOne({ _id: authConfig._id }, authConfig);
+                } catch (err) {
+                    log.warn(err);
+                    return done(err);
+                }
+            }
+        } else {
+            log.info('No settings found for ' + authConfig.name);
+        }
+    }
+
+    done();
+
+};
+
+exports.down = function (done) {
+    done();
+}
