f81e34b6f931265d54dc3172cc1717355828eacd
Kevin Gilland <kgilland@caci.com>
2021-06-15 10:07:03 -0600

    Adding more tests

--- test/security/providers/file-system-secret-store-test.js
+++ test/security/providers/file-system-secret-store-test.js
@@ -4,6 +4,7 @@ const chai = require('chai')
     , sinon = require('sinon')
     , sinonChai = require('sinon-chai')
     , expect = require("chai").expect
+    , fs = require('fs')
     , FileSystemSecretStore = require("../../../security/providers/file-system-secret-store")
     , ReadCommand = require("../../../security/commands/read-command")
     , DeleteCommand = require("../../../security/commands/delete-command");
@@ -19,12 +20,52 @@ describe("File System Secret Store Tests", function () {
         const fsStore = new FileSystemSecretStore();
         const command = new ReadCommand(dataId);
 
+        sinon.stub(fs, 'existsSync').returns(false);
+
         fsStore.send(command).then(response => {
             expect(response.status).to.be.false;
             expect(response.data).to.be.null;
             done();
         }).catch(err => {
             done(err);
+        });
+    });
+
+    it('Test read data', function (done) {
+        const dataId = '12345';
+        const fsStore = new FileSystemSecretStore();
+        const command = new ReadCommand(dataId);
+
+        const data = {
+            clientId: '0000'
+        };
+
+        sinon.stub(fs, 'existsSync').returns(true);
+        sinon.stub(fs, 'accessSync').returns(undefined);
+        sinon.stub(fs, 'readFileSync').returns(JSON.stringify(data));
+
+        fsStore.send(command).then(response => {
+            expect(response.data).to.not.be.null;
+            expect(response.data.clientId).to.not.be.null;
+            expect(response.data.clientId).to.equal(data.clientId);
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
+
+    it('Test read data, wrong permissions', function (done) {
+        const dataId = '12345';
+        const fsStore = new FileSystemSecretStore();
+        const command = new ReadCommand(dataId);
+
+        sinon.stub(fs, 'existsSync').returns(true);
+        sinon.stub(fs, 'accessSync').throws(new Error('Incorrect permissions'));
+
+        fsStore.send(command).then(() => {
+            done('An error should be thrown');
+        }).catch(err => {
+            done();
         });
     });
 
@@ -38,6 +79,21 @@ describe("File System Secret Store Tests", function () {
             done();
         }).catch(err => {
             done(err);
+        });
+    });
+
+    it('Test delete data, wrong permissions', function (done) {
+        const dataId = '12345';
+        const fsStore = new FileSystemSecretStore();
+        const command = new DeleteCommand(dataId);
+
+        sinon.stub(fs, 'existsSync').returns(true);
+        sinon.stub(fs, 'accessSync').throws(new Error('Incorrect permissions'));
+
+        fsStore.send(command).then(() => {
+            done('An error should be thrown');
+        }).catch(err => {
+            done();
         });
     });
 });
