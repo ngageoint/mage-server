a1954019a7119757e6a40a6ba4a809db7f846ed7
Kevin Gilland <kgilland@caci.com>
2021-07-07 05:50:35 -0600

    Adding skeleton for displaying affected users on auth delete.

--- web-app/src/app/admin/admin-settings/authentication-delete/authentication-delete.component.ts
+++ web-app/src/app/admin/admin-settings/authentication-delete/authentication-delete.component.ts
@@ -1,5 +1,8 @@
-import { Component, Inject } from '@angular/core'
+import { AfterViewInit, Component, Inject, ViewChild } from '@angular/core'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Strategy } from '../admin-settings.model';
 import { AuthenticationConfigurationService } from 'src/app/upgrade/ajs-upgraded-providers';
 
@@ -8,12 +11,34 @@ import { AuthenticationConfigurationService } from 'src/app/upgrade/ajs-upgraded
   templateUrl: './authentication-delete.component.html',
   styleUrls: ['./authentication-delete.component.scss']
 })
-export class AuthenticationDeleteComponent {
+export class AuthenticationDeleteComponent implements AfterViewInit {
+  displayedColumns: string[] = ['name', 'email'];
+  dataSource: MatTableDataSource<string>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(
     public dialogRef: MatDialogRef<AuthenticationDeleteComponent>,
     @Inject(MAT_DIALOG_DATA) public strategy: Strategy,
     @Inject(AuthenticationConfigurationService)
     private authenticationConfigurationService: any) {
+
+    this.dataSource = new MatTableDataSource([]);
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   close(): void {
@@ -23,7 +48,7 @@ export class AuthenticationDeleteComponent {
   delete(): void {
     this.authenticationConfigurationService.deleteConfiguration(this.strategy).then(() => {
       this.dialogRef.close('delete');
-    }).catch(err => {
+    }).catch((err: any) => {
       console.error(err);
       this.dialogRef.close('cancel');
     });
