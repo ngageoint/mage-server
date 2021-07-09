fdacf1e643b6123fa11f6dd196cc4a6c7cea1144
Kevin Gilland <kgilland@caci.com>
2021-07-09 06:24:11 -0600

    Moving "common-ish" code to an oauth class

--- authentication/google.js
+++ authentication/google.js
@@ -1,13 +1,11 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy
   , User = require('../models/user')
   , Role = require('../models/role')
-  , TokenAssertion = require('./verification').TokenAssertion
   , api = require('../api')
   , log = require('../logger')
-  , userTransformer = require('../transformers/user')
   , AuthenticationInitializer = require('./index')
-  , authenticationApiAppender = require('../utilities/authenticationApiAppender')
-  , SecurePropertyAppender = require('../security/utilities/secure-property-appender');
+  , SecurePropertyAppender = require('../security/utilities/secure-property-appender')
+  , OAuth = require('./oauth'); 
 
 function doConfigure(strategyConfig) {
   log.info('Configuring ' + strategyConfig.title + ' authentication');
@@ -70,141 +68,13 @@ function doConfigure(strategyConfig) {
 }
 
 function initialize(config) {
-  const app = AuthenticationInitializer.app;
-  const passport = AuthenticationInitializer.passport;
-  const provision = AuthenticationInitializer.provision;
-  const tokenService = AuthenticationInitializer.tokenService;
-
-  function parseLoginMetadata(req, res, next) {
-    const options = {};
-    options.userAgent = req.headers['user-agent'];
-    options.appVersion = req.param('appVersion');
-
-    req.loginOptions = options;
-    next();
-  }
-
-  function authenticate(req, res, next) {
-    passport.authenticate(config.name, function (err, user, info = {}) {
-      if (err) return next(err);
-
-      req.user = user;
-
-      // For inactive or disabled accounts don't generate an authorization token
-      if (!user.active || !user.enabled) {
-        log.warn('Failed user login attempt: User ' + user.username + ' account is inactive or disabled.');
-        return next();
-      }
-
-      if (!user.authentication.authenticationConfigurationId) {
-        log.warn('Failed user login attempt: ' + user.authentication.type + ' is not configured');
-        return next();
-      }
-
-      if (!user.authentication.authenticationConfiguration.enabled) {
-        log.warn('Failed user login attempt: Authentication ' + user.authentication.authenticationConfiguration.title + ' is disabled.');
-        return next();
-      }
-
-      // DEPRECATED session authorization, remove req.login which creates session in next version
-      req.login(user, function (err) {
-        tokenService.generateToken(user._id.toString(), TokenAssertion.Authorized, 60 * 5)
-          .then(token => {
-            req.token = token;
-            req.user = user;
-            req.info = info;
-            next();
-          }).catch(err => {
-            next(err);
-          });
-      });
-    })(req, res, next);
-  }
+  OAuth.initialize(config);
 
   SecurePropertyAppender.appendToConfig(config).then(appendedConfig => {
     doConfigure(appendedConfig);
   }).catch(err => {
     log.error(err);
   });
-
-  app.get(
-    '/auth/' + config.name + '/signin',
-    function (req, res, next) {
-      passport.authenticate(config.name, {
-        scope: ['profile', 'email', 'openid'],
-        state: req.query.state
-      })(req, res, next);
-    }
-  );
-
-  // DEPRECATED session authorization, remove in next version.
-  app.post(
-    '/auth/' + config.name + '/authorize',
-    function (req, res, next) {
-      if (req.user) {
-        log.warn('session authorization is deprecated, please use jwt');
-        return next();
-      }
-
-      passport.authenticate('authorization', function (err, user, info = {}) {
-        if (!user) return res.status(401).send(info.message);
-
-        req.user = user;
-        next();
-      })(req, res, next);
-    },
-    provision.check(config.name),
-    parseLoginMetadata,
-    function (req, res, next) {
-      new api.User().login(req.user, req.provisionedDevice, req.loginOptions, function (err, token) {
-        if (err) return next(err);
-
-        authenticationApiAppender.append(config.api).then(apiCopy => {
-          const api = Object.assign({}, apiCopy);
-          api.authenticationStrategies[config.name] = {
-            url: api.authenticationStrategies[config.name].url,
-            type: api.authenticationStrategies[config.name].type,
-            title: api.authenticationStrategies[config.name].title,
-            textColor: api.authenticationStrategies[config.name].textColor,
-            buttonColor: api.authenticationStrategies[config.name].buttonColor,
-            icon: api.authenticationStrategies[config.name].icon
-          };
-
-          res.json({
-            token: token.token,
-            expirationDate: token.expirationDate,
-            user: userTransformer.transform(req.user, { path: req.getRoot() }),
-            device: req.provisionedDevice,
-            api: api
-          });
-        }).catch(err => {
-          next(err);
-        });
-      });
-
-      req.session = null;
-    }
-  );
-
-  app.get(
-    '/auth/' + config.name + '/callback',
-    authenticate,
-    function (req, res) {
-      if (req.query.state === 'mobile') {
-        let uri;
-        if (!req.user.active || !req.user.enabled) {
-          uri = `mage://app/invalid_account?active=${req.user.active}&enabled=${req.user.enabled}`;
-        } else {
-          uri = `mage://app/authentication?token=${req.token}`
-        }
-
-        res.redirect(uri);
-      } else {
-        res.render('authentication', { host: req.getRoot(), login: { token: req.token, user: req.user } });
-      }
-    }
-  );
-
 };
 
 module.exports = {
