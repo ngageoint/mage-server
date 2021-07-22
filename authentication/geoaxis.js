1ef6d59531ea20e66d6d119b52e1cf965ed99e28
Kevin Gilland <kgilland@caci.com>
2021-07-22 11:14:12 -0600

    Using uid instead of mail

--- authentication/geoaxis.js
+++ authentication/geoaxis.js
@@ -23,7 +23,7 @@ function doConfigure(strategyConfig) {
     function (req, accessToken, refreshToken, profile, done) {
       const geoaxisUser = profile._json;
       log.info('Processing geoaxis profile ' + JSON.stringify(geoaxisUser));
-      User.getUserByAuthenticationStrategy('oauth', geoaxisUser.mail, function (err, user) {
+      User.getUserByAuthenticationStrategy('oauth', geoaxisUser.uid, function (err, user) {
         if (err) return done(err);
 
         const email = geoaxisUser.mail;
