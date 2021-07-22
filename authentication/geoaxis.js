dcaa78a6d681181c82a5113c92b2c7f31996b601
Kevin Gilland <kgilland@caci.com>
2021-07-22 11:17:59 -0600

    Adding more logging

--- authentication/geoaxis.js
+++ authentication/geoaxis.js
@@ -23,8 +23,10 @@ function doConfigure(strategyConfig) {
     function (req, accessToken, refreshToken, profile, done) {
       const geoaxisUser = profile._json;
       log.info('Processing geoaxis profile ' + JSON.stringify(geoaxisUser));
-      User.getUserByAuthenticationStrategy('oauth', geoaxisUser.uid, function (err, user) {
+      User.getUserByAuthenticationStrategy('oauth', geoaxisUser.mail, function (err, user) {
         if (err) return done(err);
+
+        log.info('Located geoaxis user ' + JSON.stringify(user));
 
         const email = geoaxisUser.mail;
 
