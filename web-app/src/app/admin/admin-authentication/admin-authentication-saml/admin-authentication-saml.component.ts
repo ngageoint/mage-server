231bcf5dbd3d611e4e10a068576eb2762be1689f
Kevin Gilland <kgilland@caci.com>
2021-09-30 10:12:53 -0600

    Removing non-user configured fields.

--- web-app/src/app/admin/admin-authentication/admin-authentication-saml/admin-authentication-saml.component.ts
+++ web-app/src/app/admin/admin-authentication/admin-authentication-saml/admin-authentication-saml.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Strategy } from '../../admin-settings/admin-settings.model';
 
@@ -7,19 +7,22 @@ import { Strategy } from '../../admin-settings/admin-settings.model';
   templateUrl: './admin-authentication-saml.component.html',
   styleUrls: ['./admin-authentication-saml.component.scss']
 })
-export class AdminAuthenticationSAMLComponent implements OnChanges {
+export class AdminAuthenticationSAMLComponent implements OnInit, OnChanges {
 
   @Input() strategy: Strategy
   @Input() editable = true
 
   formGroup = new FormGroup({})
 
+  ngOnInit(): void {
+    this.strategy.settings.uidAttribute = "uid";
+    this.strategy.settings.displayNameAttribute = "email";
+    this.strategy.settings.emailAttribute = "email";
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.strategy) {
       this.formGroup = new FormGroup({
-        uidAttribute: new FormControl(this.strategy.settings.uidAttribute, Validators.required),
-        displayNameAttribute: new FormControl(this.strategy.settings.displayNameAttribute, Validators.required),
-        emailAttribute: new FormControl(this.strategy.settings.emailAttribute, Validators.required),
         issuer: new FormControl(this.strategy.settings.options.issuer, Validators.required),
         entryPoint: new FormControl(this.strategy.settings.options.entryPoint, Validators.required)
       })
