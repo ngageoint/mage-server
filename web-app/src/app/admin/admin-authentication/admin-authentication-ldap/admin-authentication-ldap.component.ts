2fdeb99c52954bf0aaa24c6375942344df824076
Kevin Gilland <kgilland@caci.com>
2021-09-30 07:29:23 -0600

    Adding fields based on the current ldap model.  Missing descriptions.

--- web-app/src/app/admin/admin-authentication/admin-authentication-ldap/admin-authentication-ldap.component.ts
+++ web-app/src/app/admin/admin-authentication/admin-authentication-ldap/admin-authentication-ldap.component.ts
@@ -14,10 +14,19 @@ export class AdminAuthenticationLDAPComponent implements OnChanges {
 
   formGroup = new FormGroup({})
 
+  //LDAP RFC: https://datatracker.ietf.org/doc/html/rfc4510
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.strategy) {
       this.formGroup = new FormGroup({
-      
+        //https://datatracker.ietf.org/doc/html/rfc4516
+        url: new FormControl(this.strategy.settings.url, Validators.required),
+        bindDN: new FormControl(this.strategy.settings.bindDN, Validators.required),
+        bindCredentials: new FormControl(this.strategy.settings.bindCredentials, Validators.required),
+        searchBase: new FormControl(this.strategy.settings.searchBase, Validators.required),
+        searchFilter: new FormControl(this.strategy.settings.searchFilter, Validators.required),
+        usernameField: new FormControl(this.strategy.settings.ldapUsernameField, Validators.required),
+        displayNameField: new FormControl(this.strategy.settings.ldapDisplayNameField, Validators.required),
+        emailField: new FormControl(this.strategy.settings.ldapEmailField, Validators.required)
       })
     }
   }
