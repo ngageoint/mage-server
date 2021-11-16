ab62b0ad2751508016c57b6b19123bc25816fd5c
Kevin Gilland <kgilland@caci.com>
2021-11-16 11:52:35 -0700

    Taking forever to get most (not all) the tests working

--- service/functionalTests/httpTest.js
+++ service/functionalTests/httpTest.js
@@ -1,45 +1,45 @@
-var expect = require("chai").expect
- , request = require("request")
- , record = require('./record')
- , config = require('./config/httpconfig.json');
+const expect = require("chai").expect
+  , request = require("request")
+  , record = require('./record')
+  , config = require('./config/httpconfig.json');
 
 // --------- Make some HTTP requests
-  // Before: create a token.  Optional - record http responses
-  // After: Recorder cleanup
-  // Verify we can get a response = 200
-  // Verify our responses have good values
-  // Verify a request without a token is invalid
-  // Request a user and verify the data
+// Before: create a token.  Optional - record http responses
+// After: Recorder cleanup
+// Verify we can get a response = 200
+// Verify our responses have good values
+// Verify a request without a token is invalid
+// Request a user and verify the data
 
 
 // Set the connection URL
-var functionalServer = config.functionalServer.location;
+const functionalServer = config.localServer.location;
 // To switch between localhost and remote host, change conUrl to one of the above.  Configure those values in config/httpconfig.js
-var conUrl = functionalServer;
+const conUrl = functionalServer;
 // Set recordCalls to true if you want to save off all http requests for
 // offline testing.  See record.js for details
-var recordCalls = false;
+const recordCalls = false;
 // get the test user from the config file
-var testUser = config.httpTestUser;
+const testUser = config.httpTestUser;
 
-
-describe("MAGE-server API JSON test", function(){
+describe("MAGE-server API JSON test", function () {
+  let recorder;
   // a recorder to save the http request data for offline playback
-  if(recordCalls){
-    var recorder = record('mage_recording');
+  if (recordCalls) {
+    recorder = record('mage_recording');
   }
   // Need to store a token for future requests
-  var myToken = "";
+  let myToken = "";
 
   // ----- Before: get a token
-  before(function(done){
+  before(function (done) {
     // Record http requests for testing offline
-    if(recordCalls){
+    if (recordCalls) {
       recorder.before();
     }
     // Make a request for a token before the tests execute
-    var tokenOptions = {
-      url: conUrl + "/login",
+    const tokenOptions = {
+      url: conUrl + "/auth/local/signin",
       method: 'POST',
       form: {
         'username': testUser.username,
@@ -47,18 +47,18 @@ describe("MAGE-server API JSON test", function(){
         'password': testUser.password
       }
     };
-    request(tokenOptions, function(err, response, body){
+    request(tokenOptions, function (err, response, body) {
       if (err) return done(err);
 
-      var tokenObj = JSON.parse(body);
+      const tokenObj = JSON.parse(body);
       myToken = tokenObj.token;
       done();
     });
   });
 
   // ----- make sure the recorder saves to file
-  after(function(done){
-    if(recordCalls){
+  after(function (done) {
+    if (recordCalls) {
       recorder.after();
     }
     done();
@@ -68,39 +68,54 @@ describe("MAGE-server API JSON test", function(){
   // ------------- Tests -----------------
   // Make a request and verify we get a response
   // check the name property
-  it("Verify MAGE server is up - return status 200 : /api", function(done){
-    request(conUrl, function(error, response, body){
+  it("Verify MAGE server is up - return status 200 : /api", function (done) {
+    request(conUrl + "/api", function (err, response, body) {
+      if (err) {
+        console.log(err);
+        return done(err);
+      }
+
       expect(response.statusCode).to.equal(200);
-      var jsonObj = JSON.parse(body);
-      var appName = jsonObj.name.substring(0,4);
-      expect(appName).to.equal('MAGE');
+      const jsonObj = JSON.parse(body);
+      expect(jsonObj.name).to.contain('mage');
       done();
     });
   });
 
   // ----- Should be unauthorized without token
-  it("Verify request is denied when token isn't given : /api/users/{id}", function(done){
-    var tokenOptions = {
-      url: conUrl + "/users/" + testUser.userId,
+  it("Verify request is denied when token isn't given : /api/users/{id}", function (done) {
+    const tokenOptions = {
+      url: conUrl + "/api/users/" + testUser.userId,
       method: 'GET'
     };
-    request(tokenOptions, function(err, response){
+    request(tokenOptions, function (err, response) {
+      if (err) {
+        console.log(err);
+        return done(err);
+      }
+
       expect(response.statusCode).to.equal(401);
       done(err);
     });
   });
 
   // ------ Get user info
-  it("Verify response from /api/users/{id}", function(done){
-    var tokenOptions = {
-      url: conUrl + "/users/" + testUser.userId,
+  it("Verify response from /api/users/{id}", function (done) {
+
+    const tokenOptions = {
+      url: conUrl + "/api/users/" + testUser.userId,
       method: 'GET',
-      headers: {'Authorization': 'Bearer ' + myToken}
+      headers: { 'Authorization': 'Bearer ' + myToken }
     };
-    
-    request(tokenOptions, function(err, response, body){
-      var jsonObj = JSON.parse(body);
-      var username = jsonObj.username;
+
+    request(tokenOptions, function (err, response, body) {
+      if (err) {
+        console.log(err);
+        return done(err);
+      }
+
+      const jsonObj = JSON.parse(body);
+      const username = jsonObj.username;
       expect(username).to.equal(testUser.username);
       done(err);
     });
