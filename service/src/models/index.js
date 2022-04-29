ab087ed9075da9d1f6fb3c57083c49842417ff43
Robert St. John <restjohn@users.noreply.github.com>
2022-04-29 14:38:46 -0600

    [service] stop using deprecated substr() method

--- service/src/models/index.js
+++ service/src/models/index.js
@@ -4,7 +4,7 @@ const { modulesPathsInDir } = require('../utilities/loader');
 module.exports.initializeModels = function () {
   log.info('intializing database models ...');
   modulesPathsInDir(__dirname).forEach(file => {
-    const moduleName = file.substr(0, file.indexOf('.'));
+    const moduleName = file.slice(0, file.indexOf('.'));
     log.info(`intializing database model ${moduleName} ...`);
     require('./' + moduleName);
   });
