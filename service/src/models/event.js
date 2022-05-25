3026d805913e2c147c98071904ca7f1b6827a6bf
Robert St. John <restjohn@users.noreply.github.com>
2022-05-25 13:32:08 -0600

    [service] fix object casting error bad method reference when populating user fields during event pre-init hook

--- service/src/models/event.js
+++ service/src/models/event.js
@@ -134,6 +134,17 @@ function populateUserFields(event, callback) {
 }
 
 EventSchema.pre('init', function(next, event) {
+  /**
+  TODO: This is not ideal.  If one uses a query like EventModel.findById(1).populate('teamIds'),
+  chaining the populate step onto the query builder, Mongoose does not seem to
+  correctly set the populated('teamIds') state before the pre-init middleware is
+  finished executing.  Since populateUserFields() checks event.populated('teamIds'),
+  this causes an error because the teamIds on the Event document are actually
+  Team documents, but populateUserFields() attempts to tream them as ObjectID
+  instances to query for the teams, and Mongoose throws a type cast error.
+  Actually, Mongoose might not even add th populated() method to the document
+  instance during the pre-init hook's execution.
+  */
   if (event.forms) {
     populateUserFields(event, function() {
       next();
