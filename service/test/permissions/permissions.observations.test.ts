9e53abbe35bbe6efc2b8a0490766c21cd3a7eb87
Robert St. John <restjohn@users.noreply.github.com>
2022-05-30 15:09:10 -0600

    [service] fix missed reference after type rename

--- service/test/permissions/permissions.observations.test.ts
+++ service/test/permissions/permissions.observations.test.ts
@@ -9,6 +9,7 @@ import mongoose from 'mongoose'
 import { expect } from 'chai'
 import { ObservationPermission } from '../../lib/entities/authorization/entities.permissions'
 import { ErrPermissionDenied, MageError, PermissionDeniedError } from '../../lib/app.api/app.api.errors'
+import { stringify } from 'querystring'
 
 describe.only('observation permissions service', function() {
 
@@ -85,7 +86,60 @@ describe.only('observation permissions service', function() {
 
   describe('update permission', function() {
 
-    it('grants when')
-    it('denies when')
+    it('grants when the context user role has permission to update all observations regardless of event scope', async function() {
+
+      user.roleId.permissions = [ ObservationPermission.UPDATE_OBSERVATION_ALL ]
+      const denied = await permissions.ensureUpdateObservationPermission(context)
+
+      expect(denied).to.be.null
+    })
+
+    it('grants when the context user role has event participant update permission and read access for the context event', async function() {
+
+      user.roleId.permissions = [ ObservationPermission.UPDATE_OBSERVATION_EVENT ]
+      eventPermissions.userHasEventPermission(context.mageEvent, context.requestingPrincipal().id, EventAccessType.Read).resolves(true)
+      const denied = await permissions.ensureUpdateObservationPermission(context)
+
+      eventPermissions.received(1).userHasEventPermission(context.mageEvent, context.requestingPrincipal().id, EventAccessType.Read)
+    })
+
+    describe('denies', function() {
+
+      const denyTests: [ testName: string, setupContext: () => any ][] = [
+        [ 'with empty permissions', () => void(0) ],
+        [
+          'with participant update permission but without event read access',
+          () => {
+            user.roleId.permissions = [ ObservationPermission.UPDATE_OBSERVATION_EVENT ]
+            eventPermissions.userHasEventPermission(Arg.all()).resolves(false)
+          }
+        ],
+        [
+          'with event read access but without participant update permission',
+          () => {
+            user.roleId.permissions = []
+            eventPermissions.userHasEventPermission(Arg.all()).resolves(true)
+          }
+        ]
+      ]
+      denyTests.forEach(([ testName, setupContext ]) => {
+        test(testName, async function() {
+          setupContext()
+          const denied = await permissions.ensureUpdateObservationPermission(context)
+          expect(denied).to.be.instanceOf(MageError)
+          expect(denied?.code).to.equal(ErrPermissionDenied)
+          expect(denied?.data.permission).to.equal('UPDATE_OBSERVATION')
+        })
+      })
+    })
+
+    it('denies in all other cases', async function() {
+
+      let denied = await permissions.ensureUpdateObservationPermission(context)
+
+      expect(denied).to.be.instanceOf(MageError)
+      expect(denied?.code).to.equal(ErrPermissionDenied)
+      expect(denied?.data.permission).to.equal('UPDATE_OBSERVATION')
+    })
   })
 })
