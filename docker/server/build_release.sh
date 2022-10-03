ea606a962ad161fc2f6c440738ec58fc48bfe2a3
Robert St. John <restjohn@users.noreply.github.com>
2022-10-03 11:07:25 -0600

    remove release tag prefix in docker server build script

--- docker/server/build_release.sh
+++ docker/server/build_release.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 release_version=$1
-# TODO: might move away from this release tag prefix business
-release_prefix="releases"
-[[ "${release_version}" =~ [0-9]+-[a-zA-Z0-9]+\.[0-9]+$ ]] && release_prefix="prereleases"
-release_tag="${release_prefix}/${release_version}"
-gh release --repo ngageoint/mage-server download ${release_tag}
+gh release --repo ngageoint/mage-server download ${release_version}
 docker build . -t "mage-server:${release_version}"
