ff69c43744a591243f69116f822dc4825e467de7
Robert St. John <restjohn@users.noreply.github.com>
2023-07-06 22:45:09 -0600

    [service] fix required field validation fails on numeric value 0

--- service/src/entities/observations/entities.observations.fields.required.ts
+++ service/src/entities/observations/entities.observations.fields.required.ts
@@ -3,7 +3,7 @@ import { SimpleFieldValidation } from './entities.observations.fields'
 
 export const RequiredFieldValidation: SimpleFieldValidation = function RequiredFieldValidation(field, fieldEntry, result) {
   if (field.required) {
-    if (!fieldEntry) {
+    if (fieldEntry === null || fieldEntry === undefined || fieldEntry === '') {
       return result.failedBecauseTheEntry('is required', FieldConstraintKey.Required)
     }
   }
