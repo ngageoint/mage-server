ea2f159e59dc048779c5e64b93f421610be77b2f
Robert St. John <restjohn@users.noreply.github.com>
2023-08-01 16:04:26 -0600

    [service] fix `important` property reference in observation type mapping

--- service/src/models/observation.d.ts
+++ service/src/models/observation.d.ts
@@ -3,7 +3,7 @@ import { MageEvent, MageEventAttrs, MageEventId } from '../entities/events/entit
 import { Attachment, AttachmentId, FormEntry, ObservationAttrs, ObservationFeatureProperties, ObservationId, ObservationImportantFlag, ObservationState, Thumbnail } from '../entities/observations/entities.observations'
 import { MageEventDocument } from './event'
 
-export type ObservationDocument = Omit<mongoose.Document, 'toJSON'> & Omit<ObservationAttrs, 'eventId' | 'userId' | 'deviceId' | 'importantFlag' | 'favoriteUserIds' | 'attachments' | 'states' | 'properties'> & {
+export type ObservationDocument = Omit<mongoose.Document, 'toJSON'> & Omit<ObservationAttrs, 'eventId' | 'userId' | 'deviceId' | 'important' | 'favoriteUserIds' | 'attachments' | 'states' | 'properties'> & {
   userId?: mongoose.Types.ObjectId
   deviceId?: mongoose.Types.ObjectId
   important?: ObservationDocumentImportantFlag
