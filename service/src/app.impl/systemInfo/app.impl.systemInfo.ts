5a0e69965498d4f3e1ad22e0ab336b545d35d2b3
kimura-developer <136853071+kimura-developer@users.noreply.github.com>
2023-08-17 10:53:42 -0500

    systemInfo tests, authApiAppender moved into /api endpoint

--- service/src/app.impl/systemInfo/app.impl.systemInfo.ts
+++ service/src/app.impl/systemInfo/app.impl.systemInfo.ts
@@ -2,13 +2,31 @@ import { AppResponse } from '../../app.api/app.api.global'
 import * as api from '../../app.api/systemInfo/app.api.systemInfo'
 import { EnvironmentService } from '../../entities/systemInfo/entities.systemInfo'
 import * as Settings from '../../models/setting'
-import AuthenticationApiAppender  from '../../utilities/authenticationApiAppender'
+import * as AuthenticationConfiguration from '../../models/authenticationconfiguration'
 import config from '../../config'
+import AuthenticationConfigurationTransformer from '../../transformers/authenticationconfiguration'
 /**
  * This factory function creates the implementation of the {@link api.ReadSystemInfo}
  * application layer interface.
  */
 export function CreateReadSystemInfo(EnvironmentService: EnvironmentService): api.ReadSystemInfo {
+
+  // appending the authentication strategies to the api
+   async function appendAuthenticationStrategies(api: any, options: any = {}): Promise<any> {
+    const apiCopy = {
+      ...api,
+      authenticationStrategies: {}
+    };
+    const authenticationConfigurations = await AuthenticationConfiguration.getAllConfigurations();
+    const transformedConfigurations = AuthenticationConfigurationTransformer.transform(
+      authenticationConfigurations.filter(config => config.enabled || options.includeDisabled),options
+    );
+    transformedConfigurations.forEach((configuration: { name: string | number }) => {
+      apiCopy.authenticationStrategies[configuration.name] = { ...configuration };
+    });
+    return apiCopy;
+  }
+
   return async function readSystemInfo(req: api.ReadSystemInfoRequest): Promise<api.ReadSystemInfoResponse> {
     // TODO: will need a permission check to determine what level of system
     // information the requesting principal is allowed to see
@@ -16,15 +34,14 @@ export function CreateReadSystemInfo(EnvironmentService: EnvironmentService): ap
     const disclaimer = await Settings.getSetting('disclaimer') || {}
     const contactInfo = await Settings.getSetting('contactinfo') || {}
 
-
-    const api = Object.assign({}, config.api, {
+    const apiConfig = Object.assign({}, config.api, {
       environment: environment,
       disclaimer: disclaimer,
       contactinfo: contactInfo
     });
     
-    const authApiAppender =  await AuthenticationApiAppender.append(api, { whitelist: true })
+    const updatedApiConfig = await appendAuthenticationStrategies(apiConfig, { whitelist: true });
     
-    return AppResponse.success(authApiAppender as any)
+    return AppResponse.success(updatedApiConfig as any)
   }
 }
---
+++ service/test/adapters/systemInfo/adapters.systemInfo.controllers.web.test.ts
@@ -0,0 +1,62 @@
+import { expect } from 'chai';
+import { AppResponse, AppRequest } from '../../../lib/app.api/app.api.global';
+import { beforeEach } from 'mocha';
+import express from 'express';
+import { WebAppRequestFactory } from '../../../lib/adapters/adapters.controllers.web';
+import { Substitute as Sub, SubstituteOf, Arg } from '@fluffy-spoon/substitute';
+import supertest from 'supertest';
+import {
+  EnvironmentService,
+  SystemInfo
+} from '../../../lib/entities/systemInfo/entities.systemInfo';
+import { SystemInfoAppLayer } from '../../../lib/app.api/systemInfo/app.api.systemInfo';
+import { SystemInfoRoutes } from '../../../lib/adapters/systemInfo/adapters.systemInfo.controllers.web';
+
+describe('SystemInfo web controller', () => {
+  const root = '/api';
+
+  type AppRequestFactoryHandle = {
+    createRequest: WebAppRequestFactory;
+  };
+
+  let client: supertest.SuperTest<supertest.Test>;
+  let appLayer: SubstituteOf<SystemInfoAppLayer>;
+  let appReqFactory: SubstituteOf<AppRequestFactoryHandle>;
+
+  beforeEach(function() {
+    appLayer = Sub.for<SystemInfoAppLayer>();
+    appReqFactory = Sub.for<AppRequestFactoryHandle>();
+    const endpoint = express();
+    endpoint.use(function lookupUser(
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) {
+      req.testUser = req.headers['user'] as string;
+      next();
+    });
+    const controller = SystemInfoRoutes(appLayer, appReqFactory.createRequest);
+    endpoint.use(root, controller);
+    client = supertest(endpoint);
+  });
+
+  describe('GET /api', () => {
+    it('should return configuration', async () => {
+      const expected: SystemInfo = {
+        mageVersion: 'someValue',
+        environment: {
+          nodeVersion: 'test',
+          monogdbVersion: 'test'
+        },
+        disclaimer: 'test',
+        contactInfo: 'test'
+      };
+      appLayer
+        .readSystemInfo(Arg.any())
+        .returns(Promise.resolve(AppResponse.success(expected)));
+      const response = await client.get(`${root}`);
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(expected);
+    });
+  });
+});
