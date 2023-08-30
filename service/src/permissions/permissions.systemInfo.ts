f61b58996a9a113dcb941bf78dfaba9f9d8c8d0b
kimura-developer <136853071+kimura-developer@users.noreply.github.com>
2023-08-30 17:43:47 -0500

    removed redundant entities code

--- service/src/permissions/permissions.systemInfo.ts
+++ service/src/permissions/permissions.systemInfo.ts
@@ -5,27 +5,40 @@ import {
 import { AppRequestContext } from '../app.api/app.api.global';
 import { SystemInfoPermissionService } from '../app.api/systemInfo/app.api.systemInfo';
 import { SystemInfoPermission } from '../entities/authorization/entities.permissions';
+import { rolesWithPermission, SystemInfoAccessType } from '../entities/systemInfo/entities.systemInfo';
 import {
   UserWithRole,
   ensureContextUserHasPermission
 } from './permissions.role-based.base';
 
-export class RoleBasedSystemInfoPermissionService implements SystemInfoPermissionService {
-  async ensureReadSystemInfoPermission( ctx: AppRequestContext<UserWithRole>): Promise<null | PermissionDeniedError> {
+export class RoleBasedSystemInfoPermissionService
+  implements SystemInfoPermissionService {
+  async ensureReadSystemInfoPermission(
+    ctx: AppRequestContext<UserWithRole>
+  ): Promise<null | PermissionDeniedError> {
     const user = ctx.requestingPrincipal();
 
-    // Check if the user is the "admin"
-    if (user && user.username !== 'admin') {
+    // If user doesn't exist, deny permission.
+    if (!user) {
+      return permissionDenied(
+        SystemInfoPermission.READ_SYSTEM_INFO,
+        'Unknown User',
+        'SystemInfo'
+      );
+    }
+
+    // Check if the user's role has the required permission.
+    if (
+      user.roleId.permissions.includes(SystemInfoPermission.READ_SYSTEM_INFO)
+    ) {
+      return null; // This means no error and the user has permission
+    } else {
       return permissionDenied(
         SystemInfoPermission.READ_SYSTEM_INFO,
         user.username,
         'SystemInfo'
       );
     }
-
-    return ensureContextUserHasPermission(
-      ctx,
-      SystemInfoPermission.READ_SYSTEM_INFO
-    );
   }
 }
+
