f34a244c2e8eda3967cbeb6797243ea5d71cfdc5
kimura-developer <136853071+kimura-developer@users.noreply.github.com>
2023-09-20 10:53:06 -0500

    WIP

--- service/src/app.impl/users/app.impl.users.ts
+++ service/src/app.impl/users/app.impl.users.ts
@@ -3,19 +3,9 @@ import { UserRepository } from '../../entities/users/entities.users';
 import { withPermission, KnownErrorsOf } from '../../app.api/app.api.global';
 import { PageOf } from '../../entities/entities.global';
 
-export function SearchUsers(
-  userRepo: UserRepository,
-  permissions: api.UsersPermissionService
+export function SearchUsers(userRepo: UserRepository,permissions: api.UsersPermissionService
 ): api.SearchUsers {
-  return async function searchUsers(
-    req: api.UserSearchRequest
-  ): ReturnType<api.SearchUsers> {
-    // Use the buildFilter function to construct the filter
-    const userSearch: api.UserSearchRequest['userSearch'] = buildFilter(
-      req.userSearch
-    );
-
-    // Continue with the rest of the logic, including the search operation
+  return async function searchUsers(req: api.UserSearchRequest): ReturnType<api.SearchUsers> {
     return await withPermission<
       PageOf<api.UserSearchResult>,
       KnownErrorsOf<api.SearchUsers>
@@ -23,7 +13,7 @@ export function SearchUsers(
       permissions.ensureReadUsersPermission(req.context),
       async (): Promise<PageOf<api.UserSearchResult>> => {
         const page = await userRepo.find<api.UserSearchResult>(
-          userSearch,
+          req.userSearch,
           x => {
             return {
               id: x.id,
@@ -44,16 +34,4 @@ export function SearchUsers(
       }
     );
   };
-}
-
-// Function to abstract parameter handling and filter building
-function buildFilter(userSearch: api.UserSearchRequest['userSearch']): api.UserSearchRequest['userSearch'] {
-  const filter: api.UserSearchRequest['userSearch'] = {
-    nameOrContactTerm: userSearch.nameOrContactTerm,
-    pageSize: userSearch.pageSize || 250,
-    pageIndex: userSearch.pageIndex || 0,
-    includeTotalCount: userSearch.includeTotalCount,
-  };
-
-  return filter;
 }
