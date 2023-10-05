d745e7e8c3d115d233826e6c50347e3f6437debc
kimura-developer <136853071+kimura-developer@users.noreply.github.com>
2023-10-05 11:45:06 -0500

    fix: Refactor userSearchObservable to prevent state interference.

--- web-app/projects/core-lib/user/user-read.service.ts
+++ web-app/projects/core-lib/user/user-read.service.ts
@@ -1,12 +1,11 @@
-import { HttpClient } from '@angular/common/http'
-import { InjectionToken, Injectable } from '@angular/core'
-import { PageOf, PagingParameters } from '@ngageoint/mage.web-core-lib/paging'
-import { Observable, of } from 'rxjs'
-import { tap } from 'rxjs/operators';
-import { User } from './user.model'
-import { userSearchObservable } from './user-search-observable'
+import { HttpClient } from '@angular/common/http';
+import { InjectionToken, Injectable } from '@angular/core';
+import { PageOf } from '@ngageoint/mage.web-core-lib/paging';
+import { Observable } from 'rxjs';
+import { User } from './user.model';
+import { userSearchObservable } from './user-search-observable';
 
-export const USER_READ_BASE_URL = '/api/next-users'
+export const USER_READ_BASE_URL = '/api/next-users';
 
 type SearchQueryParams = {
   page_size: string;
@@ -17,60 +16,16 @@ type SearchQueryParams = {
   enabled?: 'true' | 'false';
 };
 
-const reqKeys: { [SearchParamKey in keyof UserSearchParams]: string } = {
-  term: 'term',
-  pageSize: 'page_size',
-  pageIndex: 'page',
-  includeTotalCount: 'total',
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class UserReadService {
-  private lastSearchParams: UserSearchParams | null = null;
-  private lastSearchResult: PageOf<UserSearchResult> | null = null;
-
   constructor(private http: HttpClient) {}
 
   search(which: UserSearchParams): Observable<PageOf<UserSearchResult>> {
-    // Check if the search parameters are the same as the last search
-    if (
-      this.lastSearchParams &&
-      JSON.stringify(this.lastSearchParams) === JSON.stringify(which)
-    ) {
-      return of(this.lastSearchResult!);
-    }
-
-    const queryParams: SearchQueryParams = {
-      page_size: String(which.pageSize),
-      page: String(which.pageIndex)
-    };
-
-    if (typeof which.term === 'string') {
-      queryParams.term = which.term;
-    }
-
-    if (which.active !== undefined) {
-      queryParams.active = which.active ? 'true' : 'false';
-    }
-    if (which.enabled !== undefined) {
-      queryParams.enabled = which.enabled ? 'true' : 'false';
-    }
-
-    return this.http
-      .get<PageOf<UserSearchResult>>(`${USER_READ_BASE_URL}/search`, {
-        params: queryParams
-      })
-      .pipe(
-        tap((result) => {
-          this.lastSearchParams = which;
-          this.lastSearchResult = result;
-        })
-      );
+    return userSearchObservable(which, this.http, USER_READ_BASE_URL);
   }
 }
-
 
 export interface UserSearchParams {
   term?: string;
@@ -81,11 +36,13 @@ export interface UserSearchParams {
   includeTotalCount?: boolean;
 }
 
-export type UserSearchResult = Pick<User, 'id' | 'username' | 'displayName' | 'email' | 'active' | 'enabled'> & {
-  /**
-   * A reduction of all the phone numbers to a single string
-   */
-  allPhones?: string | null | undefined
-}
+export type UserSearchResult = Pick<
+  User,
+  'id' | 'username' | 'displayName' | 'email' | 'active' | 'enabled'
+> & {
+  allPhones?: string | null | undefined;
+};
 
-export const USER_READ_SERVICE = new InjectionToken<UserReadService>('UserReadService')
+export const USER_READ_SERVICE = new InjectionToken<UserReadService>(
+  'UserReadService'
+);
--- web-app/projects/core-lib/user/user-search-observable.ts
+++ web-app/projects/core-lib/user/user-search-observable.ts
@@ -1,32 +1,38 @@
 import { Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { tap, map } from 'rxjs/operators';
 import { PageOf } from '@ngageoint/mage.web-core-lib/paging';
-import {
-  UserReadService,
-  UserSearchParams,
-  UserSearchResult
-} from './user-read.service';
-
-let lastSearchParams: UserSearchParams | null = null;
-let lastSearchResult: PageOf<UserSearchResult> | null = null;
+import { UserSearchParams, UserSearchResult } from './user-read.service';
 
 export function userSearchObservable(
   which: UserSearchParams,
-  userReadService: UserReadService
+  http: HttpClient,
+  baseUrl: string
 ): Observable<PageOf<UserSearchResult>> {
-  // Check if search params are the same as the last search
-  if (
-    lastSearchParams &&
-    JSON.stringify(lastSearchParams) === JSON.stringify(which)
-  ) {
-    return of(lastSearchResult!);
+  const cachedResults = new Map<string, PageOf<UserSearchResult>>();
+
+  const cacheKey = JSON.stringify(which);
+  if (cachedResults.has(cacheKey)) {
+    return of(cachedResults.get(cacheKey)!);
   }
 
-  return userReadService.baseSearch(which).pipe(
-    tap((result) => {
-      lastSearchParams = which;
-      lastSearchResult = result;
-    })
-  );
-}
+  const queryParams = {
+    page_size: String(which.pageSize),
+    page: String(which.pageIndex),
+    term: which.term,
+    active: which.active ? 'true' : 'false',
+    enabled: which.enabled ? 'true' : 'false',
+    total: which.includeTotalCount ? 'true' : 'false'
+  };
 
+  return http
+    .get<PageOf<UserSearchResult>>(`${baseUrl}/search`, {
+      params: queryParams as any
+    })
+    .pipe(
+      map((result) => result as PageOf<UserSearchResult>),
+      tap((result) => {
+        cachedResults.set(cacheKey, result);
+      })
+    );
+}
