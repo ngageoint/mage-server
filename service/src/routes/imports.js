7ab0b89df966fa6e8301c886bb0c2ed1936bb54b
Robert St. John <restjohn@users.noreply.github.com>
2023-10-12 11:35:17 -0600

    [service] add error logging to kml layer import

--- service/src/routes/imports.js
+++ service/src/routes/imports.js
@@ -21,7 +21,9 @@ module.exports = function(app, security) {
       if (err) return next(err);
 
       const kml = new DOMParser({
-        errorHandler: () => { /* ignore */ }
+        errorHandler: (level, message) => {
+          console.log('kml parsing', level, req.file.filename, message)
+        }
       }).parseFromString(data);
 
       if (!kml || kml.documentElement.nodeName !== 'kml') {
