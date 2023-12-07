e46103ab89b5a8498564d58cbc70943fb725fb47
Robert St. John <restjohn@users.noreply.github.com>
2023-12-06 23:24:29 -0700

    [service] exports: get exporter factory tests passing

--- service/test/export/exporterFactoryTest.js
+++ service/test/export/exporterFactoryTest.js
@@ -1,36 +1,47 @@
 const { expect } = require('chai');
-const { createExportTransform } = require('../../lib/export');
+const { exportFactory } = require('../../lib/export');
+const EventModel = require('../../lib/models/event')
 
 require('chai').should();
+
+const eventAttrs = {
+    id: 303,
+    name: 'Export Tests',
+    forms: [],
+    feedIds: [],
+    layerIds: [],
+    style: {},
+    acl: {},
+}
 
 describe("export factory", function () {
 
     it("should create kml exporter", function () {
-        const exporter = createExportTransform('kml', {});
+        const exporter = exportFactory.createExportTransform('kml', { event: new EventModel.Model(eventAttrs) });
         expect(exporter).to.not.be.undefined;
         expect(exporter).to.not.be.null;
     });
 
     it("should create geojson exporter", function () {
-        const exporter = createExportTransform('geojson', {});
+        const exporter = exportFactory.createExportTransform('geojson', { event: new EventModel.Model(eventAttrs) });
         expect(exporter).to.not.be.undefined;
         expect(exporter).to.not.be.null;
     });
 
     it("should create csv exporter", function () {
-        const exporter = createExportTransform('csv', {});
+        const exporter = exportFactory.createExportTransform('csv', { event: new EventModel.Model(eventAttrs) });
         expect(exporter).to.not.be.undefined;
         expect(exporter).to.not.be.null;
     });
 
     it("should create geopackage exporter", function () {
-        const exporter = createExportTransform('geopackage', {});
+        const exporter = exportFactory.createExportTransform('geopackage', { event: new EventModel.Model(eventAttrs) });
         expect(exporter).to.not.be.undefined;
         expect(exporter).to.not.be.null;
     });
 
     it("should handle unsupported exporter", function () {
-        const exporter = createExportTransform('fake', {});
+        const exporter = exportFactory.createExportTransform('fake', { event: new EventModel.Model(eventAttrs) });
         expect(exporter).to.be.undefined;
     });
 });
