1f6d7916aa2f6cf68fb87c79f6315e6717bd078f
Robert St. John <restjohn@users.noreply.github.com>
2024-03-11 14:56:27 -0600

    [web-app] unsubscribe from feed fetch when the feed panel tab changes to prevent showing content of incorrect feed

--- web-app/src/app/feed/feed-list/feed-list.component.ts
+++ web-app/src/app/feed/feed-list/feed-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Feed, FeedService } from '@ngageoint/mage.web-core-lib/feed';
 import { Feature } from 'geojson';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'feed-list',
@@ -11,13 +12,15 @@ export class FeedListComponent implements OnChanges {
   @Input() feed: Feed
 
   items: Array<Feature> = []
+  feedSubscription: Subscription | null = null
 
   constructor(private feedService: FeedService) {}
 
   ngOnChanges(changes: SimpleChanges): void {
     const feed: Feed = changes.feed.currentValue;
+    this.feedSubscription?.unsubscribe()
     if (feed) {
-      this.feedService.feedItems(feed.id).subscribe(items => {
+      this.feedSubscription = this.feedService.feedItems(feed.id).subscribe(items => {
         this.items = items;
       });
     }
