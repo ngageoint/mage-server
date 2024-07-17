fa38816ecc2c1ff7405a391f5e9dafa528cf054a
Robert St. John <restjohn@users.noreply.github.com>
2024-07-17 09:11:24 -0600

    [service] users next: port verification jwt module to typescript

--- service/src/authentication/verification.ts
+++ service/src/authentication/verification.ts
@@ -1,152 +1,139 @@
-const JWT = require('jsonwebtoken');
+import JWT from 'jsonwebtoken'
+import { Json } from '../entities/entities.json_types'
 
-function payload(subject, assertion) {
-  return { subject: `${subject}`, assertion: assertion };
+type Payload = {
+  subject: string | null,
+  assertion: TokenAssertion | null,
+  expiration: number | null,
+  [key: string]: Json
 }
 
-const VerificationErrorReason = {
-  NoToken: 'no_token',
-  Subject: 'subject',
-  Assertion: 'assertion',
-  Expired: 'expired',
-  Decode: 'decode',
-  Implementation: 'implementation'
+export enum VerificationErrorReason {
+  NoToken = 'no_token',
+  Subject = 'subject',
+  Assertion = 'assertion',
+  Expired = 'expired',
+  Decode = 'decode',
+  Implementation = 'implementation'
 }
 
-const TokenAssertion = {
-  Authorized: 'urn:mage:auth:authorized',
-  Captcha: 'urn:mage:signup:captcha'
+export enum TokenAssertion {
+  Authorized = 'urn:mage:auth:authorized',
+  Captcha = 'urn:mage:signup:captcha'
 }
 
-class TokenGenerateError extends Error {
+export class TokenGenerateError extends Error {
 
-  constructor(subject, assertion, secondsToLive, cause) {
-    super();
-    this.name = 'TokenVerificationError';
-    this.subject = subject;
-    this.assertion = assertion;
-    this.secondsToLive = secondsToLive;
-    this.cause = cause;
-    let causeMessage = '';
-    if (cause) {
-      causeMessage = 'cause=' + (cause instanceof Error ? `${"\n" + cause.stack || `${cause.name}: ${cause.message}`}` : `${cause}`)
-    }
-    this.message = `error generating token: subject=${subject}; assertion=${assertion}; secondsToLive=${secondsToLive}; ${causeMessage}`
+  constructor(readonly subject: string, readonly assertion: TokenAssertion, readonly secondsToLive: number, readonly cause?: Error | null) {
+    const causeMessage = cause ? `cause=\n${cause.stack || `${cause.name}: ${cause.message}`}` : ''
+    super(`error generating token: subject=${subject}; assertion=${assertion}; secondsToLive=${secondsToLive}; ${causeMessage}`)
   }
 }
 
 class TokenVerificationError extends Error {
+
   /**
    * @param reason why the verification failed
-   * @param subject the expected subject
-   * @param assertion the expected assertion
+   * @param expected the expected `Payload`
    * @param token the encoded token string
-   * @param decoded the decoded payload of the token
+   * @param decoded the decoded `Payload` of the token
    * @param cause cause of the verification error from the underlying token implementation
    */
-  constructor(reason, expected, token, decoded, cause) {
-    super();
-    this.reason = reason;
-    this.expected = expected || { subject: null, assertion: null };
-    this.token = token;
-    this.decoded = decoded;
+  constructor(readonly reason: VerificationErrorReason, readonly expected: Payload | null, readonly token: string, readonly decoded: any, readonly cause?: Error | null) {
     let reasonMessage = `${reason}`;
     let causeMessage = '';
     if (cause) {
       causeMessage = 'cause=' + (cause instanceof Error ? `${"\n" + cause.stack || `${cause.name}: ${cause.message}`}` : `${cause}`)
     }
-
-    decoded = decoded || { subject: null, assertion: null, expiration: null };
     if (reason === VerificationErrorReason.Subject) {
-      reasonMessage = `subject was ${decoded.subject}`
-    } else if (reason === VerificationErrorReason.Assertion) {
-      reasonMessage = `assertion was ${decoded.assertion}`
-    } else if (reason === VerificationErrorReason.Expired) {
-      reasonMessage = `token expired ${new Date(decoded.expiration || 0)} (${decoded.expiration})`
-    } else if (reason === VerificationErrorReason.Decode) {
-      reasonMessage = `failed to decode token`;
+      reasonMessage = `subject was ${decoded?.subject}`
     }
-
-    this.message = `TokenVerificationError (${reasonMessage}): expected subject=${this.expected.subject}, assertion=${this.expected.assertion}; token=${token}; ${causeMessage}`
+    else if (reason === VerificationErrorReason.Assertion) {
+      reasonMessage = `assertion was ${decoded?.assertion}`
+    }
+    else if (reason === VerificationErrorReason.Expired) {
+      reasonMessage = `token expired ${new Date(decoded?.expiration || 0)} (${decoded?.expiration})`
+    }
+    else if (reason === VerificationErrorReason.Decode) {
+      reasonMessage = `failed to decode token`
+    }
+    super(`TokenVerificationError (${reasonMessage}): expected subject=${expected?.subject}, assertion=${expected?.assertion}; token=${token}; ${causeMessage}`)
   }
 }
 
-const JWTService = class {
+export class JWTService {
 
-  constructor(secret, issuer) {
-    this._secret = secret;
-    this._issuer = issuer;
-    this._algorithm = 'HS256';
-  }
+  private readonly algorithm = 'HS256'
 
-  generateToken(subject, assertion, secondsToLive, claims = {}) {
+  constructor(private readonly secret: string, private readonly issuer: string) {}
+
+  generateToken(subject: string, assertion: TokenAssertion, secondsToLive: number, claims = {}): Promise<string> {
     return new Promise((resolve, reject) => {
-      const payload = Object.assign({ assertion: assertion }, claims);
-
-      JWT.sign(payload, this._secret, {
-        algorithm: this._algorithm,
-        issuer: this._issuer,
-        expiresIn: secondsToLive,
-        subject: subject,
-      },(err, token) => {
-        if (err) {
-          reject(new TokenGenerateError(subject, assertion, secondsToLive, err));
-        } else {
-          resolve(token);
+      const payload = Object.assign({ assertion }, claims);
+      JWT.sign(payload, this.secret,
+        {
+          algorithm: this.algorithm,
+          issuer: this.issuer,
+          expiresIn: secondsToLive,
+          subject: subject,
+        },
+        (err, token) => {
+          if (err) {
+            reject(new TokenGenerateError(subject, assertion, secondsToLive, err))
+          }
+          else {
+            resolve(String(token))
+          }
         }
-      });
-    });
+      )
+    })
   }
 
-  verifyToken(token, expected) {
+  verifyToken(token: string, expected: Payload): Promise<Payload> {
     return new Promise((resolve, reject) => {
-      JWT.verify(token, this._secret, { algorithms: [this._algorithm], issuer: this._issuer }, (err, payload) => {
+      JWT.verify(token, this.secret, { algorithms: [this.algorithm], issuer: this.issuer }, (err: JWT.VerifyErrors | null, anyPayload?: any) => {
+        const jwtPayload = anyPayload as JWT.JwtPayload
         if (!expected) {
-          expected = { subject: null, assertion: null };
+          expected = { subject: null, assertion: null, expiration: null };
         }
-
         if (err) {
           let reason = VerificationErrorReason.Implementation;
           let decoded = null;
           try {
             const jwt = JWT.decode(token, { json: true });
             if (jwt) {
-              decoded = Object.assign({}, jwt, {
+              decoded = {
+                ...jwt,
                 subject: jwt.sub || null,
                 assertion: jwt.assertion || null,
                 expiration: jwt.exp || null
-              });
+              };
             }
           } catch (err) {
             reason = VerificationErrorReason.Decode;
           }
-
           if (!decoded) {
             reason = VerificationErrorReason.Decode;
             decoded = { subject: null, assertion: null, expiration: null };
-          } else if (err instanceof JWT.TokenExpiredError) {
+          }
+          else if (err instanceof JWT.TokenExpiredError) {
             reason = VerificationErrorReason.Expired;
           }
-          return reject(new TokenVerificationError(reason, expected, token, payload, err));
-        } else if (expected.subject && payload.sub !== expected.subject) {
-          return reject(new TokenVerificationError(VerificationErrorReason.Subject, expected, token, payload))
-        } else if (expected.assertion && payload.assertion !== expected.assertion) {
-          return reject(new TokenVerificationError(VerificationErrorReason.Assertion, expected, token, payload));
+          return reject(new TokenVerificationError(reason, expected, token, jwtPayload, err));
         }
-
-        resolve(Object.assign({}, payload, {
-            subject: payload.sub || null,
-            assertion: payload.assertion || null,
-            expiration: payload.exp || null
-        }));
-
-      });
-    });
+        else if (expected.subject && jwtPayload.sub !== expected.subject) {
+          return reject(new TokenVerificationError(VerificationErrorReason.Subject, expected, token, jwtPayload))
+        }
+        else if (expected.assertion && jwtPayload.assertion !== expected.assertion) {
+          return reject(new TokenVerificationError(VerificationErrorReason.Assertion, expected, token, jwtPayload));
+        }
+        resolve({
+          ...jwtPayload,
+          subject: jwtPayload.sub || null,
+          assertion: jwtPayload.assertion || null,
+          expiration: jwtPayload.exp || null
+        })
+      })
+    })
   }
 }
-
-exports.payload = payload;
-exports.TokenAssertion = TokenAssertion;
-exports.TokenGenerateError = TokenGenerateError;
-exports.VerificationErrorReason = VerificationErrorReason;
-exports.JWTService = JWTService;
