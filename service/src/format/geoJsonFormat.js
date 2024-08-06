de0c152a7dfa9f66f2869dc77200f1f9d929ce88
Robert St. John <restjohn@users.noreply.github.com>
2024-08-05 21:16:17 -0600

    style(service): update syntax in geoJsonFormat module

--- service/src/format/geoJsonFormat.js
+++ service/src/format/geoJsonFormat.js
@@ -1,71 +1,70 @@
-var parseEnvelope = function(text) {
-  var bbox = JSON.parse(text);
-  if (bbox.length !== 4) {
+function parseEnvelope(text) {
+  const coords = JSON.parse(text);
+  if (coords.length !== 4) {
     throw new Error("Invalid geometry: " + text);
   }
 
-  var xmin = parseFloat(bbox[0]);
-  var ymin = parseFloat(bbox[1]);
-  var xmax = parseFloat(bbox[2]);
-  var ymax = parseFloat(bbox[3]);
+  let xmin = parseFloat(coords[0]);
+  let ymin = parseFloat(coords[1]);
+  let xmax = parseFloat(coords[2]);
+  let ymax = parseFloat(coords[3]);
 
+  // TODO: is this the right thing to do?
   xmin = xmin < -180 ? -180 : xmin;
   ymin = ymin < -90 ? -90 : ymin;
   xmax = xmax > 180 ? 180 : xmax;
   ymax = ymax > 90 ? 90 : ymax;
 
-  bbox = {xmin: xmin, ymin: ymin, xmax: xmax, ymax: ymax };
+  const bbox = { xmin, ymin, xmax, ymax };
 
-  var geometries = [];
-
-  // TODO hack until mongo fixes queries for more than
+  // TODO: hack until mongo fixes queries for more than
   // 180 degrees longitude.  Create 2 geometries if we cross
   // the prime meridian
   if (bbox.xmax > 0 && bbox.xmin < 0) {
-    geometries.push({
-      type: 'Polygon',
-      coordinates: [ [
-        [bbox.xmin, bbox.ymin],
-        [0, bbox.ymin],
-        [0, bbox.ymax],
-        [bbox.xmin, bbox.ymax],
-        [bbox.xmin, bbox.ymin]
-      ] ]
-    });
-
-    geometries.push({
-      type: 'Polygon',
-      coordinates: [ [
-        [0, bbox.ymin],
-        [bbox.xmax, bbox.ymin],
-        [bbox.xmax, bbox.ymax],
-        [0, bbox.ymax],
-        [0, bbox.ymin]
-      ] ]
-    });
-  } else {
-    geometries.push({
-      type: 'Polygon',
-      coordinates: [ [
-        [bbox.xmin, bbox.ymin],
-        [bbox.xmax, bbox.ymin],
-        [bbox.xmax, bbox.ymax],
-        [bbox.xmin, bbox.ymax],
-        [bbox.xmin, bbox.ymin]
-      ] ]
-    });
+    return [
+      {
+        type: 'Polygon',
+        coordinates: [ [
+          [bbox.xmin, bbox.ymin],
+          [0, bbox.ymin],
+          [0, bbox.ymax],
+          [bbox.xmin, bbox.ymax],
+          [bbox.xmin, bbox.ymin]
+        ] ]
+      },
+      {
+        type: 'Polygon',
+        coordinates: [ [
+          [0, bbox.ymin],
+          [bbox.xmax, bbox.ymin],
+          [bbox.xmax, bbox.ymax],
+          [0, bbox.ymax],
+          [0, bbox.ymin]
+        ] ]
+      }
+    ]
   }
+  return [
+    {
+      type: 'Polygon',
+      coordinates: [ [
+        [bbox.xmin, bbox.ymin],
+        [bbox.xmax, bbox.ymin],
+        [bbox.xmax, bbox.ymax],
+        [bbox.xmin, bbox.ymax],
+        [bbox.xmin, bbox.ymin]
+      ] ]
+    }
+  ]
+}
 
-  return geometries;
-};
-
-var parseGeometry = function(type, text) {
+function parseGeometry(type, text) {
   switch (type) {
   case 'bbox':
     return parseEnvelope(text);
   default:
-    return [JSON.parse(text)];
+    return [ JSON.parse(text) ];
   }
-};
+}
 
 exports.parse = parseGeometry;
