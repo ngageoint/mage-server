aab463a88fff815137643a6aa7415f7d3548ee2d
Robert St. John <restjohn@users.noreply.github.com>
2024-08-21 09:24:14 -0600

    refactor(service): devices typescript/di: add functional test stubs for device operations

---
+++ service/functionalTests/devices/devices.test.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai'
+
+describe('device operations', function() {
+
+  describe('removing', function() {
+
+    it('invalidates associated sessions', async function() {
+      expect.fail('todo')
+    })
+
+    it('prevents the owning user from authenticating with the device', async function() {
+      expect.fail('todo')
+    })
+  })
+
+  describe('disabling', function() {
+
+    it('invalidates existing associated sessions', async function() {
+      expect.fail('todo')
+    })
+
+    it('prevents the owning user from authenticating with the device', async function() {
+      expect.fail('todo')
+    })
+  })
+
+  describe('enabling', function() {
+
+    it('allows the owning user to authenticate with the device', async function() {
+      expect.fail('todo')
+    })
+  })
+})
