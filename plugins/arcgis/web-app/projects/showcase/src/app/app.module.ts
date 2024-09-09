30146a4cce967d68a8dd24d8e081c56e4388a138
Jordan Foster <jordanafoster6@gmail.com>
2024-09-09 16:22:21 -0500

    1226: Update showcase port and import arcgis components

--- plugins/arcgis/web-app/projects/showcase/src/app/app.module.ts
+++ plugins/arcgis/web-app/projects/showcase/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MageArcModule } from 'projects/main/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MageArcModule,
     BrowserModule,
     AppRoutingModule
   ],
