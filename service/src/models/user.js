88eee2a35e74fb330d93e876cffcfb25d7b1f631
Robert St. John <restjohn@users.noreply.github.com>
2024-09-12 07:10:47 -0600

    refactor(service): users/auth: remove obsolete user model functions

--- service/src/models/user.js
+++ service/src/models/user.js
@@ -3,9 +3,8 @@
 const mongoose = require('mongoose')
   , moment = require('moment')
   , Authentication = require('./authentication')
+  // TODO: users-next
   , AuthenticationConfiguration = require('./authenticationconfiguration')
-  , { pageQuery } = require('../adapters/base/adapters.base.db.mongoose')
-  , { pageOf } = require('../entities/entities.global')
   , FilterParser = require('../utilities/filterParser');
 
 
@@ -14,36 +13,6 @@ exports.transform = DbUserToObject;
 // Creates the Model for the User Schema
 const User = mongoose.model('User', UserSchema);
 exports.Model = User;
-
-exports.getUserById = function (id, callback) {
-  let result = User.findById(id).populate('roleId').populate({ path: 'authenticationId', populate: { path: 'authenticationConfigurationId' } });
-  if (typeof callback === 'function') {
-    result = result.then(
-      user => {
-        callback(null, user);
-      },
-      err => {
-        callback(err);
-      });
-  }
-  return result;
-};
-
-exports.getUserByUsername = function (username, callback) {
-  User.findOne({ username: username.toLowerCase() }).populate('roleId').populate({ path: 'authenticationId', populate: { path: 'authenticationConfigurationId' } }).exec(callback);
-};
-
-exports.getUserByAuthenticationId = function (id, callback) {
-  User.findOne({ authenticationId: id }).populate('roleId').populate({ path: 'authenticationId', populate: { path: 'authenticationConfigurationId' } }).exec(callback);
-}
-
-exports.getUserByAuthenticationStrategy = function (strategy, uid, callback) {
-  Authentication.getAuthenticationByStrategy(strategy, uid, function (err, authentication) {
-    if (err || !authentication) return callback(err);
-
-    User.findOne({ authenticationId: authentication._id }).populate('roleId').populate({ path: 'authenticationId', populate: { path: 'authenticationConfigurationId' } }).exec(callback);
-  });
-}
 
 function createQueryConditions(filter) {
   const conditions = FilterParser.parse(filter);
@@ -73,66 +42,6 @@ exports.count = function (options, callback) {
     callback(err, count);
   });
 };
-
-exports.getUsers = async function (options, callback) {
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-
-  options = options || {};
-  const filter = options.filter || {};
-
-  const conditions = createQueryConditions(filter);
-
-  let baseQuery = User.find(conditions).populate({ path: 'authenticationId', populate: { path: 'authenticationConfigurationId' } });
-
-  if (options.lean) {
-    baseQuery = baseQuery.lean();
-  }
-
-  if (options.populate && (options.populate.indexOf('roleId') !== -1)) {
-    baseQuery = baseQuery.populate('roleId');
-  }
-
-  const isPaging = options.limit != null && options.limit > 0;
-  if (isPaging) {
-    const limit = Math.abs(options.limit) || 10;
-    const start = (Math.abs(options.start) || 0);
-    const page = Math.ceil(start / limit);
-
-    const which = {
-      pageSize: limit,
-      pageIndex: page,
-      includeTotalCount: true
-    };
-    try {
-      const counted = await pageQuery(baseQuery, which);
-      const users = [];
-      for await (const userDoc of counted.query.cursor()) {
-        users.push(entityForDocument(userDoc));
-      }
-      const pageof = pageOf(users, which, counted.totalCount);
-      callback(null, users, pageof);
-    } catch (err) {
-      callback(err);
-    }
-  } else {
-    baseQuery.exec(function (err, users) {
-      callback(err, users, null);
-    });
-  }
-};
-
-function entityForDocument(doc) {
-  const json = doc.toJSON();
-  const entity = {
-    ...json,
-    id: doc._id.toHexString()
-  }
-
-  return entity;
-}
 
 exports.createUser = function (user, callback) {
   Authentication.createAuthentication(user.authentication).then(authentication => {
