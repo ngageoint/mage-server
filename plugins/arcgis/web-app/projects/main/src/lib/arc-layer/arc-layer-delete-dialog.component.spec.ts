19cbe88897c5d06e651fa5aca49f11a52c231242
Katherine Brown <katherinebrown@Katherines-MacBook-Pro.local>
2024-10-28 11:51:32 -0500

    updated tests

--- plugins/arcgis/web-app/projects/main/src/lib/arc-layer/arc-layer-delete-dialog.component.spec.ts
+++ plugins/arcgis/web-app/projects/main/src/lib/arc-layer/arc-layer-delete-dialog.component.spec.ts
@@ -1,28 +1,39 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MatDialogRef } from '@angular/material/dialog';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http'; 
+import { MatDialogModule } from '@angular/material/dialog'; 
 import { ArcLayerDeleteDialogComponent } from './arc-layer-delete-dialog.component';
+import { ArcService } from '../arc.service'
 
 describe('Arc Layer Delete Dialog', () => {
   let component: ArcLayerDeleteDialogComponent;
   let fixture: ComponentFixture<ArcLayerDeleteDialogComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule, MatDialogModule], 
       declarations: [ArcLayerDeleteDialogComponent],
-      imports: [HttpClientTestingModule],
-      providers: [{
-        provide: MatDialogRef,
-        useValue: {}
-      },]
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: jasmine.createSpy('close')
+          }
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {} 
+        },
+        ArcService 
+      ]
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ArcLayerDeleteDialogComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
-
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- plugins/arcgis/web-app/projects/showcase/src/app/app.component.spec.ts
+++ plugins/arcgis/web-app/projects/showcase/src/app/app.component.spec.ts
@@ -1,35 +1,23 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges(); 
   });
 
-  it(`should have as title 'showcase'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('showcase');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('showcase app is running!');
+  it('should create the app component', () => {
+    expect(component).toBeTruthy();
   });
 });
