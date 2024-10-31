01c01c88d2c8721572710eaa226719fb73a45b4f
Robert St. John <restjohn@users.noreply.github.com>
2024-10-31 12:38:15 -0600

    refactor(service): users/auth: change default generic parameter type in web app request factory from object to record

--- service/src/adapters/adapters.controllers.web.ts
+++ service/src/adapters/adapters.controllers.web.ts
@@ -3,7 +3,7 @@ import { ErrEntityNotFound, ErrInfrastructure, ErrInvalidInput, ErrPermissionDen
 import { AppRequest } from '../app.api/app.api.global'
 
 export interface WebAppRequestFactory<Req extends AppRequest = AppRequest> {
-  <RequestParams extends object = object>(webReq: express.Request, params?: RequestParams): Req & RequestParams
+  <RequestParams extends Record<string, any> = Record<string, any>>(webReq: express.Request, params?: RequestParams): Req & RequestParams
 }
 
 /**
