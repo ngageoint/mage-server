fa66fa541453fb18e0609e03a40e8f745da49fc7
Robert St. John <restjohn@users.noreply.github.com>
2024-11-05 12:27:05 -0700

    refactor(service): users/auth: add discriminated union for express passport user type to distinguish between session and idp users

--- service/src/@types/express/index.d.ts
+++ service/src/@types/express/index.d.ts
@@ -1,9 +1,24 @@
-import { UserDocument } from '../../models/user'
+import { UserExpanded as MageUser } from '../../entities/users/entities.users'
+import { IdentityProviderUser } from '../../ingress/ingress.entities'
+
+
+export type WebIngressUserFromSessionToken = { from: 'sessionToken', account: MageUser }
+export type WebIngressUserFromIdentityProvider = { from: 'identityProvider', account: IdentityProviderUser }
+/**
+ * The `WebIngressUser` type determines the ingress path of the requesting user through the Passport middleware stack.
+ * When the `mage` key is present, the requesting user authenticated using an established Mage session token.  When the
+ * `identityProvider` key is present, the requesting user authenticated with a third party identity provider account
+ * and will establish a new Mage session.
+ */
+export type WebIngressUser = WebIngressUserFromSessionToken | WebIngressUserFromIdentityProvider
 
 declare module 'express-serve-static-core' {
   export interface Request {
-    user: UserDocument & any
-    provisionedDeviceId: string
+    user?: Express.User & WebIngressUser
+    token?: string
+    // TODO: users-next: reconcile these two device properties and change to device entity
+    provisionedDevice?: any
+    provisionedDeviceId?: string
     /**
      * Return the root HTTP URL of the server, including the scheme, e.g.,
      * `https://mage.io`.
