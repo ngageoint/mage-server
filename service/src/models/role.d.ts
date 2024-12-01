ae54610a86dc0f07c4beb025f31e1db2aab1715d
Robert St. John <restjohn@users.noreply.github.com>
2024-11-30 23:24:33 -0700

    refactor(service): users/auth: role database type changes

--- service/src/models/role.d.ts
+++ service/src/models/role.d.ts
@@ -1,20 +1,22 @@
-
+import mongoose from 'mongoose'
 import { AnyPermission } from '../entities/authorization/entities.permissions'
 
 type Callback<R> = (err: any, result?: R) => any
 
 export declare interface RoleDocument {
-  id: string
+  _id: mongoose.Types.ObjectId
   name: string
   description?: string
   permissions: AnyPermission[]
 }
 
+export type RoleModelInstance = mongoose.HydratedDocument<RoleDocument>
+
 export declare type RoleJson = Omit<RoleDocument, '_id'>
 
-export declare function getRoleById(id: string, callback: Callback<RoleDocument | null>): void
-export declare function getRole(name: string, callback: Callback<RoleDocument | null>): void
-export declare function getRoles(callback: Callback<RoleDocument[]>): void
-export declare function createRole(role: RoleDocument, callback: Callback<RoleDocument>): void
-export declare function updateRole(id: string, update: Partial<RoleDocument>, callback: Callback<RoleDocument>): void
-export declare function deleteRole(role: RoleDocument, callback: Callback<RoleDocument>): void
+export declare function getRoleById(id: string, callback: Callback<RoleModelInstance | null>): void
+export declare function getRole(name: string, callback: Callback<RoleModelInstance | null>): void
+export declare function getRoles(callback: Callback<RoleModelInstance[]>): void
+export declare function createRole(role: Omit<RoleDocument, '_id'>, callback: Callback<RoleModelInstance>): void
+export declare function updateRole(id: string, update: Partial<RoleDocument>, callback: Callback<RoleModelInstance>): void
+export declare function deleteRole(role: RoleModelInstance, callback: Callback<RoleModelInstance>): void
