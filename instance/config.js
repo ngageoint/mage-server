500e5b66531cee906533d8756a0e0c5869d52153
Clinton Werth <clinton@Clintons-MacBook-Pro.local>
2025-03-14 13:42:45 -0500

    working container to push to test

--- instance/config.js
+++ instance/config.js
@@ -34,9 +34,7 @@ module.exports = {
     plugins: {
       servicePlugins: [
         '@ngageoint/mage.arcgis.service',
-        '@ngageoint/mage.image.service',
-        '@ngageoint/mage.nga-msi',
-        '@ngageoint/mage.random',
+
         '@ngageoint/mage.sftp.service'
       ],
       webUIPlugins: [
