65882e8fcf4bf97ed45a8c57b02cf9dbf7627ba4
Ryan Carpenter <ryan.carpenter@omnifederal.com>
2025-04-02 12:00:11 -0600

    Create Contact Us Section on Info Page (#253)
    * Create Contact Us Area on Information Page containing Email and Phone Number
    
    * Fix Budgeting Issue
    
    * Raise Budgets
    
    * more budgeting

--- web-app/src/app/about/about.component.ts
+++ web-app/src/app/about/about.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss', '../../../node_modules/font-awesome/css/font-awesome.min.css']
 })
 export class AboutComponent implements OnInit {
   mageVersion: {
