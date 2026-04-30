5f3d55f991ceab3ff5c11d95267a494d812d86cd
Ryan Carpenter <ryan.carpenter@omnifederal.com>
2026-04-30 15:02:00 -0600

    weird refresh bug

--- web-app/admin/src/app/routing.module.ts
+++ web-app/admin/src/app/routing.module.ts
@@ -11,7 +11,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'top',
+      anchorScrolling: 'enabled'
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
